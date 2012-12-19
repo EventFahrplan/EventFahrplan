e1541f614519d93e005bdbb3b5209fbf28e12258
tuxmobil <tuxmobil@gmx.de>
2012-12-19 11:29:07 +0100

    use Roboto for detail view, adjust spacing

--- src/nerd/tuxmobil/fahrplan/congress/EventDetail.java
+++ src/nerd/tuxmobil/fahrplan/congress/EventDetail.java
@@ -1,6 +1,7 @@
 package nerd.tuxmobil.fahrplan.congress;
 
 import java.util.Locale;
+
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
@@ -8,6 +9,7 @@ import com.actionbarsherlock.view.MenuItem;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
@@ -23,50 +25,68 @@ public class EventDetail extends SherlockActivity {
 	private String title;
 	private static String feedbackURL = "https://cccv.pentabarf.org/feedback/29C3/event/"; // + 4302.en.html
 	private Locale locale;
-	
+	private Typeface boldCondensed;
+	private Typeface black;
+	private Typeface light;
+	private Typeface regular;
+	private Typeface bold;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail);
-        
+
+		boldCondensed = Typeface.createFromAsset(getAssets(), "Roboto-BoldCondensed.ttf");
+		black = Typeface.createFromAsset(getAssets(), "Roboto-Black.ttf");
+		light = Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf");
+		regular = Typeface.createFromAsset(getAssets(), "Roboto-Regular.ttf");
+		bold = Typeface.createFromAsset(getAssets(), "Roboto-Bold.ttf");
         Intent intent = getIntent();
-        
+
         locale = getResources().getConfiguration().locale;
-     
+
         event_id = intent.getStringExtra("eventid");
-        
+
         TextView t = (TextView)findViewById(R.id.title);
         title = intent.getStringExtra("title");
+        t.setTypeface(boldCondensed);
         t.setText(title);
-        
+
         t = (TextView)findViewById(R.id.subtitle);
         t.setText(intent.getStringExtra("subtitle"));
-        
+        t.setTypeface(light);
+        if (intent.getStringExtra("subtitle").length() == 0) t.setVisibility(View.GONE);
+
         t = (TextView)findViewById(R.id.speakers);
+        t.setTypeface(black);
         t.setText(intent.getStringExtra("spkr"));
-        
+
         t = (TextView)findViewById(R.id.abstractt);
+        t.setTypeface(bold);
         String s = intent.getStringExtra("abstract");
-        s = s.replaceAll("\\[(.*?)\\]\\(([^ \\)]+).*?\\)", "<a href=\"$2\">$1</a>");   
+        s = s.replaceAll("\\[(.*?)\\]\\(([^ \\)]+).*?\\)", "<a href=\"$2\">$1</a>");
         t.setText(Html.fromHtml(s), TextView.BufferType.SPANNABLE);
         t.setMovementMethod(new LinkMovementMethod());
-        
+
         t = (TextView)findViewById(R.id.description);
+        t.setTypeface(regular);
         s = intent.getStringExtra("descr");
-        s = s.replaceAll("\\[(.*?)\\]\\(([^ \\)]+).*?\\)", "<a href=\"$2\">$1</a>");   
+        s = s.replaceAll("\\[(.*?)\\]\\(([^ \\)]+).*?\\)", "<a href=\"$2\">$1</a>");
         t.setText(Html.fromHtml(s), TextView.BufferType.SPANNABLE);
         t.setMovementMethod(new LinkMovementMethod());
-        
+
         TextView l = (TextView)findViewById(R.id.linksSection);
+        l.setTypeface(bold);
         String links = intent.getStringExtra("links");
         t = (TextView)findViewById(R.id.links);
-        
+        t.setTypeface(regular);
+
         if (links.length() > 0) {
         	Log.d(LOG_TAG, "show links");
         	l.setVisibility(View.VISIBLE);
         	t.setVisibility(View.VISIBLE);
         	links = links.replaceAll("\\),", ")<br>");
-	        links = links.replaceAll("\\[(.*?)\\]\\(([^ \\)]+).*?\\)", "<a href=\"$2\">$1</a>");   
+	        links = links.replaceAll("\\[(.*?)\\]\\(([^ \\)]+).*?\\)", "<a href=\"$2\">$1</a>");
 	        t.setText(Html.fromHtml(links), TextView.BufferType.SPANNABLE);
 	        t.setMovementMethod(new LinkMovementMethod());
         } else {
@@ -74,7 +94,7 @@ public class EventDetail extends SherlockActivity {
         	t.setVisibility(View.GONE);
         }
     }
-    
+
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
 		MenuInflater mi = new MenuInflater(getApplication());
