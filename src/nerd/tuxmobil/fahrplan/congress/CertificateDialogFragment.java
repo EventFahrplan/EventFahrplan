5e4ad847e0ccb6b02a6c47210b153c7ea746337d
tuxmobil <tuxmobil@gmx.de>
2013-12-28 10:26:12 +0100

    catch the case where the certificate chain is empty (for yet unknown reasons)

--- src/nerd/tuxmobil/fahrplan/congress/CertificateDialogFragment.java
+++ src/nerd/tuxmobil/fahrplan/congress/CertificateDialogFragment.java
@@ -112,7 +112,7 @@ public class CertificateDialogFragment extends SherlockDialogFragment {
 						new DialogInterface.OnClickListener() {
 							public void onClick(DialogInterface dialog, int which) {
 								try {
-									TrustManagerFactory.addCertificateChain(chain);
+									if (chain != null) TrustManagerFactory.addCertificateChain(chain);
 									if (listener != null) {
 										listener.cert_accepted();
 									}
