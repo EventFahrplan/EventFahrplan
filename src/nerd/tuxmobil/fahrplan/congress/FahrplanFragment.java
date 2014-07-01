johnjohndoe/CampFahrplan

Avoid hard-coding database columns indices.

src/nerd/tuxmobil/fahrplan/congress/FahrplanFragment.java
@@ -659,27 +659,27 @@ public class FahrplanFragment extends SherlockFragment implements OnClickListene
 
 		cursor.moveToFirst();
 		while (!cursor.isAfterLast()) {
-			Lecture lecture = new Lecture(cursor.getString(0));
+			Lecture lecture = new Lecture(cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.EVENT_ID)));
 
-			lecture.title = cursor.getString(1);
-			lecture.subtitle = cursor.getString(2);
-			lecture.day = cursor.getInt(3);
-			lecture.room = cursor.getString(4);
-			lecture.startTime = cursor.getInt(5);
-			lecture.duration = cursor.getInt(6);
-			lecture.speakers = cursor.getString(7);
-			lecture.track = cursor.getString(8);
-			lecture.type = cursor.getString(9);
-			lecture.lang = cursor.getString(10);
-			lecture.abstractt = cursor.getString(11);
-			lecture.description = cursor.getString(12);
-			lecture.relStartTime = cursor.getInt(13);
-			lecture.date = cursor.getString(14);
-			lecture.links = cursor.getString(15);
-			lecture.dateUTC = cursor.getLong(16);
-			lecture.room_index = cursor.getInt(17);
-			lecture.recordingLicense = cursor.getString(18);
-			lecture.recordingOptOut = cursor.getInt(19) == LecturesTable.Values.REC_OPTOUT_OFF ? Lecture.RECORDING_OPTOUT_OFF : Lecture.RECORDING_OPTOUT_ON;
+			lecture.title = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.TITLE));
+			lecture.subtitle = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.SUBTITLE));
+			lecture.day = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.DAY));
+			lecture.room = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.ROOM));
+			lecture.startTime = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.START));
+			lecture.duration = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.DURATION));
+			lecture.speakers = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.SPEAKERS));
+			lecture.track = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.TRACK));
+			lecture.type = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.TYPE));
+			lecture.lang = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.LANG));
+			lecture.abstractt = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.ABSTRACT));
+			lecture.description = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.DESCR));
+			lecture.relStartTime = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.REL_START));
+			lecture.date = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.DATE));
+			lecture.links = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.LINKS));
+			lecture.dateUTC = cursor.getLong(cursor.getColumnIndex(LecturesTable.Columns.DATE_UTC));
+			lecture.room_index = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.ROOM_IDX));
+			lecture.recordingLicense = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.REC_LICENSE));
+			lecture.recordingOptOut = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.REC_OPTOUT)) == LecturesTable.Values.REC_OPTOUT_OFF ? Lecture.RECORDING_OPTOUT_OFF : Lecture.RECORDING_OPTOUT_ON;
 
 			MyApp.lectureList.add(lecture);
 			cursor.moveToNext();
@@ -737,8 +737,8 @@ public class FahrplanFragment extends SherlockFragment implements OnClickListene
 
 		hCursor.moveToFirst();
 		while (!hCursor.isAfterLast()) {
-			String lecture_id = hCursor.getString(1);
-			int highlightState = hCursor.getInt(2);
+			String lecture_id = hCursor.getString(cursor.getColumnIndex(HighlightsTable.Columns.EVENT_ID));
+			int highlightState = hCursor.getInt(cursor.getColumnIndex(HighlightsTable.Columns.HIGHLIGHT));
 			MyApp.LogDebug(LOG_TAG, "lecture "+lecture_id+" is hightlighted:" + highlightState);
 
 			for (Lecture lecture : MyApp.lectureList) {
@@ -784,7 +784,7 @@ public class FahrplanFragment extends SherlockFragment implements OnClickListene
 
 		alarmCursor.moveToFirst();
 		while (!alarmCursor.isAfterLast()) {
-			String lecture_id = alarmCursor.getString(5);
+			String lecture_id = alarmCursor.getString(alarmCursor.getColumnIndex(AlarmsTable.Columns.EVENT_ID));
 			MyApp.LogDebug(LOG_TAG, "lecture "+lecture_id+" has alarm");
 
 			for (Lecture lecture : MyApp.lectureList) {

EventFahrplan/EventFahrplan

src/nerd/tuxmobil/fahrplan/congress/FahrplanFragment.java
@@ -659,27 +659,27 @@ public class FahrplanFragment extends SherlockFragment implements OnClickListene
 
 		cursor.moveToFirst();
 		while (!cursor.isAfterLast()) {
-			Lecture lecture = new Lecture(cursor.getString(0));
+			Lecture lecture = new Lecture(cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.EVENT_ID)));
 
-			lecture.title = cursor.getString(1);
-			lecture.subtitle = cursor.getString(2);
-			lecture.day = cursor.getInt(3);
-			lecture.room = cursor.getString(4);
-			lecture.startTime = cursor.getInt(5);
-			lecture.duration = cursor.getInt(6);
-			lecture.speakers = cursor.getString(7);
-			lecture.track = cursor.getString(8);
-			lecture.type = cursor.getString(9);
-			lecture.lang = cursor.getString(10);
-			lecture.abstractt = cursor.getString(11);
-			lecture.description = cursor.getString(12);
-			lecture.relStartTime = cursor.getInt(13);
-			lecture.date = cursor.getString(14);
-			lecture.links = cursor.getString(15);
-			lecture.dateUTC = cursor.getLong(16);
-			lecture.room_index = cursor.getInt(17);
-			lecture.recordingLicense = cursor.getString(18);
-			lecture.recordingOptOut = cursor.getInt(19) == LecturesTable.Values.REC_OPTOUT_OFF ? Lecture.RECORDING_OPTOUT_OFF : Lecture.RECORDING_OPTOUT_ON;
+			lecture.title = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.TITLE));
+			lecture.subtitle = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.SUBTITLE));
+			lecture.day = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.DAY));
+			lecture.room = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.ROOM));
+			lecture.startTime = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.START));
+			lecture.duration = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.DURATION));
+			lecture.speakers = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.SPEAKERS));
+			lecture.track = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.TRACK));
+			lecture.type = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.TYPE));
+			lecture.lang = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.LANG));
+			lecture.abstractt = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.ABSTRACT));
+			lecture.description = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.DESCR));
+			lecture.relStartTime = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.REL_START));
+			lecture.date = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.DATE));
+			lecture.links = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.LINKS));
+			lecture.dateUTC = cursor.getLong(cursor.getColumnIndex(LecturesTable.Columns.DATE_UTC));
+			lecture.room_index = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.ROOM_IDX));
+			lecture.recordingLicense = cursor.getString(cursor.getColumnIndex(LecturesTable.Columns.REC_LICENSE));
+			lecture.recordingOptOut = cursor.getInt(cursor.getColumnIndex(LecturesTable.Columns.REC_OPTOUT)) == LecturesTable.Values.REC_OPTOUT_OFF ? Lecture.RECORDING_OPTOUT_OFF : Lecture.RECORDING_OPTOUT_ON;
 
 			MyApp.lectureList.add(lecture);
 			cursor.moveToNext();
@@ -737,8 +737,8 @@ public class FahrplanFragment extends SherlockFragment implements OnClickListene
 
 		hCursor.moveToFirst();
 		while (!hCursor.isAfterLast()) {
-			String lecture_id = hCursor.getString(1);
-			int highlightState = hCursor.getInt(2);
+			String lecture_id = hCursor.getString(hCursor.getColumnIndex(HighlightsTable.Columns.EVENT_ID));
+			int highlightState = hCursor.getInt(hCursor.getColumnIndex(HighlightsTable.Columns.HIGHLIGHT));
 			MyApp.LogDebug(LOG_TAG, "lecture "+lecture_id+" is hightlighted:" + highlightState);
 
 			for (Lecture lecture : MyApp.lectureList) {
@@ -784,7 +784,7 @@ public class FahrplanFragment extends SherlockFragment implements OnClickListene
 
 		alarmCursor.moveToFirst();
 		while (!alarmCursor.isAfterLast()) {
-			String lecture_id = alarmCursor.getString(5);
+			String lecture_id = alarmCursor.getString(alarmCursor.getColumnIndex(AlarmsTable.Columns.EVENT_ID));
 			MyApp.LogDebug(LOG_TAG, "lecture "+lecture_id+" has alarm");
 
 			for (Lecture lecture : MyApp.lectureList) {