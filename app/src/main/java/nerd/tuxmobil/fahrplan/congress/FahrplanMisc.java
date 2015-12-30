EventFahrplan/EventFahrplan

commit 98a34ca4305ca138fefb079d8d21ce0cc6ad7b79

Organize alarm time values in resource files.

app/src/main/java/nerd/tuxmobil/fahrplan/congress/FahrplanMisc.java
@@ -273,7 +273,7 @@ public class FahrplanMisc {
     }
 
     public static void addAlarm(Context context, Lecture lecture, int alarmTimesIndex) {
-        int[] alarm_times = {0, 5, 10, 15, 30, 45, 60};
+        int[] alarm_times = context.getResources().getIntArray(R.array.alarm_time_values);
         long when;
         Time time;
         long startTime;

app/src/main/res/values-de/arrays.xml
@@ -1,0 +1,14 @@
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+
+    <string-array name="alarm_time_titles">
+        <item>zur Startzeit</item>
+        <item>5 Minuten</item>
+        <item>10 Minuten</item>
+        <item>15 Minuten</item>
+        <item>30 Minuten</item>
+        <item>45 Minuten</item>
+        <item>60 Minuten</item>
+    </string-array>
+
+</resources>

app/src/main/res/values-de/strings.xml
@@ -35,15 +35,6 @@
     <string name="event_online">Event online</string>
     <string name="event_detail_section_title_links">Links</string>
     <string name="alarm_prompt">Erinnerung</string>
-    <string-array name="alarm_array">
-        <item>zur Startzeit</item>
-        <item>5 Minuten</item>
-        <item>10 Minuten</item>
-        <item>15 Minuten</item>
-        <item>30 Minuten</item>
-        <item>45 Minuten</item>
-        <item>60 Minuten</item>
-    </string-array>
     <string name="choose_alarm_time">Wähle die Alarmzeit</string>
     <string name="reminders">Erinnerungen</string>
     <string name="reminder">Erinnerung</string>

app/src/main/res/values/arrays.xml
@@ -1,0 +1,24 @@
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+
+    <string-array name="alarm_time_titles">
+        <item>at start time</item>
+        <item>5 minutes</item>
+        <item>10 minutes</item>
+        <item>15 minutes</item>
+        <item>30 minutes</item>
+        <item>45 minutes</item>
+        <item>60 minutes</item>
+    </string-array>
+
+    <string-array name="alarm_time_values" translatable="false">
+        <item>0</item>
+        <item>5</item>
+        <item>10</item>
+        <item>15</item>
+        <item>30</item>
+        <item>45</item>
+        <item>60</item>
+    </string-array>
+
+</resources>

app/src/main/res/values/strings.xml
@@ -35,15 +35,6 @@
     <string name="event_online">Event online</string>
     <string name="event_detail_section_title_links">Links</string>
     <string name="alarm_prompt">Reminder</string>
-    <string-array name="alarm_array">
-        <item>at start time</item>
-        <item>5 Minutes</item>
-        <item>10 Minutes</item>
-        <item>15 Minutes</item>
-        <item>30 Minutes</item>
-        <item>45 Minutes</item>
-        <item>60 Minutes</item>
-    </string-array>
     <string name="choose_alarm_time">Choose alarm time</string>
     <string name="reminders">Reminders</string>
     <string name="reminder">Reminder</string>

commit bb8cb5aa0a626483b0c61c7592d99f835a86e3af

Fix setting non-default alarm time.
+ Resolves issue #135.
+ Bug introduced in 98a34ca4305ca138fefb079d8d21ce0cc6ad7b79.

app/src/main/java/nerd/tuxmobil/fahrplan/congress/FahrplanMisc.java
@@ -19,7 +19,10 @@ import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import nerd.tuxmobil.fahrplan.congress.FahrplanContract.AlarmsTable;
 import nerd.tuxmobil.fahrplan.congress.FahrplanContract.HighlightsTable;
@@ -273,7 +276,12 @@ public class FahrplanMisc {
     }
 
     public static void addAlarm(Context context, Lecture lecture, int alarmTimesIndex) {
-        int[] alarm_times = context.getResources().getIntArray(R.array.alarm_time_values);
+        String[] alarm_times = context.getResources().getStringArray(R.array.alarm_time_values);
+        List<String> alarmTimeStrings = new ArrayList<String>(Arrays.asList(alarm_times));
+        List<Integer> alarmTimes = new ArrayList<Integer>(alarmTimeStrings.size());
+        for (String alarmTimeString : alarmTimeStrings) {
+            alarmTimes.add(Integer.parseInt(alarmTimeString));
+        }
         long when;
         Time time;
         long startTime;
@@ -288,7 +296,7 @@ public class FahrplanMisc {
             startTime = time.normalize(true);
             when = time.normalize(true);
         }
-        long alarmTimeDiffInSeconds = alarm_times[alarmTimesIndex] * 60 * 1000;
+        long alarmTimeDiffInSeconds = alarmTimes.get(alarmTimesIndex) * 60 * 1000;
         when -= alarmTimeDiffInSeconds;
 
         // DEBUG
@@ -317,7 +325,7 @@ public class FahrplanMisc {
         // Set new alarm
         alarmManager.set(AlarmManager.RTC_WAKEUP, when, pendingintent);
 
-        int alarmTimeInMin = alarm_times[alarmTimesIndex];
+        int alarmTimeInMin = alarmTimes.get(alarmTimesIndex);
 
         // write to DB
 