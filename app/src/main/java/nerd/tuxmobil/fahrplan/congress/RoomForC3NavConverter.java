5fc36173786221cf6f83595c4d9976c63af0c5aa
Tobias Preuss <tobias.preuss@googlemail.com>
2016-03-16 12:51:41 +0100

    Improve formatting.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/RoomForC3NavConverter.java
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/RoomForC3NavConverter.java
@@ -8,26 +8,24 @@ import java.util.Map;
 
 public class RoomForC3NavConverter {
 
-    private final static Map<String,String> CCH_MAP= new HashMap<String, String>() {{
-        put("HALL 1","h1");
-        put("HALL 2","h2");
-        put("HALL 3","h3");
-        put("HALL 6","h6");
-        put("HALL 13","h13");
-        put("HALL 14","h14");
+    private final static Map<String, String> CCH_MAP = new HashMap<String, String>() {{
+        put("HALL 1", "h1");
+        put("HALL 2", "h2");
+        put("HALL 3", "h3");
+        put("HALL 6", "h6");
+        put("HALL 13", "h13");
+        put("HALL 14", "h14");
 
-        put("HALL B","hb");
-        put("HALL G","hg");
-        put("HALL F","hf");
+        put("HALL B", "hb");
+        put("HALL G", "hg");
+        put("HALL F", "hf");
     }};
 
     @Nullable
     public static String convert(@NonNull final String venue, @NonNull final String room) {
-
         if (venue.toUpperCase().equals("CCH")) {
             return CCH_MAP.get(room.toUpperCase());
         }
-
         return null;
     }
 
