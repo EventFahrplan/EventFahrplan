73b7dceeb5f69e66c143e3ab952429d69d1d9704
Tobias Preuss <tobias.preuss@googlemail.com>
2017-12-15 12:28:27 +0100

    Delete unnecessary imports.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/changes/ChangeListFragment.java
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/changes/ChangeListFragment.java
@@ -6,8 +6,6 @@ import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.List;
