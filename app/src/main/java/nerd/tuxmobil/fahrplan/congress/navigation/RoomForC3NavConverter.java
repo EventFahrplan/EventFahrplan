e3eeb51dbdb0d9ccf73380a1f84a799d3c2866f9
Laura <3868507+codingcatgirl@users.noreply.github.com>
2017-12-23 23:57:22 +0100

    all known mappings (for now) for c3nav

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/navigation/RoomForC3NavConverter.java
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/navigation/RoomForC3NavConverter.java
@@ -16,28 +16,28 @@ public class RoomForC3NavConverter {
         put("SAAL DIJKSTRA", "hall-d");
 
         // From everything.schedule.xml
-        put("Assembly:Chaos West", null);
+        put("Assembly:Chaos West", "chaos-west-stage");
         put("Assembly:Bogonauten", null);
         put("Assembly:Haecksen", null);
-        put("Assembly:HardwareHackingArea", null);
-        put("Assembly:Jugend hackt", null);
-        put("Assembly:Milliways", null);
+        put("Assembly:HardwareHackingArea", "hardware-hacking-area");
+        put("Assembly:Jugend hackt", "jugend-hackt");
+        put("Assembly:Milliways", "milliways");
         put("Assembly:Open Knowledge Assembly", null);
         put("Assembly:Physikfachschaft Rostock", null);
         put("Assembly:TeaHouse", null);
-        put("CCL Hall 3", null);
-        put("Chaos West Stage", null);
-        put("Hall 3", null);
-        put("Hive Stage", null);
-        put("Kidspace", null);
-        put("Komona Aquarius", null);
-        put("Komona Blue Princess", null);
-        put("Komona Coral Reef", null);
-        put("Komona D.Ressrosa", null);
-        put("Lecture room 11", null);
-        put("Lecture room 12", null);
-        put("Seminar room 13", null);
-        put("Seminar room 14-15", null);
+        put("CCL Hall 3", "ccl-hall-3");
+        put("Chaos West Stage", "chaos-west-stage");
+        put("Hall 3", "ccl-hall-3");
+        put("Hive Stage", "the-hive-stage");
+        put("Kidspace", "kidspace");
+        put("Komona Aquarius", "komona-aquarius");
+        put("Komona Blue Princess", "komona-blue-princess");
+        put("Komona Coral Reef", "komona-coral-reef");
+        put("Komona D.Ressrosa", "komona-d-ressrosa");
+        put("Lecture room 11", "ccl-lecture-room-11");
+        put("Lecture room 12", "ccl-lecture-room-12");
+        put("Seminar room 13", "ccl-seminar-room-13");
+        put("Seminar room 14-15", "ccl-seminar-room-13");
     }};
 
     @Nullable
