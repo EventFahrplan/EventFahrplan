021462cce04458ba0d686346084a059d757746f5
Tobias Preuss <tobias.preuss@googlemail.com>
2018-07-29 17:13:03 +0200

    Use gradle-versions-plugin v.0.20.0.

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -7,7 +7,7 @@ object Versions {
     val assertjAndroid = "1.2.0"
     val emailIntentBuilder = "1.0.0"
     val espresso = "3.0.2"
-    val gradleVersions = "0.18.0"
+    val gradleVersions = "0.20.0"
     val junit = "4.12"
     val kotlin = "1.2.50"
     val mockito = "2.18.0"
