ca0ebf18aec4de27b7692fe38a3447fedcb5bed3
Tobias Preuss <tobias.preuss@googlemail.com>
2018-09-14 09:14:59 +0200

    Resolve Lint warnings.

--- app/src/test/java/nerd/tuxmobil/fahrplan/congress/utils/ConferenceTimeFrameTest.java
+++ app/src/test/java/nerd/tuxmobil/fahrplan/congress/utils/ConferenceTimeFrameTest.java
@@ -11,72 +11,72 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ConferenceTimeFrameTest {
 
     // 2015-12-27T00:00:00+0100, in seconds: 1451170800000
-    final long FIRST_DAY_START_TIME = 1451170800000L;
+    private static final long FIRST_DAY_START_TIME = 1451170800000L;
 
     // 2015-12-31T00:00:00+0100, in seconds: 1451516400000
-    final long LAST_DAY_END_TIME = 1451516400000L;
+    private static final long LAST_DAY_END_TIME = 1451516400000L;
 
     private ConferenceTimeFrame conference;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         conference = new ConferenceTimeFrame(FIRST_DAY_START_TIME, LAST_DAY_END_TIME);
     }
 
     @Test
-    public void getFirstDayStart() throws Exception {
+    public void getFirstDayStart() {
         assertThat(conference.getFirstDayStartTime()).isEqualTo(FIRST_DAY_START_TIME);
     }
 
     @Test
-    public void containsWithTimeWithFirstDayEvent() throws Exception {
+    public void containsWithTimeWithFirstDayEvent() {
         // 2015-12-27T11:30:00+0100, in seconds: 1451212200000
         assertThat(conference.contains(1451212200000L)).isTrue();
     }
 
     @Test
-    public void containsWithOneSecondBeforeFirstDay() throws Exception {
+    public void containsWithOneSecondBeforeFirstDay() {
         // 2015-12-26T23:59:59+0100, in seconds: 1451170799000
         assertThat(conference.contains(1451170799000L)).isFalse();
     }
 
     @Test
-    public void containsWithTimeOfLastDay() throws Exception {
+    public void containsWithTimeOfLastDay() {
         assertThat(conference.contains(LAST_DAY_END_TIME)).isFalse();
     }
 
     @Test
-    public void endsBeforeWithTimeOfLastDay() throws Exception {
+    public void endsBeforeWithTimeOfLastDay() {
         assertThat(conference.endsBefore(LAST_DAY_END_TIME)).isTrue();
     }
 
     @Test
-    public void endsBeforeWithTimeBeforeLastDay() throws Exception {
+    public void endsBeforeWithTimeBeforeLastDay() {
         assertThat(conference.endsBefore(LAST_DAY_END_TIME - 1)).isFalse();
     }
 
     @Test
-    public void startsAfterWithTimeOfFirstDay() throws Exception {
+    public void startsAfterWithTimeOfFirstDay() {
         assertThat(conference.startsAfter(FIRST_DAY_START_TIME)).isFalse();
     }
 
     @Test
-    public void startsAfterWithTimeBeforeFirstDay() throws Exception {
+    public void startsAfterWithTimeBeforeFirstDay() {
         assertThat(conference.startsAfter(FIRST_DAY_START_TIME - 1)).isTrue();
     }
 
     @Test
-    public void startsAtOrBeforeWithTimeOfFirstDay() throws Exception {
+    public void startsAtOrBeforeWithTimeOfFirstDay() {
         assertThat(conference.startsAtOrBefore(FIRST_DAY_START_TIME)).isTrue();
     }
 
     @Test
-    public void startsAtOrBeforeWithTimeAfterFirstDay() throws Exception {
+    public void startsAtOrBeforeWithTimeAfterFirstDay() {
         assertThat(conference.startsAtOrBefore(FIRST_DAY_START_TIME + 1)).isTrue();
     }
 
     @Test
-    public void startsAtOrBeforeWithTimeBeforeFirstDay() throws Exception {
+    public void startsAtOrBeforeWithTimeBeforeFirstDay() {
         assertThat(conference.startsAtOrBefore(FIRST_DAY_START_TIME - 1)).isFalse();
     }
 
