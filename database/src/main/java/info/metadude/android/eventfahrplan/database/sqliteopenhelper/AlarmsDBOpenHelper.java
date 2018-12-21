2aab57a51dc96c00aa53248c5edfec179f4132e9
Tobias Preuss <tobias.preuss@googlemail.com>
2018-12-21 11:02:44 +0100

    Clear database from 34C3 on upgrade.

--- database/src/main/java/info/metadude/android/eventfahrplan/database/sqliteopenhelper/AlarmsDBOpenHelper.java
+++ database/src/main/java/info/metadude/android/eventfahrplan/database/sqliteopenhelper/AlarmsDBOpenHelper.java
@@ -8,7 +8,7 @@ import info.metadude.android.eventfahrplan.database.contract.FahrplanContract.Al
 
 public class AlarmsDBOpenHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
 
     private static final String DATABASE_NAME = "alarms";
 
@@ -39,6 +39,11 @@ public class AlarmsDBOpenHelper extends SQLiteOpenHelper {
             db.execSQL("ALTER TABLE " + AlarmsTable.NAME + " ADD " +
                     AlarmsTable.Columns.ALARM_TIME_IN_MIN + " INTEGER DEFAULT" +
                     AlarmsTable.Defaults.ALARM_TIME_IN_MIN_DEFAULT);
+        }
+        if (oldVersion < 3) {
+            // Clear database from 34C3.
+            db.execSQL("DROP TABLE IF EXISTS " + AlarmsTable.NAME);
+            onCreate(db);
         }
     }
 }
