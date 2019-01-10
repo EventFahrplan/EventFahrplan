3bac56ddff0e6a50dfae84a75d5827380d762904
Tobias Preuss <tobias.preuss@googlemail.com>
2019-01-10 15:49:35 +0100

    Communicate that the list of lectures is not modified by these methods.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/utils/FahrplanMisc.java
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/utils/FahrplanMisc.java
@@ -205,19 +205,19 @@ public class FahrplanMisc {
         return lectures;
     }
 
-    public static int getChangedLectureCount(@NonNull List<Lecture> list, boolean favsOnly) {
+    public static int getChangedLectureCount(@NonNull final List<Lecture> list, boolean favsOnly) {
         int count = count(list, event -> event.isChanged() && (!favsOnly || event.highlight));
         MyApp.LogDebug(LOG_TAG, "getChangedLectureCount " + favsOnly + ":" + count);
         return count;
     }
 
-    public static int getNewLectureCount(@NonNull List<Lecture> list, boolean favsOnly) {
+    public static int getNewLectureCount(@NonNull final List<Lecture> list, boolean favsOnly) {
         int count = count(list, event -> event.changedIsNew && (!favsOnly || event.highlight));
         MyApp.LogDebug(LOG_TAG, "getNewLectureCount " + favsOnly + ":" + count);
         return count;
     }
 
-    public static int getCancelledLectureCount(@NonNull List<Lecture> list, boolean favsOnly) {
+    public static int getCancelledLectureCount(@NonNull final List<Lecture> list, boolean favsOnly) {
         int count = count(list, event -> event.changedIsCanceled && (!favsOnly || event.highlight));
         MyApp.LogDebug(LOG_TAG, "getCancelledLectureCount " + favsOnly + ":" + count);
         return count;
