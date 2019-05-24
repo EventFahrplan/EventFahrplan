b06a64a2f43f47e4c3432f2dd451f8e97bcf78ca
Tobias Preuss <tobias.preuss@googlemail.com>
2019-05-24 11:46:06 +0200

    Limit visibility to the constants which are actually accessed from modules.
    + Let constants be "const".

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -1,55 +1,61 @@
-@file:Suppress("MayBeConstant", "unused")
+@file:Suppress("unused")
 
 package nerd.tuxmobil.fahrplan.congress
 
-object Versions {
-    val androidGradle = "3.3.2"
-    val assertjAndroid = "1.2.0"
-    val emailIntentBuilder = "1.0.0"
-    val espresso = "3.0.2"
-    val gradleVersions = "0.21.0"
-    val junit = "4.12"
-    val kotlin = "1.3.31"
-    val mockito = "2.27.0"
-    val mockitoKotlin = "2.1.0"
-    val okhttp = "3.12.3"
-    val snackengage = "0.19"
-    val sonarQubeGradle = "2.7.1"
-    val supportLibrary = "27.1.1"
-    val testRules = "1.0.2"
-    val threeTenBp = "1.4.0"
-    val tracedroid = "1.4"
+object Android {
+    const val buildToolsVersion = "28.0.3"
+    const val compileSdkVersion = 27
+    const val minSdkVersion = 14
+    const val targetSdkVersion = 27
 }
 
-object Android {
-    val buildToolsVersion = "28.0.3"
-    val compileSdkVersion = 27
-    val minSdkVersion = 14
-    val targetSdkVersion = 27
-}
+private const val kotlinVersion = "1.3.31"
 
 object GradlePlugins {
-    val androidGradle = "com.android.tools.build:gradle:${Versions.androidGradle}"
-    val gradleVersions = "com.github.ben-manes:gradle-versions-plugin:${Versions.gradleVersions}"
-    val sonarQubeGradle = "org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:${Versions.sonarQubeGradle}"
-    val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+
+    private object Versions {
+        const val androidGradle = "3.3.2"
+        const val gradleVersions = "0.21.0"
+        const val sonarQubeGradle = "2.7.1"
+    }
+
+    const val androidGradle = "com.android.tools.build:gradle:${Versions.androidGradle}"
+    const val gradleVersions = "com.github.ben-manes:gradle-versions-plugin:${Versions.gradleVersions}"
+    const val sonarQubeGradle = "org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:${Versions.sonarQubeGradle}"
+    const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
 }
 
 object Libs {
-    val assertjAndroid = "com.squareup.assertj:assertj-android:${Versions.assertjAndroid}"
-    val emailIntentBuilder = "de.cketti.mailto:email-intent-builder:${Versions.emailIntentBuilder}"
-    val espresso = "com.android.support.test.espresso:espresso-core:${Versions.espresso}"
-    val junit = "junit:junit:${Versions.junit}"
-    val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
-    val mockitoCore = "org.mockito:mockito-core:${Versions.mockito}"
-    val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoKotlin}"
-    val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
-    val okhttpLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}"
-    val snackengagePlayrate = "com.github.ligi.snackengage:snackengage-playrate:${Versions.snackengage}"
-    val supportLibraryAnnotations = "com.android.support:support-annotations:${Versions.supportLibrary}"
-    val supportLibraryAppcompatV7 = "com.android.support:appcompat-v7:${Versions.supportLibrary}"
-    val supportLibraryDesign = "com.android.support:design:${Versions.supportLibrary}"
-    val testRules = "com.android.support.test:rules:${Versions.testRules}"
-    val threeTenBp = "org.threeten:threetenbp:${Versions.threeTenBp}"
-    val tracedroid = "org.ligi:tracedroid:${Versions.tracedroid}"
+
+    private object Versions {
+        const val assertjAndroid = "1.2.0"
+        const val emailIntentBuilder = "1.0.0"
+        const val espresso = "3.0.2"
+        const val junit = "4.12"
+        const val mockito = "2.27.0"
+        const val mockitoKotlin = "2.1.0"
+        const val okhttp = "3.12.3"
+        const val snackengage = "0.19"
+        const val supportLibrary = "27.1.1"
+        const val testRules = "1.0.2"
+        const val threeTenBp = "1.4.0"
+        const val tracedroid = "1.4"
+    }
+
+    const val assertjAndroid = "com.squareup.assertj:assertj-android:${Versions.assertjAndroid}"
+    const val emailIntentBuilder = "de.cketti.mailto:email-intent-builder:${Versions.emailIntentBuilder}"
+    const val espresso = "com.android.support.test.espresso:espresso-core:${Versions.espresso}"
+    const val junit = "junit:junit:${Versions.junit}"
+    const val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion"
+    const val mockitoCore = "org.mockito:mockito-core:${Versions.mockito}"
+    const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoKotlin}"
+    const val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttp}"
+    const val okhttpLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttp}"
+    const val snackengagePlayrate = "com.github.ligi.snackengage:snackengage-playrate:${Versions.snackengage}"
+    const val supportLibraryAnnotations = "com.android.support:support-annotations:${Versions.supportLibrary}"
+    const val supportLibraryAppcompatV7 = "com.android.support:appcompat-v7:${Versions.supportLibrary}"
+    const val supportLibraryDesign = "com.android.support:design:${Versions.supportLibrary}"
+    const val testRules = "com.android.support.test:rules:${Versions.testRules}"
+    const val threeTenBp = "org.threeten:threetenbp:${Versions.threeTenBp}"
+    const val tracedroid = "org.ligi:tracedroid:${Versions.tracedroid}"
 }
