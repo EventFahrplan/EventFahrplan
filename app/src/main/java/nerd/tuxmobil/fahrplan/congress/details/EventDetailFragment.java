EventFahrplan/EventFahrplan

commit 8d0107331a4d88dc2626d5095cbf3349d0d66a00

Don't use MyApp.lectureList to populate EventDetailFragment
The MyApp.lectureList mechanism is populating Lecture.isHighlight and
Lecture.hasAlarm. AppRepository.readLectureByLectureId() was therefore
updated to also provide this functionality.

Resolves #204

app/src/main/java/nerd/tuxmobil/fahrplan/congress/details/EventDetailFragment.java
@@ -158,7 +158,10 @@ public class EventDetailFragment extends Fragment {
 
             locale = getResources().getConfiguration().locale;
 
+            // TODO: Remove this after 36C3. Right now it's only kept to minimize the likelihood of
+            //  unintended behavior changes.
             FahrplanFragment.loadLectureList(appRepository, day, requiresScheduleReload);
+
             lecture = eventIdToLecture(eventId);
 
             // Detailbar
@@ -345,17 +348,7 @@ public class EventDetailFragment extends Fragment {
 
     @NonNull
     private Lecture eventIdToLecture(String eventId) {
-        if (MyApp.lectureList == null) {
-            throw new NullPointerException("Lecture list is null.");
-        }
-        for (Lecture lecture : MyApp.lectureList) {
-            if (lecture.lectureId.equals(eventId)) {
-                return lecture;
-            }
-        }
-        Lecture lecture = appRepository.readLectureByLectureId(eventId);
-        Log.d(LOG_TAG, lecture.lectureId + ": " + lecture.getChangedStateString());
-        throw new IllegalStateException("Lecture list does not contain eventId: " + eventId + ", lecture: " + lecture.getChangedStateString());
+        return appRepository.readLectureByLectureId(eventId);
     }
 
     @Override

app/src/main/java/nerd/tuxmobil/fahrplan/congress/repositories/AppRepository.kt
@@ -279,8 +279,21 @@ object AppRepository {
         highlightsDatabaseRepository.insert(values, lecture.lectureId)
     }
 
-    fun readLectureByLectureId(lectureId: String) =
-            lecturesDatabaseRepository.queryLectureByLectureId(lectureId).first().toLectureAppModel()
+    fun readLectureByLectureId(lectureId: String): Lecture {
+        val lecture = lecturesDatabaseRepository.queryLectureByLectureId(lectureId).toLectureAppModel()
+
+        val highlight = highlightsDatabaseRepository.queryByEventId(lectureId.toInt())
+        if (highlight != null) {
+            lecture.highlight = highlight.isHighlight
+        }
+
+        val alarms = alarmsDatabaseRepository.query(lectureId)
+        if (alarms.isNotEmpty()) {
+            lecture.hasAlarm = true
+        }
+
+        return lecture
+    }
 
     private fun readLecturesForDayIndexOrderedByDateUtc(dayIndex: Int) =
             lecturesDatabaseRepository.queryLecturesForDayIndexOrderedByDateUtc(dayIndex).toLecturesAppModel()

database/src/main/java/info/metadude/android/eventfahrplan/database/repositories/HighlightsDatabaseRepository.kt
@@ -56,4 +56,33 @@ class HighlightsDatabaseRepository(
         return highlights.toList()
     }
 
+    fun queryByEventId(eventId: Int): Highlight? {
+        val database = sqLiteOpenHelper.readableDatabase
+        val cursor = try {
+            database.read(
+                tableName = HighlightsTable.NAME,
+                selection = "$EVENT_ID=?",
+                selectionArgs = arrayOf(eventId.toString())
+            )
+        } catch (e: SQLiteException) {
+            database.close()
+            sqLiteOpenHelper.close()
+            return null
+        }
+
+        val highlight = cursor.use {
+            if (cursor.moveToFirst()) {
+                val highlightState = cursor.getInt(HIGHLIGHT)
+                val isHighlighted = highlightState == HIGHLIGHT_STATE_ON
+                Highlight(eventId, isHighlighted)
+            } else {
+                null
+            }
+        }
+
+        database.close()
+        sqLiteOpenHelper.close()
+        return highlight
+    }
+
 }

database/src/main/java/info/metadude/android/eventfahrplan/database/repositories/LecturesDatabaseRepository.kt
@@ -33,7 +33,7 @@ class LecturesDatabaseRepository(
         read(LecturesTable.NAME,
                 selection = "$EVENT_ID=?",
                 selectionArgs = arrayOf(lectureId))
-    }
+    }.first()
 
     fun queryLecturesForDayIndexOrderedByDateUtc(dayIndex: Int) = query {
         read(LecturesTable.NAME,

commit c2d45668c35c0d6ad9ac86c552a2ac6436170f11

Ensure de/selecting a favorite in details is reflected in the main view.
+ The main schedule view relies on the correct state of the static
  lecture list (MyApp.lectureList) in order to render the favor state.
  Therefore, it is required that once a lecture is modified in the details
  view also the corresponding entry in the static lecture list (if present)
  is updated.
+ Bug introduced in 8d0107331a4d88dc2626d5095cbf3349d0d66a00.
+ Relates to issue #204, #214.

app/src/main/java/nerd/tuxmobil/fahrplan/congress/details/EventDetailFragment.java
@@ -403,6 +403,7 @@ public class EventDetailFragment extends Fragment {
                 if (lecture != null) {
                     lecture.highlight = true;
                     appRepository.updateHighlight(lecture);
+                    appRepository.updateLecturesLegacy(lecture);
                 }
                 refreshUI(activity);
                 return true;
@@ -410,6 +411,7 @@ public class EventDetailFragment extends Fragment {
                 if (lecture != null) {
                     lecture.highlight = false;
                     appRepository.updateHighlight(lecture);
+                    appRepository.updateLecturesLegacy(lecture);
                 }
                 refreshUI(activity);
                 return true;

app/src/main/java/nerd/tuxmobil/fahrplan/congress/repositories/AppRepository.kt
@@ -17,6 +17,7 @@ import info.metadude.android.eventfahrplan.network.repositories.ScheduleNetworkR
 import info.metadude.kotlin.library.engelsystem.models.Shift
 import kotlinx.coroutines.Job
 import nerd.tuxmobil.fahrplan.congress.BuildConfig
+import nerd.tuxmobil.fahrplan.congress.MyApp
 import nerd.tuxmobil.fahrplan.congress.dataconverters.*
 import nerd.tuxmobil.fahrplan.congress.exceptions.AppExceptionHandler
 import nerd.tuxmobil.fahrplan.congress.logging.Logging
@@ -315,6 +316,20 @@ object AppRepository {
 
     fun readDateInfos() =
             readLecturesOrderedByDateUtc().toDateInfos()
+
+    /**
+     * Updates the legacy static lecture list.
+     * This method should be invoked when the state of a [lecture] is changed
+     * while other parts of the application such as the main screen render
+     * their views based on the [legacy static lecture list][MyApp.lectureList].
+     */
+    @Deprecated("Drop method once MyApp.lectureList is gone.")
+    fun updateLecturesLegacy(lecture: Lecture) {
+        MyApp.lectureList?.firstOrNull { lecture.lectureId == it.lectureId }?.let {
+            it.highlight = lecture.highlight
+            it.hasAlarm = lecture.hasAlarm
+        }
+    }
 
     private fun updateLectures(lectures: List<Lecture>) {
         val lecturesDatabaseModel = lectures.toLecturesDatabaseModel()