139ece756965f8de1be8ab31b9401c622845e9a8
Tobias Preuss <tobias.preuss@googlemail.com>
2020-05-04 18:39:25 +0200

    Use Android plugin v.3.6.3.
    + Changelog: https://developer.android.com/studio/releases/gradle-plugin#3-6-0.

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -14,7 +14,7 @@ private const val kotlinVersion = "1.3.72"
 object GradlePlugins {
 
     private object Versions {
-        const val androidGradle = "3.5.3"
+        const val androidGradle = "3.6.3"
         const val gradleVersions = "0.28.0"
         const val sonarQubeGradle = "2.8"
         const val unmockGradle = "0.7.6"
