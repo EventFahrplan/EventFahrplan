504caa0f4443a57421a0eea9fae12b2995dd1060
Tobias Preuss <tobias.preuss@googlemail.com>
2020-07-06 19:42:09 +0200

    Remove unused "toNetworkHttpStatus" extension function.
    + Unused since: 2bc63f631809a584973a66f514c6be312b5dcdb5.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/dataconverters/HttpStatusExtensions.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/dataconverters/HttpStatusExtensions.kt
@@ -16,17 +16,3 @@ fun NetworkHttpStatus.toAppHttpStatus() = when (this) {
     NetworkHttpStatus.HTTP_NOT_FOUND -> AppHttpStatus.HTTP_NOT_FOUND
     NetworkHttpStatus.HTTP_CLEARTEXT_NOT_PERMITTED -> AppHttpStatus.HTTP_CLEARTEXT_NOT_PERMITTED
 }
-
-fun AppHttpStatus.toNetworkHttpStatus() = when (this) {
-    AppHttpStatus.HTTP_OK -> NetworkHttpStatus.HTTP_OK
-    AppHttpStatus.HTTP_LOGIN_FAIL_UNTRUSTED_CERTIFICATE -> NetworkHttpStatus.HTTP_LOGIN_FAIL_UNTRUSTED_CERTIFICATE
-    AppHttpStatus.HTTP_DNS_FAILURE -> NetworkHttpStatus.HTTP_DNS_FAILURE
-    AppHttpStatus.HTTP_COULD_NOT_CONNECT -> NetworkHttpStatus.HTTP_COULD_NOT_CONNECT
-    AppHttpStatus.HTTP_SSL_SETUP_FAILURE -> NetworkHttpStatus.HTTP_SSL_SETUP_FAILURE
-    AppHttpStatus.HTTP_CANNOT_PARSE_CONTENT -> NetworkHttpStatus.HTTP_CANNOT_PARSE_CONTENT
-    AppHttpStatus.HTTP_WRONG_HTTP_CREDENTIALS -> NetworkHttpStatus.HTTP_WRONG_HTTP_CREDENTIALS
-    AppHttpStatus.HTTP_CONNECT_TIMEOUT -> NetworkHttpStatus.HTTP_CONNECT_TIMEOUT
-    AppHttpStatus.HTTP_NOT_MODIFIED -> NetworkHttpStatus.HTTP_NOT_MODIFIED
-    AppHttpStatus.HTTP_NOT_FOUND -> NetworkHttpStatus.HTTP_NOT_FOUND
-    AppHttpStatus.HTTP_CLEARTEXT_NOT_PERMITTED -> NetworkHttpStatus.HTTP_CLEARTEXT_NOT_PERMITTED
-}
