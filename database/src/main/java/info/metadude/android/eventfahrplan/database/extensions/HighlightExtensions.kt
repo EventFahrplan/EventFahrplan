7a069fd0a9ecd9a0c88c3be422ac555e50aa46eb
Tobias Preuss <tobias.preuss@googlemail.com>
2020-07-13 21:41:25 +0200

    Use "contentValuesOf" function provided by "core-ktx".

--- database/src/main/java/info/metadude/android/eventfahrplan/database/extensions/HighlightExtensions.kt
+++ database/src/main/java/info/metadude/android/eventfahrplan/database/extensions/HighlightExtensions.kt
@@ -1,13 +1,13 @@
 package info.metadude.android.eventfahrplan.database.extensions
 
-import android.content.ContentValues
+import androidx.core.content.contentValuesOf
 import info.metadude.android.eventfahrplan.database.contract.FahrplanContract.HighlightsTable.Columns.HIGHLIGHT
 import info.metadude.android.eventfahrplan.database.contract.FahrplanContract.HighlightsTable.Columns.SESSION_ID
 import info.metadude.android.eventfahrplan.database.contract.FahrplanContract.HighlightsTable.Values.HIGHLIGHT_STATE_OFF
 import info.metadude.android.eventfahrplan.database.contract.FahrplanContract.HighlightsTable.Values.HIGHLIGHT_STATE_ON
 import info.metadude.android.eventfahrplan.database.models.Highlight
 
-fun Highlight.toContentValues() = ContentValues().apply {
-    put(SESSION_ID, sessionId)
-    put(HIGHLIGHT, if (isHighlight) HIGHLIGHT_STATE_ON else HIGHLIGHT_STATE_OFF)
-}
+fun Highlight.toContentValues() = contentValuesOf(
+        SESSION_ID to sessionId,
+        HIGHLIGHT to if (isHighlight) HIGHLIGHT_STATE_ON else HIGHLIGHT_STATE_OFF
+)
--- database/src/main/java/info/metadude/android/eventfahrplan/database/extensions/MetaExtensions.kt
+++ database/src/main/java/info/metadude/android/eventfahrplan/database/extensions/MetaExtensions.kt
@@ -1,13 +1,17 @@
 package info.metadude.android.eventfahrplan.database.extensions
 
-import android.content.ContentValues
-import info.metadude.android.eventfahrplan.database.contract.FahrplanContract.MetasTable.Columns.*
+import androidx.core.content.contentValuesOf
+import info.metadude.android.eventfahrplan.database.contract.FahrplanContract.MetasTable.Columns.ETAG
+import info.metadude.android.eventfahrplan.database.contract.FahrplanContract.MetasTable.Columns.NUM_DAYS
+import info.metadude.android.eventfahrplan.database.contract.FahrplanContract.MetasTable.Columns.SUBTITLE
+import info.metadude.android.eventfahrplan.database.contract.FahrplanContract.MetasTable.Columns.TITLE
+import info.metadude.android.eventfahrplan.database.contract.FahrplanContract.MetasTable.Columns.VERSION
 import info.metadude.android.eventfahrplan.database.models.Meta
 
-fun Meta.toContentValues() = ContentValues().apply {
-    put(ETAG, eTag)
-    put(NUM_DAYS, numDays)
-    put(SUBTITLE, subtitle)
-    put(TITLE, title)
-    put(VERSION, version)
-}
+fun Meta.toContentValues() = contentValuesOf(
+        ETAG to eTag,
+        NUM_DAYS to numDays,
+        SUBTITLE to subtitle,
+        TITLE to title,
+        VERSION to version
+)
