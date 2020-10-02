54e3675e110180ce012a3fdf31c02ebe76f64f62
Tobias Preuss <tobias.preuss@googlemail.com>
2020-10-02 18:57:44 +0200

    Use core-ktx v.1.3.2.
    + Changelog: https://developer.android.com/jetpack/androidx/releases/core#core_and_core-ktx_version_132_2.

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -36,7 +36,7 @@ object Libs {
         const val assertjAndroid = "1.2.0"
         const val betterLinkMovementMethod = "2.2.0" // minSdkVersion 16, see AndroidManifest
         const val constraintLayout = "2.0.2"
-        const val coreKtx = "1.3.1"
+        const val coreKtx = "1.3.2"
         const val emailIntentBuilder = "2.0.0"
         const val engelsystem = "4.0.1"
         const val espresso = "3.3.0"
