ca3e2f4fdac45144893ebe2041eaaff7e7c7771a
Tobias Preuss <tobias.preuss@googlemail.com>
2020-12-03 20:49:22 +0100

    Replace AssertJ with Truth assertions.

--- app/src/test/java/nerd/tuxmobil/fahrplan/congress/schedule/ConferenceTest.kt
+++ app/src/test/java/nerd/tuxmobil/fahrplan/congress/schedule/ConferenceTest.kt
@@ -1,9 +1,9 @@
 package nerd.tuxmobil.fahrplan.congress.schedule
 
+import com.google.common.truth.Truth.assertThat
 import info.metadude.android.eventfahrplan.commons.temporal.Moment
 import info.metadude.android.eventfahrplan.commons.temporal.Moment.Companion.MINUTES_OF_ONE_DAY
 import nerd.tuxmobil.fahrplan.congress.models.Session
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 class ConferenceTest {
