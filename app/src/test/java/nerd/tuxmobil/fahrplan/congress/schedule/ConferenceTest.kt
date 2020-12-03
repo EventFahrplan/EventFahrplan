0b6ef789318fba63c20904b30a7184944b956197
Tobias Preuss <tobias.preuss@googlemail.com>
2020-12-03 19:52:52 +0100

    Make ConferenceTest easier to read and maintain.

--- app/src/test/java/nerd/tuxmobil/fahrplan/congress/schedule/ConferenceTest.kt
+++ app/src/test/java/nerd/tuxmobil/fahrplan/congress/schedule/ConferenceTest.kt
@@ -10,52 +10,31 @@ class ConferenceTest {
 
     @Test
     fun `calculateTimeFrame with frab data spanning multiple days`() {
-        val opening = Session("Opening").apply {
-            dateUTC = 1536332400000L // 2018-09-07T17:00:00+02:00
-            duration = 30
-        }
-        val closing = Session("Closing").apply {
-            dateUTC = 1536504300000L // 2018-09-09T16:45:00+02:00
-            duration = 30
-        }
-        with(Conference()) {
-            calculateTimeFrame(listOf(opening, closing)) { dateUTC: Long -> Moment.ofEpochMilli(dateUTC).minuteOfDay }
-            assertThat(firstSessionStartsAt).isEqualTo(17 * 60 - 2 * 60) // 17:00h -2h zone offset = 15:00h
-            assertThat(lastSessionEndsAt).isEqualTo(1035 - 2 * 60 + MINUTES_OF_ONE_DAY) // -> 17:15 -2h zone offset + day switch
-        }
+        val opening = createSession("Opening", duration = 30, dateUtc = 1536332400000L) // 2018-09-07T17:00:00+02:00
+        val closing = createSession("Closing", duration = 30, dateUtc = 1536504300000L) // 2018-09-09T16:45:00+02:00
+        val (firstSessionStartsAt, lastSessionEndsAt) = createConference(opening, closing)
+        assertThat(firstSessionStartsAt).isEqualTo(17 * 60 - 2 * 60) // 17:00h -2h zone offset = 15:00h
+        assertThat(lastSessionEndsAt).isEqualTo(1035 - 2 * 60 + MINUTES_OF_ONE_DAY) // -> 17:15 -2h zone offset + day switch
     }
 
     @Test
     fun `calculateTimeFrame with frab data spanning a single day`() {
-        val opening = Session("Opening").apply {
-            dateUTC = 1536332400000L // 2018-09-07T17:00:00+02:00
-            duration = 30
-        }
-        val closing = Session("Closing").apply {
-            dateUTC = 1536336000000L // 2018-09-07T18:00:00+02:00
-            duration = 30
-        }
-        with(Conference()) {
-            calculateTimeFrame(listOf(opening, closing)) { dateUTC: Long -> Moment.ofEpochMilli(dateUTC).minuteOfDay }
-            assertThat(firstSessionStartsAt).isEqualTo(17 * 60 - 2 * 60) // 17:00h -2h zone offset = 15:00h
-            assertThat(lastSessionEndsAt).isEqualTo(18 * 60 + 30 - 2 * 60) // -> 18:00 -2h zone offset
+        val opening = createSession("Opening", duration = 30, dateUtc = 1536332400000L) // 2018-09-07T17:00:00+02:00
+        val closing = createSession("Closing", duration = 30, dateUtc = 1536336000000L) // 2018-09-07T18:00:00+02:00
+        val (firstSessionStartsAt, lastSessionEndsAt) = createConference(opening, closing)
+        assertThat(firstSessionStartsAt).isEqualTo(17 * 60 - 2 * 60) // 17:00h -2h zone offset = 15:00h
+        assertThat(lastSessionEndsAt).isEqualTo(18 * 60 + 30 - 2 * 60) // -> 18:00 -2h zone offset
+    }
+
+    private fun createConference(vararg sessions: Session) = Conference().apply {
+        calculateTimeFrame(listOf(*sessions)) { dateUtc: Long ->
+            Moment.ofEpochMilli(dateUtc).minuteOfDay
         }
     }
 
-    @Test
-    fun `calculateTimeFrame with Pentabarf data`() {
-        val opening = Session("Opening").apply {
-            duration = 25
-            relStartTime = 570 // 09:30
-        }
-        val closing = Session("Closing").apply {
-            duration = 10
-            relStartTime = 1070 // 17:50
-        }
-        with(Conference()) {
-            calculateTimeFrame(listOf(opening, closing)) { dateUTC: Long -> Moment.ofEpochMilli(dateUTC).minuteOfDay }
-            assertThat(firstSessionStartsAt).isEqualTo(570) // 09:30
-            assertThat(lastSessionEndsAt).isEqualTo(1080) // 18:00
-        }
+    private fun createSession(sessionId: String, duration: Int, dateUtc: Long) = Session(sessionId).apply {
+        this.dateUTC = dateUtc
+        this.duration = duration
     }
+
 }
