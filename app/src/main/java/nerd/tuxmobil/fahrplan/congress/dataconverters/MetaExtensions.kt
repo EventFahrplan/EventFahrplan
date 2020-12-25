f86f2c601186ba69a4c4395587dbaede34474875
Tobias Preuss <tobias.preuss@googlemail.com>
2020-12-25 23:34:39 +0100

    Prevent crash ZoneRulesException when re-installing the app.
    Stacktrace excerpt:
    
     at org.threeten.bp.zone.ZoneRulesProvider.getProvider(ZoneRulesProvider.java:165)
     at org.threeten.bp.zone.ZoneRulesProvider.getRules(ZoneRulesProvider.java:122)
     at org.threeten.bp.ZoneRegion.ofId(ZoneRegion.java:143)
     at org.threeten.bp.ZoneId.of(ZoneId.java:358)
     at nerd.tuxmobil.fahrplan.congress.dataconverters.MetaExtensionsKt.toMetaAppModel(MetaExtensions.kt:24)
     at nerd.tuxmobil.fahrplan.congress.repositories.AppRepository.readMeta(AppRepository.kt:424)

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/dataconverters/MetaExtensions.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/dataconverters/MetaExtensions.kt
@@ -1,6 +1,7 @@
 package nerd.tuxmobil.fahrplan.congress.dataconverters
 
 import org.threeten.bp.ZoneId
+import org.threeten.bp.zone.ZoneRulesException
 import info.metadude.android.eventfahrplan.database.models.Meta as MetaDatabaseModel
 import info.metadude.android.eventfahrplan.network.models.Meta as MetaNetworkModel
 import nerd.tuxmobil.fahrplan.congress.models.Meta as MetaAppModel
@@ -20,8 +21,13 @@ fun MetaDatabaseModel.toMetaAppModel() = MetaAppModel(
         numDays = numDays,
         subtitle = subtitle,
         timeZoneId = timeZoneName?.let {
-            // Try/catch omitted here because this happened in MetaValidation.validate before.
-            ZoneId.of(it)
+            try {
+                ZoneId.of(it)
+            } catch (e: ZoneRulesException) {
+                // TODO Duplicate try/catch, also in MetaValidation.validate.
+                // Crashes on re-installation otherwise. Further investigation needed.
+                null
+            }
         },
         title = title,
         version = version
