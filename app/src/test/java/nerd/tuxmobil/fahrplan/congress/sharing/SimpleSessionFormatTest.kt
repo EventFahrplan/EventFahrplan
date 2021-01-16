c05c7c30a0644a1b244f2789972fd884fdba567e
Tobias Preuss <tobias.preuss@googlemail.com>
2021-01-16 12:50:44 +0100

    Fix SimpleSessionFormatTest for build flavors configured for Pentabarf.

--- app/src/test/java/nerd/tuxmobil/fahrplan/congress/sharing/SimpleSessionFormatTest.kt
+++ app/src/test/java/nerd/tuxmobil/fahrplan/congress/sharing/SimpleSessionFormatTest.kt
@@ -1,7 +1,9 @@
 package nerd.tuxmobil.fahrplan.congress.sharing
 
 import com.google.common.truth.Truth.assertThat
+import nerd.tuxmobil.fahrplan.congress.BuildConfig
 import nerd.tuxmobil.fahrplan.congress.models.Session
+import nerd.tuxmobil.fahrplan.congress.utils.ServerBackendType
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -26,6 +28,7 @@ class SimpleSessionFormatTest {
         room = "Yellow pavilion"
         date = "2019-12-27T11:00:00+01:00"
         url = "https://example.com/2019/LD3FX9.html"
+        slug = "LD3FX9"
     }
 
     private val session2 = Session("S2").apply {
@@ -33,6 +36,7 @@ class SimpleSessionFormatTest {
         room = "Dark cellar"
         date = "2019-12-28T17:00:00+01:00"
         url = "https://example.com/2019/U28VSA.html"
+        slug = "U28VSA"
     }
 
     private val session3 = Session("S3").apply {
@@ -41,6 +45,7 @@ class SimpleSessionFormatTest {
         date = "2019-12-29T09:00:00+01:00"
         links = "https://events.ccc.de/congress/2019/wiki/index.php/Session:A/V_Angel_Meeting"
         url = "https://example.com/2019/U28VSA.html"
+        slug = "U28VSA"
     }
 
     private val session4 = Session("S4").apply {
@@ -48,6 +53,7 @@ class SimpleSessionFormatTest {
         room = "Sunshine tent"
         date = "2019-09-01T16:00:00+02:00"
         url = "https://example.com/2019/U9SD23.html"
+        slug = "U9SD23"
     }
 
     @Before
@@ -69,7 +75,7 @@ class SimpleSessionFormatTest {
                 A talk which changes your life
                 Freitag, 27. Dezember 2019 11:00 GMT+01:00, Yellow pavilion
 
-                https://example.com/2019/LD3FX9.html
+                $expectedSession1Url
                 """.trimIndent())
     }
 
@@ -80,7 +86,7 @@ class SimpleSessionFormatTest {
                 A talk which changes your life
                 Freitag, 27. Dezember 2019 11:00 MEZ (Europe/Berlin), Yellow pavilion
 
-                https://example.com/2019/LD3FX9.html
+                $expectedSession1Url
                 """.trimIndent())
     }
 
@@ -105,7 +111,7 @@ class SimpleSessionFormatTest {
                 A talk which changes your life
                 Freitag, 27. Dezember 2019 11:00 MEZ (Europe/Berlin), Yellow pavilion
 
-                https://example.com/2019/LD3FX9.html
+                $expectedSession1Url
                 """.trimIndent())
     }
 
@@ -116,14 +122,14 @@ class SimpleSessionFormatTest {
                 A talk which changes your life
                 Freitag, 27. Dezember 2019 11:00 MEZ (Europe/Berlin), Yellow pavilion
 
-                https://example.com/2019/LD3FX9.html
+                $expectedSession1Url
 
                 ---
 
                 The most boring workshop ever
                 Samstag, 28. Dezember 2019 17:00 MEZ (Europe/Berlin), Dark cellar
 
-                https://example.com/2019/U28VSA.html
+                $expectedSession2Url
                 """.trimIndent())
     }
 
@@ -134,8 +140,18 @@ class SimpleSessionFormatTest {
                 Central european summer time
                 Sonntag, 1. September 2019 16:00 MESZ (Europe/Berlin), Sunshine tent
 
-                https://example.com/2019/U9SD23.html
+                $expectedSession4Url
                 """.trimIndent())
     }
+
+    private val expectedSession1Url = createSessionUrl(slug = "LD3FX9", url = "https://example.com/2019/LD3FX9.html")
+    private val expectedSession2Url = createSessionUrl(slug = "U28VSA", url = "https://example.com/2019/U28VSA.html")
+    private val expectedSession4Url = createSessionUrl(slug = "U9SD23", url = "https://example.com/2019/U9SD23.html")
+
+    private fun createSessionUrl(slug: String, url: String) = if (isPentabarfConfigured()) createPentabarfUrl(slug) else url
+
+    private fun isPentabarfConfigured() = ServerBackendType.PENTABARF.name == BuildConfig.SERVER_BACKEND_TYPE
+
+    private fun createPentabarfUrl(slug: String) = String.format(BuildConfig.EVENT_URL, slug)
 
 }
