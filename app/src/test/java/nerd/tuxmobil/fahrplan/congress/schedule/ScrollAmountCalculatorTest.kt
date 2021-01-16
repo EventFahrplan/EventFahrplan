ad7010ef4365b7c2b0bf8b2823fdf017996d95d7
Tobias Preuss <tobias.preuss@googlemail.com>
2021-01-16 16:12:13 +0100

    Extract ScrollAmountCalculator to make code testable.
    + Still use Java here to make the code comparison easy.

---
+++ app/src/test/java/nerd/tuxmobil/fahrplan/congress/schedule/ScrollAmountCalculatorTest.kt
@@ -0,0 +1,126 @@
+package nerd.tuxmobil.fahrplan.congress.schedule
+
+import com.google.common.truth.Truth.assertThat
+import info.metadude.android.eventfahrplan.commons.temporal.Moment
+import nerd.tuxmobil.fahrplan.congress.NoLogging
+import nerd.tuxmobil.fahrplan.congress.models.DateInfo
+import nerd.tuxmobil.fahrplan.congress.models.DateInfos
+import nerd.tuxmobil.fahrplan.congress.models.RoomData
+import nerd.tuxmobil.fahrplan.congress.models.ScheduleData
+import nerd.tuxmobil.fahrplan.congress.models.Session
+import org.junit.Test
+import org.threeten.bp.ZoneOffset
+
+class ScrollAmountCalculatorTest {
+
+    private companion object {
+        const val BOX_HEIGHT = 34 // Pixel 2 portrait mode
+        const val COLUMN_INDEX = 0
+    }
+
+    @Test
+    fun `calculateScrollAmount returns 0 if room index preceeds the valid column indices`() {
+        val session = createFirstSession()
+        val scrollAmount = createCalculator(session).calculateScrollAmount(
+                nowMoment = Moment.ofEpochMilli(session.dateUTC),
+                currentDayIndex = session.day,
+                columnIndex = -1
+        )
+        assertThat(scrollAmount).isEqualTo(0)
+    }
+
+    @Test
+    fun `calculateScrollAmount returns 0 if room index exceeds the valid column indices`() {
+        val session = createFirstSession()
+        val scrollAmount = createCalculator(session).calculateScrollAmount(
+                nowMoment = Moment.ofEpochMilli(session.dateUTC),
+                currentDayIndex = session.day,
+                columnIndex = COLUMN_INDEX + 1
+        )
+        assertThat(scrollAmount).isEqualTo(0)
+    }
+
+    @Test
+    fun `calculateScrollAmount returns 0 if conference has not started but it will today`() {
+        val session = createFirstSession()
+        val scrollAmount = createCalculator(session).calculateScrollAmount(
+                nowMoment = Moment.ofEpochMilli(session.dateUTC).minusMinutes(1),
+                currentDayIndex = session.day
+        )
+        assertThat(scrollAmount).isEqualTo(0)
+    }
+
+    @Test
+    fun `calculateScrollAmount returns 0 if conference starts now`() {
+        val session = createFirstSession()
+        val scrollAmount = createCalculator(session).calculateScrollAmount(
+                nowMoment = Moment.ofEpochMilli(session.dateUTC),
+                currentDayIndex = session.day
+        )
+        assertThat(scrollAmount).isEqualTo(0)
+    }
+
+    @Test
+    fun `calculateScrollAmount returns 0 if first session is almost done`() {
+        val session = createFirstSession()
+        val scrollAmount = createCalculator(session).calculateScrollAmount(
+                nowMoment = Moment.ofEpochMilli(session.endsAtDateUtc).minusMinutes(1),
+                currentDayIndex = session.day
+        )
+        assertThat(scrollAmount).isEqualTo(0)
+    }
+
+    @Test
+    fun `calculateScrollAmount returns end of session if first session is done`() {
+        val session = createFirstSession()
+        val scrollAmount = createCalculator(session).calculateScrollAmount(
+                nowMoment = Moment.ofEpochMilli(session.endsAtDateUtc),
+                currentDayIndex = session.day
+        )
+        assertThat(scrollAmount).isEqualTo(408)
+    }
+
+    @Test
+    fun `calculateScrollAmount returns 408 for a session crossing the intra-day limit`() {
+        val session = createLateSession()
+        val scrollAmount = createCalculator(session).calculateScrollAmount(
+                nowMoment = Moment.ofEpochMilli(session.endsAtDateUtc),
+                currentDayIndex = session.day
+        )
+        assertThat(scrollAmount).isEqualTo(408)
+    }
+
+    private fun createCalculator(session: Session): ScrollAmountCalculator {
+        val sessions = listOf(session)
+        val roomData = RoomData(roomName = session.room, sessions = sessions)
+        val scheduleData = ScheduleData(dayIndex = session.day, roomDataList = listOf(roomData))
+        val conference = Conference.ofSessions(sessions)
+        val dateInfo = DateInfo(dayIdx = session.day, date = Moment.parseDate(session.date))
+        val dateInfos = DateInfos().apply { add(dateInfo) }
+        return ScrollAmountCalculator(NoLogging, dateInfos, scheduleData, conference)
+    }
+
+    private fun ScrollAmountCalculator.calculateScrollAmount(
+            nowMoment: Moment,
+            currentDayIndex: Int,
+            columnIndex: Int = COLUMN_INDEX
+    ) = calculateScrollAmount(nowMoment, currentDayIndex, BOX_HEIGHT, columnIndex)
+
+    private fun createFirstSession() = createBaseSession("s1",
+            Moment.ofEpochMilli(1582963200000L) // February 29, 2020 08:00:00 AM GMT
+    )
+
+    private fun createLateSession() = createBaseSession("s2",
+            Moment.ofEpochMilli(1583019000000L) // February 29, 2020 11:30:00 PM GMT
+    )
+
+    private fun createBaseSession(sessionId: String, moment: Moment) = Session(sessionId).apply {
+        day = 0
+        date = moment.toZonedDateTime(ZoneOffset.UTC).toLocalDate().toString()
+        dateUTC = moment.toMilliseconds()
+        startTime = moment.minuteOfDay
+        duration = 60
+        room = "Main hall"
+    }
+
+}
