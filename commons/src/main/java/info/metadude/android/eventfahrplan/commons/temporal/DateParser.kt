216f6d65f7c96c9d2f0f40faf437ad37fc484f61
Tobias Preuss <tobias.preuss@googlemail.com>
2021-02-18 23:37:27 +0100

    Test time rendering in different device time zones.
    + Make code from "network" DateParser available in
      "commons" module to allow usage in other modules.

--- commons/src/main/java/info/metadude/android/eventfahrplan/commons/temporal/DateParser.kt
+++ commons/src/main/java/info/metadude/android/eventfahrplan/commons/temporal/DateParser.kt
@@ -1,10 +1,24 @@
 package info.metadude.android.eventfahrplan.commons.temporal
 
+import org.threeten.bp.Instant
+import org.threeten.bp.ZoneOffset
 import org.threeten.bp.ZonedDateTime
 import org.threeten.bp.format.DateTimeFormatter
 import org.threeten.bp.format.DateTimeParseException
 
 object DateParser {
+
+    /**
+     * Parses the given [text] and returns its date value represented in milliseconds.
+     *
+     * Expects the [text] to be provided in [ISO_DATE_TIME][DateTimeFormatter.ISO_DATE_TIME]
+     * format (e.g. 2019-01-01T00:00:00Z).
+     */
+    fun parseDateTime(text: String): Long {
+        val instant = Instant.from(DateTimeFormatter.ISO_DATE_TIME.parse(text))
+        val atUtcOffset = instant.atOffset(ZoneOffset.UTC)
+        return atUtcOffset.toEpochSecond() * Moment.MILLISECONDS_OF_ONE_SECOND
+    }
 
     /**
      * Parses the given [text] and returns its time zone offset value represented in seconds.
--- commons/src/test/java/info/metadude/android/eventfahrplan/commons/temporal/DateParserTest.kt
+++ commons/src/test/java/info/metadude/android/eventfahrplan/commons/temporal/DateParserTest.kt
@@ -3,8 +3,63 @@ package info.metadude.android.eventfahrplan.commons.temporal
 import com.google.common.truth.Truth.assertThat
 import org.junit.Assert.fail
 import org.junit.Test
+import org.threeten.bp.DateTimeException
+import org.threeten.bp.format.DateTimeParseException
 
 class DateParserTest {
+
+    @Test
+    fun `parseDateTime returns milliseconds for 2019 date and time with time zone and offset`() {
+        assertThat(DateParser.parseDateTime("2019-01-01T00:00:00Z")).isEqualTo(1546300800000)
+    }
+
+    @Test
+    fun `parseDateTime returns milliseconds for 1970 epoch date and time UTC`() {
+        assertThat(DateParser.parseDateTime("1970-01-01T00:00:00Z")).isEqualTo(0)
+    }
+
+    @Test
+    fun `parseDateTime returns milliseconds for 1970 epoch date and time with time zone with zero offset`() {
+        assertThat(DateParser.parseDateTime("1970-01-01T00:00:00+00:00")).isEqualTo(0)
+    }
+
+    @Test
+    fun `parseDateTime returns milliseconds for short after 1970 epoch date and time with time zone with offset`() {
+        assertThat(DateParser.parseDateTime("1970-01-01T02:00:00+01:00")).isEqualTo(3600000)
+    }
+
+    @Test
+    fun `parseDateTime returns milliseconds for 2016 date and time with time zone with offset`() {
+        // Test format of date / times ever seen in schedule.xml files.
+        assertThat(DateParser.parseDateTime("2016-09-14T14:30:00+02:00")).isEqualTo(1473856200000)
+    }
+
+    @Test
+    fun `parseDateTime returns milliseconds for 2016 date and time UTC`() {
+        // Test format of date / times ever seen in schedule.xml files.
+        assertThat(DateParser.parseDateTime("2016-09-14T12:30:00Z")).isEqualTo(1473856200000)
+    }
+
+    @Test
+    fun `parseDateTime fails when time zone offset is missing without a colon`() {
+        // Test format of date / times ever seen in schedule.xml files.
+        try {
+            DateParser.parseDateTime("2016-09-14T14:30:00+0200")
+            fail("Failure expected because malformed date string should not be parsed.")
+        } catch (e: DateTimeParseException) {
+            assertThat(e.message).startsWith("Text '2016-09-14T14:30:00+0200' could not be parsed")
+        }
+    }
+
+    @Test
+    fun `parseDateTime fails when time zone offset is missing`() {
+        try {
+            DateParser.parseDateTime("1970-01-01T03:00:00")
+            fail("Failure expected because malformed date string should not be parsed.")
+        } catch (e: DateTimeException) {
+            assertThat(e.message).startsWith("Unable to obtain Instant from TemporalAccessor: DateTimeBuilder")
+        }
+    }
 
     @Test
     fun `parseTimeZoneOffset returns negative integer for negative time zone offset`() {
