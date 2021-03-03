5f3c1e83ec2fa6015b000e04018a6443299418fe
Tobias Preuss <tobias.preuss@googlemail.com>
2021-03-03 20:59:12 +0100

    Use coroutines v.1.4.3.
    + Changelog:
      https://github.com/Kotlin/kotlinx.coroutines/blob/master/CHANGES.md#version-143.
    + Depends on org.jetbrains.kotlin:kotlin-stdlib:1.4.30 -> 1.4.31.

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -41,7 +41,7 @@ object Libs {
         const val engelsystem = "5.0.0"
         const val espresso = "3.3.0"
         const val junit = "4.13.2"
-        const val kotlinCoroutines = "1.4.2"
+        const val kotlinCoroutines = "1.4.3"
         const val material = "1.3.0"
         const val mockito = "3.8.0"
         const val mockitoKotlin = "2.2.0"
