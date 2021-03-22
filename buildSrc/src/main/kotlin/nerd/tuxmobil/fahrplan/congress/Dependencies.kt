ed12b0cfdaac371320cc33909a5eb358ce66b7fa
Tobias Preuss <tobias.preuss@googlemail.com>
2021-03-22 18:57:45 +0100

    Use Android plugin v.4.1.3.
    + Changelog: https://developer.android.com/studio/releases/gradle-plugin#4-1-0.

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -12,7 +12,7 @@ object Android {
 object Plugins {
 
     private object Versions {
-        const val android = "4.1.2"
+        const val android = "4.1.3"
         const val androidJunitJacoco = "0.16.0"
         const val kotlin = "1.4.32"
         const val sonarQube = "3.1.1"
