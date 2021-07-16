5dde99eda98c826b1b4f5e2153c9809bf68cff64
Tobias Preuss <tobias.preuss@googlemail.com>
2021-07-16 13:19:31 +0200

    Use truth v.1.1.3.
    + Changelog: https://github.com/google/truth/releases/tag/release_1_1_3.

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -55,7 +55,7 @@ object Libs {
         const val testExtJunit = "1.1.2"
         const val threeTenBp = "1.5.1"
         const val tracedroid = "1.4"
-        const val truth = "1.1.2"
+        const val truth = "1.1.3"
     }
 
     const val annotation = "androidx.annotation:annotation:${Versions.annotation}"
