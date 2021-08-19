ddf65869007e88f5c8462f63f596297b5adcac94
Tobias Preuss <tobias.preuss@googlemail.com>
2021-08-19 22:15:06 +0200

    Use constraintlayout v.2.1.0.
    + Changelog: https://developer.android.com/jetpack/androidx/releases/constraintlayout#version_21_2.

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -41,7 +41,7 @@ object Libs {
         const val appCompat = "1.3.1"
         const val assertjAndroid = "1.2.0"
         const val betterLinkMovementMethod = "2.2.0"
-        const val constraintLayout = "2.0.4"
+        const val constraintLayout = "2.1.0"
         const val coreKtx = "1.6.0"
         const val emailIntentBuilder = "2.0.0"
         const val engelsystem = "5.2.0"
