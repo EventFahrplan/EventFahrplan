a13a64d7a16c3bcca19603edbcd6c5a83eb3dfd9
Tobias Preuss <tobias.preuss@googlemail.com>
2021-11-02 23:43:12 +0100

    Use mockito v.4.0.0.
    + Changelog: https://github.com/mockito/mockito/compare/v3.12.0...v4.0.0.

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -52,7 +52,7 @@ object Libs {
         const val liveDataKtx = "2.3.1"
         const val markwon = "4.6.2"
         const val material = "1.4.0"
-        const val mockito = "3.12.0"
+        const val mockito = "4.0.0"
         const val mockitoKotlin = "2.2.0"
         const val moshi = "1.12.0"
         const val okhttp = "3.12.13"
