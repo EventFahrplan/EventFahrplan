be3fcf182293520e1f70373d13fb760261308795
isiko <koerner@informatik-ag.net>
2022-01-21 20:41:13 +0100

    Transpiled BundleKeys from Java to Kotlin.

---
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/contract/BundleKeys.kt
@@ -0,0 +1,44 @@
+package nerd.tuxmobil.fahrplan.congress.contract
+
+object BundleKeys {
+
+    // Add + delete alarm
+    const val ALARM_SESSION_ID = "nerd.tuxmobil.fahrplan.congress.ALARM_SESSION_ID"
+    const val ALARM_DAY = "nerd.tuxmobil.fahrplan.congress.ALARM_DAY"
+    const val ALARM_TITLE = "nerd.tuxmobil.fahrplan.congress.ALARM_TITLE"
+    const val ALARM_START_TIME = "nerd.tuxmobil.fahrplan.congress.ALARM_START_TIME"
+
+    // Session alarm notification
+    const val SESSION_ALARM_SESSION_ID =
+        "nerd.tuxmobil.fahrplan.congress.SESSION_ALARM_SESSION_ID"
+    const val SESSION_ALARM_DAY_INDEX =
+        "nerd.tuxmobil.fahrplan.congress.SESSION_ALARM_DAY_INDEX"
+    const val SESSION_ALARM_NOTIFICATION_ID =
+        "nerd.tuxmobil.fahrplan.congress.SESSION_ALARM_NOTIFICATION_ID"
+
+    // Side pane
+    const val SIDEPANE = "nerd.tuxmobil.fahrplan.congress.SIDEPANE"
+
+    // Changes dialog
+    const val CHANGES_DLG_NUM_CHANGED =
+        "nerd.tuxmobil.fahrplan.congress.ChangesDialog.NUM_CHANGES"
+    const val CHANGES_DLG_NUM_NEW =
+        "nerd.tuxmobil.fahrplan.congress.ChangesDialog.NUM_NEW"
+    const val CHANGES_DLG_NUM_CANCELLED =
+        "nerd.tuxmobil.fahrplan.congress.ChangesDialog.NUM_CANCELLED"
+    const val CHANGES_DLG_NUM_MARKED =
+        "nerd.tuxmobil.fahrplan.congress.ChangesDialog.NUM_MARKED"
+    const val CHANGES_DLG_VERSION =
+        "nerd.tuxmobil.fahrplan.congress.ChangesDialog.VERSION"
+
+    // Settings
+    const val SCHEDULE_URL_UPDATED =
+        "nerd.tuxmobil.fahrplan.congress.Prefs.SCHEDULE_URL_UPDATED"
+    const val ENGELSYSTEM_SHIFTS_URL_UPDATED =
+        "nerd.tuxmobil.fahrplan.congress.Prefs.ENGELSYSTEM_SHIFTS_URL_UPDATED"
+    const val ALTERNATIVE_HIGHLIGHTING_UPDATED =
+        "nerd.tuxmobil.fahrplan.congress.Prefs.ALTERNATIVE_HIGHLIGHT"
+    const val USE_DEVICE_TIME_ZONE_UPDATED =
+        "nerd.tuxmobil.fahrplan.congress.Prefs.USE_DEVICE_TIME_ZONE_UPDATED"
+
+}
