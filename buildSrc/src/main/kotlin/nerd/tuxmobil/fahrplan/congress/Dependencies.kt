08bb954d4bfddb31de76c3149e1b4363cafd33af
Tobias Preuss <tobias.preuss@googlemail.com>
2022-03-21 22:16:36 +0100

    Document compileSdk dependency for preference.
    + Changelog: https://developer.android.com/jetpack/androidx/releases/preference/#version_12_2.

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -58,7 +58,7 @@ object Libs {
         const val mockitoKotlin = "4.0.0"
         const val moshi = "1.13.0"
         const val okhttp = "3.12.13"
-        const val preference = "1.1.1"
+        const val preference = "1.1.1" // compileSdk 31 is required as of 1.2.0
         const val retrofit = "2.6.4"
         const val robolectric = "4.3_r2-robolectric-0"
         const val snackengage = "0.29"
