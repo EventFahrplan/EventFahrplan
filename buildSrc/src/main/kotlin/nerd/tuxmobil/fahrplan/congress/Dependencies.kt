ef6a3f757e407339c0b5bb69b5e42c3104624048
Tobias Preuss <tobias.preuss@googlemail.com>
2022-04-08 09:49:58 +0200

    Use Android Gradle Plugin v.7.1.3.
    + Changelogs:
      https://developer.android.com/studio/releases/gradle-plugin#7-1-0
      https://androidstudio.googleblog.com/2022/04/android-studio-bumblebee-202111-patch-3.html

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -18,7 +18,7 @@ object Android {
 object Plugins {
 
     private object Versions {
-        const val android = "7.1.2"
+        const val android = "7.1.3"
         const val androidJunitJacoco = "0.16.0"
         const val dexcount = "3.1.0"
         const val kotlin = "1.6.20"
