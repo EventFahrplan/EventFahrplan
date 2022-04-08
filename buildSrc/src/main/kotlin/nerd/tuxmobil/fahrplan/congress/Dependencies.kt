d60b84cf036e04c92da3e7e9e3247a7b13d987fe
Tobias Preuss <tobias.preuss@googlemail.com>
2022-04-08 09:44:35 +0200

    Use coroutines v.1.6.1.
    + Changelog: https://github.com/Kotlin/kotlinx.coroutines/releases/tag/1.6.1.

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -52,7 +52,7 @@ object Libs {
         const val engelsystem = "7.0.0"
         const val espresso = "3.4.0"
         const val junit = "4.13.2"
-        const val kotlinCoroutines = "1.6.0"
+        const val kotlinCoroutines = "1.6.1"
         const val lifecycle = "2.4.1"
         const val markwon = "4.6.2"
         const val material = "1.5.0"
