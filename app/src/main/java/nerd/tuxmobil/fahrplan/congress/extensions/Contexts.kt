490decaef7ad162958a0d195c76c0c8bb6e2a21f
Tobias Preuss <tobias.preuss@googlemail.com>
2022-11-23 21:52:10 +0100

    Migrate from NotificationChannel to NotificationManagerCompat.
    + Resolve TODO: "LockscreenVisibility is a read-only property; it can only
      be modified by the system.", "setLockscreenVisibility is thus
      intentionally not available as part of NotificationChannelCompat."
      Source: https://issuetracker.google.com/issues/193814308

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/extensions/Contexts.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/extensions/Contexts.kt
@@ -3,19 +3,19 @@
 package nerd.tuxmobil.fahrplan.congress.extensions
 
 import android.app.AlarmManager
-import android.app.NotificationManager
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
 import android.view.LayoutInflater
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.getSystemService
 
 fun Context.getAlarmManager() = getSystemService<AlarmManager>()!!
 
 fun Context.getLayoutInflater() = getSystemService<LayoutInflater>()!!
 
-fun Context.getNotificationManager() = getSystemService<NotificationManager>()!!
+fun Context.getNotificationManager() = getSystemService<NotificationManagerCompat>()!!
 
 fun Context.startActivity(intent: Intent, onActivityNotFound: () -> Unit) {
     try {
