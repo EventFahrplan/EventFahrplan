EventFahrplan/EventFahrplan

commit 490decaef7ad162958a0d195c76c0c8bb6e2a21f

Migrate from NotificationChannel to NotificationManagerCompat.
+ Resolve TODO: "LockscreenVisibility is a read-only property; it can only
  be modified by the system.", "setLockscreenVisibility is thus
  intentionally not available as part of NotificationChannelCompat."
  Source: https://issuetracker.google.com/issues/193814308

app/src/main/java/nerd/tuxmobil/fahrplan/congress/extensions/Contexts.kt
@@ -3,19 +3,19 @@
 package nerd.tuxmobil.fahrplan.congress.extensions
 
 import android.app.AlarmManager
-import android.app.NotificationManager
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
 import android.view.LayoutInflater
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.getSystemService
 
 fun Context.getAlarmManager() = getSystemService<AlarmManager>()!!
 
 fun Context.getLayoutInflater() = getSystemService<LayoutInflater>()!!
 
-fun Context.getNotificationManager() = getSystemService<NotificationManager>()!!
+fun Context.getNotificationManager() = getSystemService<NotificationManagerCompat>()!!
 
 fun Context.startActivity(intent: Intent, onActivityNotFound: () -> Unit) {
     try {

app/src/main/java/nerd/tuxmobil/fahrplan/congress/notifications/NotificationHelper.kt
@@ -1,21 +1,20 @@
 package nerd.tuxmobil.fahrplan.congress.notifications
 
 import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.ContextWrapper
 import android.net.Uri
-import android.os.Build
+import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import nerd.tuxmobil.fahrplan.congress.R
 import nerd.tuxmobil.fahrplan.congress.extensions.getNotificationManager
 
 internal class NotificationHelper(context: Context) : ContextWrapper(context) {
 
-    private val notificationManager: NotificationManager by lazy {
+    private val notificationManager: NotificationManagerCompat by lazy {
         getNotificationManager()
     }
 
@@ -71,15 +70,13 @@ internal class NotificationHelper(context: Context) : ContextWrapper(context) {
     }
 
     private fun createNotificationChannel(id: String, name: String, descriptionText: String) {
-        // TODO Replace with NotificationChannelCompat once it's complete. See https://issuetracker.google.com/issues/193814308
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            with(NotificationChannel(id, name, NotificationManager.IMPORTANCE_DEFAULT)) {
-                description = descriptionText
-                lightColor = color
-                lockscreenVisibility = Notification.VISIBILITY_PUBLIC
-                notificationManager.createNotificationChannel(this)
-            }
-        }
+        val channel = NotificationChannelCompat
+            .Builder(id, NotificationManagerCompat.IMPORTANCE_DEFAULT)
+            .setName(name)
+            .setDescription(descriptionText)
+            .setLightColor(color)
+            .build()
+        notificationManager.createNotificationChannel(channel)
     }
 
     private fun getNotificationBuilder(

commit 73716c678348731f8ecc594a4f3df5ac40d64034

Fix crash when opening schedule changes screen.
+ Broken since: 490decaef7ad162958a0d195c76c0c8bb6e2a21f.
+ Stacktrace:

  java.lang.NullPointerException
    at nerd.tuxmobil.fahrplan.congress.extensions.Contexts.getNotificationManager(Contexts.kt:32)
    at nerd.tuxmobil.fahrplan.congress.notifications.NotificationHelper$notificationManager$2.invoke(NotificationHelper.kt:18)
    at nerd.tuxmobil.fahrplan.congress.notifications.NotificationHelper$notificationManager$2.invoke(NotificationHelper.kt:17)
    at kotlin.SynchronizedLazyImpl.getValue(LazyJVM.kt:74)
    at nerd.tuxmobil.fahrplan.congress.notifications.NotificationHelper.getNotificationManager(NotificationHelper.kt:17)
    at nerd.tuxmobil.fahrplan.congress.notifications.NotificationHelper.createNotificationChannel(NotificationHelper.kt:79)
    at nerd.tuxmobil.fahrplan.congress.notifications.NotificationHelper.createChannels(NotificationHelper.kt:26)
    at nerd.tuxmobil.fahrplan.congress.notifications.NotificationHelper.<init>(NotificationHelper.kt:22)
    at nerd.tuxmobil.fahrplan.congress.changes.ChangeListFragment$observeViewModel$2.invoke(ChangeListFragment.kt:90)
    at nerd.tuxmobil.fahrplan.congress.changes.ChangeListFragment$observeViewModel$2.invoke(ChangeListFragment.kt:89)
    at nerd.tuxmobil.fahrplan.congress.changes.ChangeListFragment.observeViewModel$lambda$1(ChangeListFragment.kt:89)
    at nerd.tuxmobil.fahrplan.congress.changes.ChangeListFragment.$r8$lambda$Rge6oC2T5Y_uOCd8coeZ3068k6Q(Unknown Source:0)
    at nerd.tuxmobil.fahrplan.congress.changes.ChangeListFragment$$ExternalSyntheticLambda1.onChanged(Unknown Source:2)
    at info.metadude.android.eventfahrplan.commons.livedata.SingleLiveEvent$observe$1.invoke(SingleLiveEvent.kt:35)
    at info.metadude.android.eventfahrplan.commons.livedata.SingleLiveEvent$observe$1.invoke(SingleLiveEvent.kt:33)
    at info.metadude.android.eventfahrplan.commons.livedata.SingleLiveEvent.observe$lambda$0(SingleLiveEvent.kt:33)

app/src/main/java/nerd/tuxmobil/fahrplan/congress/extensions/Contexts.kt
@@ -15,7 +15,7 @@ fun Context.getAlarmManager() = getSystemService<AlarmManager>()!!
 
 fun Context.getLayoutInflater() = getSystemService<LayoutInflater>()!!
 
-fun Context.getNotificationManager() = getSystemService<NotificationManagerCompat>()!!
+fun Context.getNotificationManager() = NotificationManagerCompat.from(this)
 
 fun Context.startActivity(intent: Intent, onActivityNotFound: () -> Unit) {
     try {