EventFahrplan/EventFahrplan

commit b411522eea3afe0e7cac40984fe3e4b1c379d76f

Transpile AboutDialog

app/src/main/java/nerd/tuxmobil/fahrplan/congress/about/AboutDialog.kt
@@ -1,0 +1,172 @@
+package nerd.tuxmobil.fahrplan.congress.about
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.fragment.app.DialogFragment
+import nerd.tuxmobil.fahrplan.congress.BuildConfig
+import nerd.tuxmobil.fahrplan.congress.R
+import nerd.tuxmobil.fahrplan.congress.extensions.requireViewByIdCompat
+import nerd.tuxmobil.fahrplan.congress.extensions.setLinkText
+import nerd.tuxmobil.fahrplan.congress.extensions.toSpanned
+import nerd.tuxmobil.fahrplan.congress.extensions.withArguments
+import nerd.tuxmobil.fahrplan.congress.utils.LinkMovementMethodCompat
+
+class AboutDialog : DialogFragment() {
+
+    companion object {
+        const val FRAGMENT_TAG = "AboutDialog"
+        private const val BUNDLE_KEY_SCHEDULE_VERSION = "${BuildConfig.APPLICATION_ID}.BUNDLE_KEY_SCHEDULE_VERSION"
+        private const val BUNDLE_KEY_SUBTITLE = "${BuildConfig.APPLICATION_ID}.BUNDLE_KEY_SUBTITLE"
+        private const val BUNDLE_KEY_TITLE = "${BuildConfig.APPLICATION_ID}.BUNDLE_KEY_TITLE"
+
+        fun newInstance(scheduleVersion: String, subtitle: String, title: String) =
+            AboutDialog().withArguments(
+                BUNDLE_KEY_SCHEDULE_VERSION to scheduleVersion,
+                BUNDLE_KEY_SUBTITLE to subtitle,
+                BUNDLE_KEY_TITLE to title
+            )
+    }
+
+    private var scheduleVersionText = ""
+    private var subtitleText = ""
+    private var titleText = ""
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.about_dialog, container, false)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        with(requireArguments()) {
+            scheduleVersionText = getString(BUNDLE_KEY_SCHEDULE_VERSION, "")
+            subtitleText = getString(BUNDLE_KEY_SUBTITLE, "")
+            titleText = getString(BUNDLE_KEY_TITLE, "")
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Schedule version
+        val scheduleVersion = view.requireViewByIdCompat<TextView>(R.id.about_session_version_view)
+        if (scheduleVersionText.isEmpty()) {
+            scheduleVersion.isVisible = true
+        } else {
+            scheduleVersion.isVisible = false
+            scheduleVersion.text = "${getString(R.string.fahrplan)} $scheduleVersionText"
+        }
+
+        // Session title
+        val title = view.requireViewByIdCompat<TextView>(R.id.about_session_title_view)
+        if (titleText.isEmpty()) {
+            titleText = getString(R.string.app_name)
+        }
+        title.text = titleText
+
+        // Session subtitle
+        val subtitle = view.requireViewByIdCompat<TextView>(R.id.about_session_subtitle_view)
+        if (subtitleText.isEmpty()) {
+            subtitleText = getString(R.string.app_hardcoded_subtitle)
+        }
+        subtitle.text = subtitleText
+
+        // App version
+        val appVersion = view.requireViewByIdCompat<TextView>(R.id.about_app_version_view)
+        appVersion.text = getString(R.string.appVersion, BuildConfig.VERSION_NAME)
+
+        // App disclaimer
+        val appDisclaimer = view.requireViewByIdCompat<View>(R.id.about_app_disclaimer_view)
+        appDisclaimer.isVisible = BuildConfig.SHOW_APP_DISCLAIMER
+        val linkTextColor = ContextCompat.getColor(view.context, R.color.text_link_on_dark)
+        val movementMethod = LinkMovementMethodCompat.getInstance()
+
+        // Logo copyright note
+        val logoCopyright = view.requireViewByIdCompat<TextView>(R.id.about_copyright_logo_view)
+        logoCopyright.text = getString(R.string.copyright_logo).toSpanned()
+        logoCopyright.setLinkTextColor(linkTextColor)
+        logoCopyright.movementMethod = movementMethod
+
+        // Event website URL
+        val conferenceUrl = view.requireViewByIdCompat<TextView>(R.id.about_conference_url_view)
+        val websiteUrl = BuildConfig.EVENT_WEBSITE_URL
+        conferenceUrl.setLinkText(websiteUrl, null, movementMethod, linkTextColor)
+
+        // Source code link
+        val sourceCode = view.requireViewByIdCompat<TextView>(R.id.about_source_code_view)
+        val sourceCodeUrl = BuildConfig.SOURCE_CODE_URL
+        val sourceCodeTitle = getString(R.string.about_source_code)
+        sourceCode.setLinkText(sourceCodeUrl, sourceCodeTitle, movementMethod, linkTextColor)
+
+        // Issues link
+        val issues = view.requireViewByIdCompat<TextView>(R.id.about_issues_view)
+        val issuesUrl = BuildConfig.ISSUES_URL
+        val issuesTitle = getString(R.string.about_issues_or_feature_requests)
+        issues.setLinkText(issuesUrl, issuesTitle, movementMethod, linkTextColor)
+
+        // F-Droid store link
+        val fdroidStore = view.requireViewByIdCompat<TextView>(R.id.about_f_droid_view)
+        val fdroidUrl = BuildConfig.F_DROID_URL
+        if (fdroidUrl.isEmpty()) {
+            fdroidStore.isVisible = false
+        } else {
+            fdroidStore.isVisible = true
+            val fdroidListingTitle = getString(R.string.about_f_droid_listing)
+            fdroidStore.setLinkText(fdroidUrl, fdroidListingTitle, movementMethod, linkTextColor)
+        }
+
+        // Google Play store link
+        val googlePlayStore = view.requireViewByIdCompat<TextView>(R.id.about_google_play_view)
+        val googlePlayUrl = BuildConfig.GOOGLE_PLAY_URL
+        val googlePlayListingTitle = getString(R.string.about_google_play_listing)
+        googlePlayStore.setLinkText(
+            googlePlayUrl,
+            googlePlayListingTitle,
+            movementMethod,
+            linkTextColor
+        )
+
+        // Libraries statement
+        val librariesStatement = view.requireViewByIdCompat<TextView>(R.id.about_libraries_view)
+        val libraryNames = getString(R.string.about_libraries_names)
+        val librariesStatementText = getString(R.string.about_libraries_statement, libraryNames)
+        librariesStatement.text = librariesStatementText
+
+        // Privacy statement
+        val dataPrivacyStatement = view.requireViewByIdCompat<TextView>(R.id.about_data_privacy_statement_view)
+        val dataPrivacyStatementGermanUrl = BuildConfig.DATA_PRIVACY_STATEMENT_DE_URL
+        val dataPrivacyStatementGermanTitle =
+            getString(R.string.about_data_privacy_statement_german)
+        dataPrivacyStatement.setLinkText(
+            dataPrivacyStatementGermanUrl,
+            dataPrivacyStatementGermanTitle,
+            movementMethod,
+            linkTextColor
+        )
+
+        // Build time
+        val buildTimeTextView = view.requireViewByIdCompat<TextView>(R.id.build_time)
+        val buildTimeValue = getString(R.string.build_time)
+        val buildTimeText = getString(R.string.build_info_time, buildTimeValue)
+        buildTimeTextView.text = buildTimeText
+
+        // Build version
+        val versionCodeTextView = view.requireViewByIdCompat<TextView>(R.id.build_version_code)
+        val versionCodeText =
+            getString(R.string.build_info_version_code, "" + BuildConfig.VERSION_CODE)
+        versionCodeTextView.text = versionCodeText
+
+        // Build hash
+        val buildHashTextView = view.requireViewByIdCompat<TextView>(R.id.build_hash)
+        val buildHashValue = getString(R.string.git_sha)
+        val buildHashText = getString(R.string.build_info_hash, buildHashValue)
+        buildHashTextView.text = buildHashText
+    }
+
+}

app/src/main/java/nerd/tuxmobil/fahrplan/congress/about/AboutDialog.java
@@ -1,168 +1,0 @@
-package nerd.tuxmobil.fahrplan.congress.about;
-
-import static nerd.tuxmobil.fahrplan.congress.extensions.ViewExtensions.requireViewByIdCompat;
-
-import android.os.Bundle;
-import android.text.method.MovementMethod;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.MainThread;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.DialogFragment;
-
-import nerd.tuxmobil.fahrplan.congress.BuildConfig;
-import nerd.tuxmobil.fahrplan.congress.R;
-import nerd.tuxmobil.fahrplan.congress.extensions.Strings;
-import nerd.tuxmobil.fahrplan.congress.extensions.TextViewExtensions;
-import nerd.tuxmobil.fahrplan.congress.utils.LinkMovementMethodCompat;
-
-public class AboutDialog extends DialogFragment {
-
-    public static final String FRAGMENT_TAG = "about";
-    private static final String BUNDLE_KEY_SCHEDULE_VERSION =
-            BuildConfig.APPLICATION_ID + ".BUNDLE_KEY_SCHEDULE_VERSION";
-    private static final String BUNDLE_KEY_SUBTITLE =
-            BuildConfig.APPLICATION_ID + ".BUNDLE_KEY_SUBTITLE";
-    private static final String BUNDLE_KEY_TITLE =
-            BuildConfig.APPLICATION_ID + ".BUNDLE_KEY_TITLE";
-
-    public static AboutDialog newInstance(
-            @NonNull String scheduleVersion,
-            @NonNull String subtitle,
-            @NonNull String title
-    ) {
-        Bundle arguments = new Bundle();
-        arguments.putString(BUNDLE_KEY_SCHEDULE_VERSION, scheduleVersion);
-        arguments.putString(BUNDLE_KEY_SUBTITLE, subtitle);
-        arguments.putString(BUNDLE_KEY_TITLE, title);
-        AboutDialog dialog = new AboutDialog();
-        dialog.setArguments(arguments);
-        return dialog;
-    }
-
-    @NonNull
-    private String scheduleVersionText = "";
-
-    @NonNull
-    private String subtitleText = "";
-
-    @NonNull
-    private String titleText = "";
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.about_dialog, container, false);
-    }
-
-    @MainThread
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setStyle(DialogFragment.STYLE_NO_TITLE, R.style.Dialog);
-        Bundle arguments = requireArguments();
-        scheduleVersionText = arguments.getString(BUNDLE_KEY_SCHEDULE_VERSION, "");
-        subtitleText = arguments.getString(BUNDLE_KEY_SUBTITLE, "");
-        titleText = arguments.getString(BUNDLE_KEY_TITLE, "");
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        TextView text = requireViewByIdCompat(view, R.id.about_session_version_view);
-        if (scheduleVersionText.isEmpty()) {
-            text.setVisibility(View.GONE);
-        } else {
-            text.setVisibility(View.VISIBLE);
-            String prefixedScheduleVersionText = getString(R.string.fahrplan) + " " + scheduleVersionText;
-            text.setText(prefixedScheduleVersionText);
-        }
-        text = requireViewByIdCompat(view, R.id.about_session_title_view);
-        if (titleText.isEmpty()) {
-            titleText = getString(R.string.app_name);
-        }
-        text.setText(titleText);
-        text = requireViewByIdCompat(view, R.id.about_session_subtitle_view);
-        if (subtitleText.isEmpty()) {
-            subtitleText = getString(R.string.app_hardcoded_subtitle);
-        }
-        text.setText(subtitleText);
-        text = requireViewByIdCompat(view, R.id.about_app_version_view);
-        String appVersionText = getString(R.string.appVersion, BuildConfig.VERSION_NAME);
-        text.setText(appVersionText);
-
-        View appDisclaimer = requireViewByIdCompat(view, R.id.about_app_disclaimer_view);
-        appDisclaimer.setVisibility(BuildConfig.SHOW_APP_DISCLAIMER ? View.VISIBLE : View.GONE);
-
-        int linkTextColor = ContextCompat.getColor(view.getContext(), R.color.text_link_on_dark);
-        MovementMethod movementMethod = LinkMovementMethodCompat.getInstance();
-
-        TextView logoCopyright = requireViewByIdCompat(view, R.id.about_copyright_logo_view);
-        logoCopyright.setText(Strings.toSpanned(getString(R.string.copyright_logo)));
-        logoCopyright.setLinkTextColor(linkTextColor);
-        logoCopyright.setMovementMethod(movementMethod);
-
-        TextView conferenceUrl = requireViewByIdCompat(view, R.id.about_conference_url_view);
-        String websiteUrl = BuildConfig.EVENT_WEBSITE_URL;
-        TextViewExtensions.setLinkText(conferenceUrl, websiteUrl, movementMethod, linkTextColor);
-
-        TextView sourceCode = requireViewByIdCompat(view, R.id.about_source_code_view);
-        String sourceCodeUrl = BuildConfig.SOURCE_CODE_URL;
-        String sourceCodeTitle = getString(R.string.about_source_code);
-        TextViewExtensions.setLinkText(sourceCode, sourceCodeUrl, sourceCodeTitle, movementMethod, linkTextColor);
-
-        TextView issues = requireViewByIdCompat(view, R.id.about_issues_view);
-        String issuesUrl = BuildConfig.ISSUES_URL;
-        String issuesTitle = getString(R.string.about_issues_or_feature_requests);
-        TextViewExtensions.setLinkText(issues, issuesUrl, issuesTitle, movementMethod, linkTextColor);
-
-        TextView fdroidStore = requireViewByIdCompat(view, R.id.about_f_droid_view);
-        String fdroidUrl = BuildConfig.F_DROID_URL;
-        //noinspection ConstantConditions
-        if (fdroidUrl.isEmpty()) {
-            fdroidStore.setVisibility(View.GONE);
-        } else {
-            fdroidStore.setVisibility(View.VISIBLE);
-            String fdroidListingTitle = getString(R.string.about_f_droid_listing);
-            TextViewExtensions.setLinkText(fdroidStore, fdroidUrl, fdroidListingTitle, movementMethod, linkTextColor);
-        }
-
-        TextView googlePlayStore = requireViewByIdCompat(view, R.id.about_google_play_view);
-        String googlePlayUrl = BuildConfig.GOOGLE_PLAY_URL;
-        String googlePlayListingTitle = getString(R.string.about_google_play_listing);
-        TextViewExtensions.setLinkText(googlePlayStore, googlePlayUrl, googlePlayListingTitle, movementMethod, linkTextColor);
-
-        TextView librariesStatement = requireViewByIdCompat(view, R.id.about_libraries_view);
-        String libraryNames = getString(R.string.about_libraries_names);
-        String librariesStatementText = getString(R.string.about_libraries_statement, libraryNames);
-        librariesStatement.setText(librariesStatementText);
-
-        TextView dataPrivacyStatement = requireViewByIdCompat(view, R.id.about_data_privacy_statement_view);
-        String dataPrivacyStatementGermanUrl = BuildConfig.DATA_PRIVACY_STATEMENT_DE_URL;
-        String dataPrivacyStatementGermanTitle = getString(R.string.about_data_privacy_statement_german);
-        TextViewExtensions.setLinkText(dataPrivacyStatement, dataPrivacyStatementGermanUrl, dataPrivacyStatementGermanTitle, movementMethod, linkTextColor);
-
-        // Build information
-
-        TextView buildTimeTextView = requireViewByIdCompat(view, R.id.build_time);
-        String buildTimeValue = getString(R.string.build_time);
-        String buildTimeText = getString(R.string.build_info_time, buildTimeValue);
-        buildTimeTextView.setText(buildTimeText);
-
-        TextView versionCodeTextView = requireViewByIdCompat(view, R.id.build_version_code);
-        String versionCodeText = getString(R.string.build_info_version_code, "" + BuildConfig.VERSION_CODE);
-        versionCodeTextView.setText(versionCodeText);
-
-        TextView buildHashTextView = requireViewByIdCompat(view, R.id.build_hash);
-        String buildHashValue = getString(R.string.git_sha);
-        String buildHashText = getString(R.string.build_info_hash, buildHashValue);
-        buildHashTextView.setText(buildHashText);
-    }
-}

commit 4bf084de09e4b874c41a1560d4423f365be96e78

Fix visibility of schedule version text.
+ Broken since: b411522eea3afe0e7cac40984fe3e4b1c379d76f.

app/src/main/java/nerd/tuxmobil/fahrplan/congress/about/AboutDialog.kt
@@ -63,9 +63,9 @@ class AboutDialog : DialogFragment() {
         // Schedule version
         val scheduleVersion = view.requireViewByIdCompat<TextView>(R.id.about_session_version_view)
         if (scheduleVersionText.isEmpty()) {
-            scheduleVersion.isVisible = true
-        } else {
             scheduleVersion.isVisible = false
+        } else {
+            scheduleVersion.isVisible = true
             scheduleVersion.text = "${getString(R.string.fahrplan)} $scheduleVersionText"
         }
 