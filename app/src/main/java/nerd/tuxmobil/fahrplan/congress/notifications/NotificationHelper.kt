EventFahrplan/EventFahrplan

commit 63b57b53b279f7660c50e64e288001a9abc8c6aa

Implement new notifications permission.
Since Android 13 there is a permission that has to be granted
in order to send notifications.

I implemented it so that when the permission is not granted,
the app won't let the user add alarms and instead show a Toast.
Also, when the app is started without notification permissions
and the user has alarms set, a Toast is displayed.

app/src/main/java/nerd/tuxmobil/fahrplan/congress/notifications/NotificationHelper.kt
@@ -18,6 +18,8 @@ internal class NotificationHelper(context: Context) : ContextWrapper(context) {
         getNotificationManager()
     }
 
+    val notificationsEnabled = notificationManager.areNotificationsEnabled()
+
     init {
         createChannels()
     }
@@ -125,5 +127,4 @@ internal class NotificationHelper(context: Context) : ContextWrapper(context) {
         private const val SCHEDULE_UPDATE_CHANNEL_ID = "SCHEDULE_UPDATE_CHANNEL"
         const val SCHEDULE_UPDATE_ID = 2
     }
-
 }

app/src/main/AndroidManifest.xml
@@ -6,6 +6,7 @@
     <uses-permission android:name="android.permission.RECEIVE_BOOT_COMPLETED"/>
     <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>
     <uses-permission android:name="android.permission.WAKE_LOCK" />
+    <uses-permission android:name="android.permission.POST_NOTIFICATIONS"/>
 
     <application
             android:icon="@mipmap/ic_launcher"

app/src/main/java/nerd/tuxmobil/fahrplan/congress/details/SessionDetailsFragment.kt
@@ -1,5 +1,7 @@
 package nerd.tuxmobil.fahrplan.congress.details
 
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
@@ -14,6 +16,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.annotation.CallSuper
 import androidx.annotation.IdRes
 import androidx.annotation.LayoutRes
@@ -38,6 +42,7 @@ import nerd.tuxmobil.fahrplan.congress.extensions.startActivity
 import nerd.tuxmobil.fahrplan.congress.extensions.toSpanned
 import nerd.tuxmobil.fahrplan.congress.extensions.withArguments
 import nerd.tuxmobil.fahrplan.congress.models.Session
+import nerd.tuxmobil.fahrplan.congress.notifications.NotificationHelper
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 import nerd.tuxmobil.fahrplan.congress.sharing.SessionSharer
 import nerd.tuxmobil.fahrplan.congress.sidepane.OnSidePaneCloseListener
@@ -69,13 +74,15 @@ class SessionDetailsFragment : Fragment() {
         }
 
     }
-
+    private lateinit var permissionRequestLauncher: ActivityResultLauncher<String>
     private lateinit var appRepository: AppRepository
     private lateinit var alarmServices: AlarmServices
+    private lateinit var notificationHelper: NotificationHelper
     private val viewModel: SessionDetailsViewModel by viewModels {
         SessionDetailsViewModelFactory(
             appRepository = appRepository,
             alarmServices = alarmServices,
+            notificationHelper = notificationHelper,
             defaultEngelsystemRoomName = AppRepository.ENGELSYSTEM_ROOM_NAME,
             customEngelsystemRoomName = getString(R.string.engelsystem_shifts_alias)
         )
@@ -105,6 +112,7 @@ class SessionDetailsFragment : Fragment() {
         super.onAttach(context)
         appRepository = AppRepository
         alarmServices = AlarmServices.newInstance(context, appRepository)
+        notificationHelper = NotificationHelper(context)
         markwon = Markwon.builder(requireContext())
             .usePlugin(LinkifyPlugin.create())
             .build()
@@ -114,6 +122,15 @@ class SessionDetailsFragment : Fragment() {
     @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        permissionRequestLauncher = registerForActivityResult(RequestPermission()) { isGranted ->
+            if (isGranted) {
+                viewModel.setAlarm()
+            } else {
+                showMissingPostNotificationsPermissionError()
+            }
+        }
+
         setHasOptionsMenu(true)
     }
 
@@ -142,6 +159,7 @@ class SessionDetailsFragment : Fragment() {
         activity.setResult(Activity.RESULT_CANCELED)
     }
 
+    @SuppressLint("InlinedApi")
     private fun observeViewModel() {
         viewModel.selectedSessionParameter.observe(viewLifecycleOwner) { model ->
             this.model = model
@@ -183,6 +201,12 @@ class SessionDetailsFragment : Fragment() {
             if (activity is OnSidePaneCloseListener) {
                 (activity as OnSidePaneCloseListener).onSidePaneClose(FRAGMENT_TAG)
             }
+        }
+        viewModel.requestPostNotificationsPermission.observe(viewLifecycleOwner) {
+            permissionRequestLauncher.launch(POST_NOTIFICATIONS)
+        }
+        viewModel.missingPostNotificationsPermission.observe(viewLifecycleOwner) {
+            showMissingPostNotificationsPermissionError()
         }
     }
 
@@ -324,6 +348,10 @@ class SessionDetailsFragment : Fragment() {
         this.setLinkTextColor(ContextCompat.getColor(context, R.color.text_link_on_light))
         this.movementMethod = LinkMovementMethodCompat.getInstance()
         this.isVisible = true
+    }
+
+    private fun showMissingPostNotificationsPermissionError() {
+        Toast.makeText(requireContext(), R.string.alarms_disabled_notifications_permission_missing, Toast.LENGTH_LONG).show()
     }
 
     private fun updateOptionsMenu() {

app/src/main/java/nerd/tuxmobil/fahrplan/congress/details/SessionDetailsViewModel.kt
@@ -1,6 +1,7 @@
 package nerd.tuxmobil.fahrplan.congress.details
 
 import android.net.Uri
+import android.os.Build
 import androidx.core.net.toUri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
@@ -13,6 +14,7 @@ import kotlinx.coroutines.launch
 import nerd.tuxmobil.fahrplan.congress.alarms.AlarmServices
 import nerd.tuxmobil.fahrplan.congress.models.Session
 import nerd.tuxmobil.fahrplan.congress.navigation.RoomForC3NavConverter
+import nerd.tuxmobil.fahrplan.congress.notifications.NotificationHelper
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 import nerd.tuxmobil.fahrplan.congress.repositories.ExecutionContext
 import nerd.tuxmobil.fahrplan.congress.sharing.JsonSessionFormat
@@ -24,11 +26,12 @@ import nerd.tuxmobil.fahrplan.congress.utils.SessionUrlComposition
 import nerd.tuxmobil.fahrplan.congress.wiki.containsWikiLink
 import org.threeten.bp.ZoneOffset
 
-class SessionDetailsViewModel(
+internal class SessionDetailsViewModel(
 
     private val repository: AppRepository,
     private val executionContext: ExecutionContext,
     private val alarmServices: AlarmServices,
+    private val notificationHelper: NotificationHelper,
     private val sessionFormatter: SessionFormatter,
     private val simpleSessionFormat: SimpleSessionFormat,
     private val jsonSessionFormat: JsonSessionFormat,
@@ -39,7 +42,8 @@ class SessionDetailsViewModel(
     private val formattingDelegate: FormattingDelegate = DateFormattingDelegate(),
     private val c3NavBaseUrl: String,
     private val defaultEngelsystemRoomName: String,
-    private val customEngelsystemRoomName: String
+    private val customEngelsystemRoomName: String,
+    private val runsAtLeastOnAndroidTiramisu: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
 
 ) : ViewModel() {
 
@@ -91,6 +95,8 @@ class SessionDetailsViewModel(
     val setAlarm = SingleLiveEvent<Unit>()
     val navigateToRoom = SingleLiveEvent<Uri>()
     val closeDetails = SingleLiveEvent<Unit>()
+    val requestPostNotificationsPermission = SingleLiveEvent<Unit>()
+    val missingPostNotificationsPermission = SingleLiveEvent<Unit>()
 
     private fun SelectedSessionParameter.customizeEngelsystemRoomName() = copy(
         roomName = if (roomName == defaultEngelsystemRoomName) customEngelsystemRoomName else roomName
@@ -186,7 +192,14 @@ class SessionDetailsViewModel(
     }
 
     fun setAlarm() {
-        setAlarm.postValue(Unit)
+        if (notificationHelper.notificationsEnabled) {
+            setAlarm.postValue(Unit)
+        } else {
+            when (runsAtLeastOnAndroidTiramisu) {
+                true -> requestPostNotificationsPermission.postValue(Unit)
+                false -> missingPostNotificationsPermission.postValue(Unit)
+            }
+        }
     }
 
     fun addAlarm(alarmTimesIndex: Int) {

app/src/main/java/nerd/tuxmobil/fahrplan/congress/details/SessionDetailsViewModelFactory.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import nerd.tuxmobil.fahrplan.congress.BuildConfig
 import nerd.tuxmobil.fahrplan.congress.alarms.AlarmServices
 import nerd.tuxmobil.fahrplan.congress.navigation.RoomForC3NavConverter
+import nerd.tuxmobil.fahrplan.congress.notifications.NotificationHelper
 import nerd.tuxmobil.fahrplan.congress.repositories.AppExecutionContext
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 import nerd.tuxmobil.fahrplan.congress.sharing.JsonSessionFormat
@@ -13,10 +14,11 @@ import nerd.tuxmobil.fahrplan.congress.utils.FeedbackUrlComposer
 import nerd.tuxmobil.fahrplan.congress.utils.MarkdownConverter
 import nerd.tuxmobil.fahrplan.congress.utils.SessionUrlComposer
 
-class SessionDetailsViewModelFactory(
+internal class SessionDetailsViewModelFactory(
 
     private val appRepository: AppRepository,
     private val alarmServices: AlarmServices,
+    private val notificationHelper: NotificationHelper,
     private val defaultEngelsystemRoomName: String,
     private val customEngelsystemRoomName: String
 
@@ -28,6 +30,7 @@ class SessionDetailsViewModelFactory(
             repository = appRepository,
             executionContext = AppExecutionContext,
             alarmServices = alarmServices,
+            notificationHelper = notificationHelper,
             sessionFormatter = SessionFormatter(),
             simpleSessionFormat = SimpleSessionFormat(),
             jsonSessionFormat = JsonSessionFormat(),

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/FahrplanFragment.kt
@@ -1,5 +1,7 @@
 package nerd.tuxmobil.fahrplan.congress.schedule
 
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Typeface
 import android.os.Build
@@ -22,6 +24,8 @@ import android.widget.LinearLayout.LayoutParams.MATCH_PARENT
 import android.widget.LinearLayout.LayoutParams.WRAP_CONTENT
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.appcompat.app.ActionBar.NAVIGATION_MODE_LIST
 import androidx.appcompat.app.ActionBar.OnNavigationListener
 import androidx.appcompat.app.AppCompatActivity
@@ -44,6 +48,7 @@ import nerd.tuxmobil.fahrplan.congress.alarms.AlarmServices
 import nerd.tuxmobil.fahrplan.congress.alarms.AlarmTimePickerFragment
 import nerd.tuxmobil.fahrplan.congress.calendar.CalendarSharing
 import nerd.tuxmobil.fahrplan.congress.contract.BundleKeys
+import nerd.tuxmobil.fahrplan.congress.details.SessionDetailsFragment
 import nerd.tuxmobil.fahrplan.congress.extensions.getLayoutInflater
 import nerd.tuxmobil.fahrplan.congress.extensions.isLandscape
 import nerd.tuxmobil.fahrplan.congress.extensions.requireViewByIdCompat
@@ -52,6 +57,7 @@ import nerd.tuxmobil.fahrplan.congress.models.ScheduleData
 import nerd.tuxmobil.fahrplan.congress.models.Session
 import nerd.tuxmobil.fahrplan.congress.net.ConnectivityObserver
 import nerd.tuxmobil.fahrplan.congress.net.ErrorMessage
+import nerd.tuxmobil.fahrplan.congress.notifications.NotificationHelper
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 import nerd.tuxmobil.fahrplan.congress.schedule.observables.TimeTextViewParameter
 import nerd.tuxmobil.fahrplan.congress.sharing.SessionSharer
@@ -89,6 +95,7 @@ class FahrplanFragment : Fragment(), SessionViewEventsHandler {
 
     }
 
+    private lateinit var permissionRequestLauncher: ActivityResultLauncher<String>
     private lateinit var inflater: LayoutInflater
     private lateinit var sessionViewDrawer: SessionViewDrawer
     private lateinit var errorMessageFactory: ErrorMessage.Factory
@@ -104,6 +111,7 @@ class FahrplanFragment : Fragment(), SessionViewEventsHandler {
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
+        val notificationHelper = NotificationHelper(context)
         val appRepository = AppRepository
         val alarmServices = AlarmServices.newInstance(context, appRepository)
         val menuEntriesGenerator = NavigationMenuEntriesGenerator(dayString = getString(R.string.day), todayString = getString(R.string.today))
@@ -112,6 +120,7 @@ class FahrplanFragment : Fragment(), SessionViewEventsHandler {
         val viewModelFactory = FahrplanViewModelFactory(
             repository = appRepository,
             alarmServices = alarmServices,
+            notificationHelper = notificationHelper,
             navigationMenuEntriesGenerator = menuEntriesGenerator,
             defaultEngelsystemRoomName = defaultEngelsystemRoomName,
             customEngelsystemRoomName = customEngelsystemRoomName
@@ -131,6 +140,15 @@ class FahrplanFragment : Fragment(), SessionViewEventsHandler {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        permissionRequestLauncher = registerForActivityResult(RequestPermission()) { isGranted ->
+            if (isGranted) {
+                showAlarmTimePicker()
+            } else {
+                showMissingPostNotificationsPermissionError()
+            }
+        }
+
         setHasOptionsMenu(true)
         val context = requireContext()
         roomTitleTypeFace = TypefaceFactory.getNewInstance(context).getTypeface(Font.Roboto.Light)
@@ -165,6 +183,7 @@ class FahrplanFragment : Fragment(), SessionViewEventsHandler {
         inflater = view.context.getLayoutInflater()
     }
 
+    @SuppressLint("InlinedApi")
     private fun observeViewModel() {
         viewModel.fahrplanParameter.observe(viewLifecycleOwner) { (scheduleData, numDays, dayIndex, menuEntries) ->
             menuEntries?.let { buildNavigationMenu(it, numDays) }
@@ -193,6 +212,15 @@ class FahrplanFragment : Fragment(), SessionViewEventsHandler {
         }
         viewModel.scrollToSessionParameter.observe(viewLifecycleOwner) { (sessionId, verticalPosition, roomIndex) ->
             scrollTo(sessionId, verticalPosition, roomIndex)
+        }
+        viewModel.requestPostNotificationsPermission.observe(viewLifecycleOwner) {
+            permissionRequestLauncher.launch(POST_NOTIFICATIONS)
+        }
+        viewModel.missingPostNotificationsPermission.observe(viewLifecycleOwner) {
+            showMissingPostNotificationsPermissionError()
+        }
+        viewModel.showAlarmTimePicker.observe(viewLifecycleOwner) {
+            showAlarmTimePicker()
         }
     }
 
@@ -455,7 +483,7 @@ class FahrplanFragment : Fragment(), SessionViewEventsHandler {
                 updateMenuItems()
             }
             CONTEXT_MENU_ITEM_ID_SET_ALARM -> {
-                showAlarmTimePicker()
+                viewModel.showAlarmTimePickerWithChecks()
             }
             CONTEXT_MENU_ITEM_ID_DELETE_ALARM -> {
                 viewModel.deleteAlarm(session)
@@ -522,6 +550,10 @@ class FahrplanFragment : Fragment(), SessionViewEventsHandler {
         } else {
             menu.add(0, CONTEXT_MENU_ITEM_ID_SHARE, 4, getString(R.string.menu_item_title_share_session))
         }
+    }
+
+    private fun showMissingPostNotificationsPermissionError() {
+        Toast.makeText(requireContext(), R.string.alarms_disabled_notifications_permission_missing, Toast.LENGTH_LONG).show()
     }
 
     private inner class OnDaySelectedListener(private val numDays: Int) : OnNavigationListener {

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/FahrplanViewModel.kt
@@ -1,5 +1,6 @@
 package nerd.tuxmobil.fahrplan.congress.schedule
 
+import android.os.Build
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -12,6 +13,7 @@ import kotlinx.coroutines.launch
 import nerd.tuxmobil.fahrplan.congress.alarms.AlarmServices
 import nerd.tuxmobil.fahrplan.congress.models.ScheduleData
 import nerd.tuxmobil.fahrplan.congress.models.Session
+import nerd.tuxmobil.fahrplan.congress.notifications.NotificationHelper
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 import nerd.tuxmobil.fahrplan.congress.repositories.ExecutionContext
 import nerd.tuxmobil.fahrplan.congress.schedule.observables.FahrplanEmptyParameter
@@ -29,12 +31,14 @@ internal class FahrplanViewModel(
     private val executionContext: ExecutionContext,
     private val logging: Logging,
     private val alarmServices: AlarmServices,
+    private val notificationHelper: NotificationHelper,
     private val navigationMenuEntriesGenerator: NavigationMenuEntriesGenerator,
     private val simpleSessionFormat: SimpleSessionFormat,
     private val jsonSessionFormat: JsonSessionFormat,
     private val scrollAmountCalculator: ScrollAmountCalculator,
     private val defaultEngelsystemRoomName: String,
-    private val customEngelsystemRoomName: String
+    private val customEngelsystemRoomName: String,
+    private val runsAtLeastOnAndroidTiramisu: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
 
 ) : ViewModel() {
 
@@ -56,10 +60,25 @@ internal class FahrplanViewModel(
     val scrollToCurrentSessionParameter = SingleLiveEvent<ScrollToCurrentSessionParameter>()
     val scrollToSessionParameter = SingleLiveEvent<ScrollToSessionParameter>()
 
+    val requestPostNotificationsPermission = SingleLiveEvent<Unit>()
+    val missingPostNotificationsPermission = SingleLiveEvent<Unit>()
+    val showAlarmTimePicker = SingleLiveEvent<Unit>()
+
     var preserveVerticalScrollPosition: Boolean = false
 
     init {
         updateUncanceledSessions()
+    }
+
+    fun showAlarmTimePickerWithChecks() {
+        if (notificationHelper.notificationsEnabled) {
+            showAlarmTimePicker.postValue(Unit)
+        } else {
+            when (runsAtLeastOnAndroidTiramisu) {
+                true -> requestPostNotificationsPermission.postValue(Unit)
+                false -> missingPostNotificationsPermission.postValue(Unit)
+            }
+        }
     }
 
     private fun updateUncanceledSessions() {

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/FahrplanViewModelFactory.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import info.metadude.android.eventfahrplan.commons.logging.Logging
 import nerd.tuxmobil.fahrplan.congress.alarms.AlarmServices
+import nerd.tuxmobil.fahrplan.congress.notifications.NotificationHelper
 import nerd.tuxmobil.fahrplan.congress.repositories.AppExecutionContext
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 import nerd.tuxmobil.fahrplan.congress.sharing.JsonSessionFormat
@@ -15,7 +16,8 @@ internal class FahrplanViewModelFactory(
     private val alarmServices: AlarmServices,
     private val navigationMenuEntriesGenerator: NavigationMenuEntriesGenerator,
     private val defaultEngelsystemRoomName: String,
-    private val customEngelsystemRoomName: String
+    private val customEngelsystemRoomName: String,
+    private val notificationHelper: NotificationHelper
 
 ) : ViewModelProvider.Factory {
 
@@ -27,6 +29,7 @@ internal class FahrplanViewModelFactory(
             executionContext = AppExecutionContext,
             logging = logging,
             alarmServices = alarmServices,
+            notificationHelper = notificationHelper,
             navigationMenuEntriesGenerator = navigationMenuEntriesGenerator,
             simpleSessionFormat = SimpleSessionFormat(),
             jsonSessionFormat = JsonSessionFormat(),

commit a07825412baa27fb3bc36ea5efa66df2fa2e4e83

Prevent enabled state for "post notifications" permission being cached.
+ Broken since: 63b57b53b279f7660c50e64e288001a9abc8c6aa.
+ This fixes two error cases which occur after fresh install on Android 13:

  Case 1:
  1. Visit session details
  2. Taps bell icon to set an alarm -> system permission UI appears
  3. Grant permission
  -> Alarm picker does not appear. One has to leave the screen and reopen it

  Case 2:
  1. Long tap a session to set an alarm -> system permission UI appears
  2. Grant permission -> alarm picker appears
  3. Set an alarm -> bell icon appears at the session
  4. Rotate the device
  -> Toast appears that permission is missing

app/src/main/java/nerd/tuxmobil/fahrplan/congress/notifications/NotificationHelper.kt
@@ -18,7 +18,8 @@ internal class NotificationHelper(context: Context) : ContextWrapper(context) {
         getNotificationManager()
     }
 
-    val notificationsEnabled = notificationManager.areNotificationsEnabled()
+    val notificationsEnabled: Boolean
+        get() = notificationManager.areNotificationsEnabled()
 
     init {
         createChannels()