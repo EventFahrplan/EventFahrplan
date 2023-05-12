9be3b766db5417cefff70b4b13a61bf221170a72
Tobias Preuss <tobias.preuss@googlemail.com>
2023-05-13 00:29:00 +0200

    Use material v.1.9.0.
    + Changelog: https://github.com/material-components/material-components-android/releases/tag/1.9.0.

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -55,7 +55,7 @@ object Libs {
         const val kotlinCoroutines = "1.6.4"
         const val lifecycle = "2.6.1"
         const val markwon = "4.6.2"
-        const val material = "1.8.0"
+        const val material = "1.9.0"
         const val mockito = "5.3.0"
         const val mockitoKotlin = "4.1.0"
         const val moshi = "1.14.0"
