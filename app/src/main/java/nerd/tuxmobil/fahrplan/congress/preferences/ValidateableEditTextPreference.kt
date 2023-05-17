3390bfa45aa88cbd9608f6f823a86e2296ba6931
Tobias Preuss <tobias.preuss@googlemail.com>
2023-05-17 21:16:20 +0200

    Drop support for devices running Android 4.1-4.4W.2.
    + Use minSdk 21.
    + Affects API 16-18 (Jelly Bean) and 19-20 (KitKat).
    + No device with these versions appeared for more then a year in the
      Google Play statistics.
    + Last minSdk update: 5590389b6daf3990c34f6975e046a72c65f20af3.
    + minSdk 21 is required by Jetpack Compose.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/preferences/ValidateableEditTextPreference.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/preferences/ValidateableEditTextPreference.kt
@@ -1,10 +1,8 @@
 package nerd.tuxmobil.fahrplan.congress.preferences
 
 import android.content.Context
-import android.os.Build
 import android.util.AttributeSet
 import android.widget.EditText
-import androidx.annotation.RequiresApi
 import androidx.core.content.withStyledAttributes
 import androidx.core.widget.doAfterTextChanged
 import androidx.preference.EditTextPreference
@@ -44,7 +42,6 @@ class ValidateableEditTextPreference : StyleableEditTextPreference {
     }
 
     @Suppress("unused")
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) :
             super(context, attrs, defStyleAttr, defStyleRes) {
         applyAttributes(context, attrs)
