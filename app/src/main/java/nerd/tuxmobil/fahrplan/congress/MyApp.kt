392dd8b762af43a21045327179c4398af63be906
Tobias Preuss <tobias.preuss@googlemail.com>
2023-10-25 00:17:13 +0200

    Remove unneeded explicit multidex declaration.
    + Unneeded since: 3390bfa45aa88cbd9608f6f823a86e2296ba6931.
    + Related: 9a256ca2f6429ac94e1e9c6b36ee36d3ae527ac9,
      9c0b19b09fb10731541eefceec7c160aca66118c.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/MyApp.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/MyApp.kt
@@ -1,7 +1,7 @@
 package nerd.tuxmobil.fahrplan.congress
 
+import android.app.Application
 import androidx.annotation.CallSuper
-import androidx.multidex.MultiDexApplication
 import info.metadude.android.eventfahrplan.commons.logging.Logging
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 import nerd.tuxmobil.fahrplan.congress.utils.ConferenceTimeFrame
@@ -10,7 +10,7 @@ import java.util.Calendar
 import java.util.GregorianCalendar
 import java.util.TimeZone
 
-class MyApp : MultiDexApplication() {
+class MyApp : Application() {
 
     enum class TASKS {
         NONE, FETCH, PARSE
