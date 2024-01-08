ccc5963a6a6399d5ab5dcaea7cc85e7feae5c01f
Tobias Preuss <tobias.preuss@googlemail.com>
2024-01-08 15:23:22 +0100

    Clean up ConferenceTimeFrameTest.

--- app/src/test/java/nerd/tuxmobil/fahrplan/congress/utils/ConferenceTimeFrameTest.kt
+++ app/src/test/java/nerd/tuxmobil/fahrplan/congress/utils/ConferenceTimeFrameTest.kt
@@ -9,21 +9,18 @@ import org.junit.Test
 class ConferenceTimeFrameTest {
 
     private companion object {
-        // 2015-12-27T00:00:00+0100, in milliseconds: 1451170800000
-        const val FIRST_DAY_START_TIME = 1451170800000L
+        // 2015-12-27T00:00:00+0100
+        val FIRST_DAY_START_TIME = Moment.ofEpochMilli(1451170800000L)
 
-        // 2015-12-31T00:00:00+0100, in milliseconds: 1451516400000
-        const val LAST_DAY_END_TIME = 1451516400000L
+        // 2015-12-31T00:00:00+0100
+        val LAST_DAY_END_TIME = Moment.ofEpochMilli(1451516400000L)
     }
 
     private lateinit var conference: ConferenceTimeFrame
 
     @Before
     fun setUp() {
-        conference = ConferenceTimeFrame(
-            Moment.ofEpochMilli(FIRST_DAY_START_TIME),
-            Moment.ofEpochMilli(LAST_DAY_END_TIME)
-        )
+        conference = ConferenceTimeFrame(FIRST_DAY_START_TIME, LAST_DAY_END_TIME)
     }
 
     @Test
@@ -34,10 +31,7 @@ class ConferenceTimeFrameTest {
     @Test
     fun `isValid throws an exception for last day followed by first day`() {
         try {
-            ConferenceTimeFrame(
-                Moment.ofEpochMilli(LAST_DAY_END_TIME),
-                Moment.ofEpochMilli(FIRST_DAY_START_TIME)
-            )
+            ConferenceTimeFrame(LAST_DAY_END_TIME, FIRST_DAY_START_TIME)
             fail("Expect an IllegalStateException to be thrown.")
         } catch (e: IllegalStateException) {
             assertThat(e.message).startsWith("Invalid conference time frame:")
@@ -47,10 +41,7 @@ class ConferenceTimeFrameTest {
     @Test
     fun `isValid throws an exception for same day twice`() {
         try {
-            ConferenceTimeFrame(
-                Moment.ofEpochMilli(FIRST_DAY_START_TIME),
-                Moment.ofEpochMilli(FIRST_DAY_START_TIME)
-            )
+            ConferenceTimeFrame(FIRST_DAY_START_TIME, FIRST_DAY_START_TIME)
             fail("Expect an IllegalStateException to be thrown.")
         } catch (e: IllegalStateException) {
             assertThat(e.message).startsWith("Invalid conference time frame:")
@@ -59,7 +50,7 @@ class ConferenceTimeFrameTest {
 
     @Test
     fun `firstDayStart returns the first day`() {
-        assertThat(conference.firstDayStartTime).isEqualTo(Moment.ofEpochMilli(FIRST_DAY_START_TIME))
+        assertThat(conference.firstDayStartTime).isEqualTo(FIRST_DAY_START_TIME)
     }
 
     @Test
@@ -76,47 +67,47 @@ class ConferenceTimeFrameTest {
 
     @Test
     fun `contains returns false if time marks a session at the last day end time`() {
-        assertThat(conference.contains(Moment.ofEpochMilli(LAST_DAY_END_TIME))).isFalse()
+        assertThat(conference.contains(LAST_DAY_END_TIME)).isFalse()
     }
 
     @Test
     fun `endsAtOrBefore returns true if time marks a session at the last day end time`() {
-        assertThat(conference.endsAtOrBefore(Moment.ofEpochMilli(LAST_DAY_END_TIME))).isTrue()
+        assertThat(conference.endsAtOrBefore(LAST_DAY_END_TIME)).isTrue()
     }
 
     @Test
     fun `endsAtOrBefore returns false if time marks a session starting one second before the last day end time`() {
-        assertThat(conference.endsAtOrBefore(Moment.ofEpochMilli(LAST_DAY_END_TIME).minusSeconds(1))).isFalse()
+        assertThat(conference.endsAtOrBefore(LAST_DAY_END_TIME.minusSeconds(1))).isFalse()
     }
 
     @Test
     fun `endsAtOrBefore returns true if time marks a session starting one second after the last day end time`() {
-        assertThat(conference.endsAtOrBefore(Moment.ofEpochMilli(LAST_DAY_END_TIME).plusSeconds(1))).isTrue()
+        assertThat(conference.endsAtOrBefore(LAST_DAY_END_TIME.plusSeconds(1))).isTrue()
     }
 
     @Test
     fun `startsAfter returns false if time marks a session at the first day`() {
-        assertThat(conference.startsAfter(Moment.ofEpochMilli(FIRST_DAY_START_TIME))).isFalse()
+        assertThat(conference.startsAfter(FIRST_DAY_START_TIME)).isFalse()
     }
 
     @Test
     fun `startsAfter returns true if time marks a session starting on second before the first day`() {
-        assertThat(conference.startsAfter(Moment.ofEpochMilli(FIRST_DAY_START_TIME).minusSeconds(1))).isTrue()
+        assertThat(conference.startsAfter(FIRST_DAY_START_TIME.minusSeconds(1))).isTrue()
     }
 
     @Test
     fun `startsAtOrBefore returns true if time marks a session at the first day`() {
-        assertThat(conference.startsAtOrBefore(Moment.ofEpochMilli(FIRST_DAY_START_TIME))).isTrue()
+        assertThat(conference.startsAtOrBefore(FIRST_DAY_START_TIME)).isTrue()
     }
 
     @Test
     fun `startsAtOrBefore returns true if time marks a session starting one second after the first day`() {
-        assertThat(conference.startsAtOrBefore(Moment.ofEpochMilli(FIRST_DAY_START_TIME).plusSeconds(1))).isTrue()
+        assertThat(conference.startsAtOrBefore(FIRST_DAY_START_TIME.plusSeconds(1))).isTrue()
     }
 
     @Test
     fun `startsAtOrBefore returns false if time marks a session starting one second before the first day`() {
-        assertThat(conference.startsAtOrBefore(Moment.ofEpochMilli(FIRST_DAY_START_TIME).minusSeconds(1))).isFalse()
+        assertThat(conference.startsAtOrBefore(FIRST_DAY_START_TIME.minusSeconds(1))).isFalse()
     }
 
 }
