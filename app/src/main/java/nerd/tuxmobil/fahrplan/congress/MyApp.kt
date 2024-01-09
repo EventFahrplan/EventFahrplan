bdce9bf5d7b3ac93ac6e1983fd258cc4152d8c23
Tobias Preuss <tobias.preuss@googlemail.com>
2024-01-09 20:36:28 +0100

    Refactor MyApp#getMilliseconds to use threetenbp.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/MyApp.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/MyApp.kt
@@ -8,9 +8,11 @@ import info.metadude.android.eventfahrplan.commons.temporal.Moment
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 import nerd.tuxmobil.fahrplan.congress.utils.ConferenceTimeFrame
 import org.ligi.tracedroid.TraceDroid
-import java.util.Calendar
-import java.util.GregorianCalendar
-import java.util.TimeZone
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalTime
+import org.threeten.bp.ZoneId
+import org.threeten.bp.ZonedDateTime
+
 
 class MyApp : Application() {
 
@@ -36,12 +38,10 @@ class MyApp : Application() {
 
         @VisibleForTesting
         fun getMilliseconds(timeZoneId: String, year: Int, month: Int, day: Int): Long {
-            val zeroBasedMonth = month - 1
-            val zone = TimeZone.getTimeZone(timeZoneId)
-            return GregorianCalendar(zone).apply {
-                set(year, zeroBasedMonth, day, 0, 0, 0)
-                set(Calendar.MILLISECOND, 0)
-            }.timeInMillis
+            val zoneId = ZoneId.of(timeZoneId)
+            val localDate = LocalDate.of(year, month, day)
+            val zonedDateTime = ZonedDateTime.of(localDate, LocalTime.MIDNIGHT, zoneId)
+            return zonedDateTime.toInstant().toEpochMilli()
         }
 
         val conferenceTimeFrame = ConferenceTimeFrame(
