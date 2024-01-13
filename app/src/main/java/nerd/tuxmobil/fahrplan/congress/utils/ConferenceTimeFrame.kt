928c61aeeab56d10573ee24cb78fdeb01c42bec0
Tobias Preuss <tobias.preuss@googlemail.com>
2024-01-13 23:20:24 +0100

    Document purpose of ConferenceTimeFrame class.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/utils/ConferenceTimeFrame.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/utils/ConferenceTimeFrame.kt
@@ -1,7 +1,16 @@
 package nerd.tuxmobil.fahrplan.congress.utils
 
 import info.metadude.android.eventfahrplan.commons.temporal.Moment
+import nerd.tuxmobil.fahrplan.congress.alarms.AlarmUpdater
+import nerd.tuxmobil.fahrplan.congress.schedule.Conference
 
+/**
+ * Represents the time frame of a conference which is taken into account by the [AlarmUpdater]
+ * to calculate if and when alarms should fire.
+ *
+ * This class should not be used for other purposes.
+ * It is not a replacement for the [Conference] class.
+ */
 // TODO Merge with Conference class?
 class ConferenceTimeFrame(
 
