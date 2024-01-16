EventFahrplan/EventFahrplan

commit c90b63942c0a13120a70cee8c4ef8be0fa29ea80

Pass "speakers" as in data structure around not as formatted string.
+ Formatting should only happen at the view layer.

app/src/main/java/nerd/tuxmobil/fahrplan/congress/sharing/models/SessionExport.kt
@@ -24,19 +24,19 @@ data class SessionExport(
         var date: String? = null
 ) {
     constructor(session: Session) : this(
-            session.sessionId,
-            session.title,
-            session.subtitle,
-            session.day,
-            session.room,
-            session.slug,
-            session.url,
-            session.speakers,
-            session.track,
-            session.lang,
-            session.abstractt,
-            session.description,
-            session.links,
-            session.date
+            sessionId = session.sessionId,
+            title = session.title,
+            subtitle = session.subtitle,
+            day = session.day,
+            room = session.room,
+            slug = session.slug,
+            url = session.url,
+            speakers = session.speakers.joinToString(";"),
+            track = session.track,
+            type = session.lang,
+            lang = session.abstractt,
+            abstract = session.description,
+            description = session.links,
+            links = session.date
     )
 }

app/src/main/java/nerd/tuxmobil/fahrplan/congress/dataconverters/SessionExtensions.kt
@@ -3,6 +3,7 @@
 package nerd.tuxmobil.fahrplan.congress.dataconverters
 
 import info.metadude.android.eventfahrplan.commons.temporal.Moment
+import info.metadude.android.eventfahrplan.network.serialization.FahrplanParser
 import nerd.tuxmobil.fahrplan.congress.models.DateInfo
 import nerd.tuxmobil.fahrplan.congress.models.Session
 import org.threeten.bp.ZoneOffset
@@ -50,7 +51,7 @@ fun Session.toSessionDatabaseModel() = SessionDatabaseModel(
         room = room,
         roomIndex = roomIndex,
         slug = slug,
-        speakers = speakers,
+        speakers = createSpeakersString(speakers),
         startTime = startTime, // minutes since day start
         subtitle = subtitle,
         timeZoneOffset = timeZoneOffset?.totalSeconds, // seconds
@@ -92,7 +93,7 @@ fun SessionDatabaseModel.toSessionAppModel(): Session {
     session.room = room
     session.roomIndex = roomIndex
     session.slug = slug
-    session.speakers = speakers
+    session.speakers = createSpeakersList(speakers)
     session.startTime = startTime // minutes since day start
     session.subtitle = subtitle
     session.timeZoneOffset = timeZoneOffset?.let { ZoneOffset.ofTotalSeconds(it) } // seconds
@@ -136,7 +137,7 @@ fun SessionNetworkModel.toSessionAppModel(): Session {
     session.room = room
     session.roomIndex = roomIndex
     session.slug = slug
-    session.speakers = speakers
+    session.speakers = createSpeakersList(speakers)
     session.startTime = startTime // minutes since day start
     session.subtitle = subtitle
     session.timeZoneOffset = timeZoneOffset?.let { ZoneOffset.ofTotalSeconds(it) } // seconds
@@ -168,7 +169,7 @@ fun Session.sanitize(): Session {
     if (abstractt == description) {
         abstractt = ""
     }
-    if (speakers == subtitle) {
+    if (createSpeakersString(speakers) == subtitle) {
         subtitle = ""
     }
     if (description.isEmpty()) {
@@ -191,4 +192,17 @@ fun Session.sanitize(): Session {
         track = type
     }
     return this
+}
+
+/**
+ * Delimiter which is used in [FahrplanParser] to construct the speakers string.
+ */
+private const val SPEAKERS_DELIMITER = ";"
+
+private fun createSpeakersList(speakers: String): List<String> {
+    return if (speakers.isEmpty()) emptyList() else speakers.split(SPEAKERS_DELIMITER)
+}
+
+private fun createSpeakersString(speakers: List<String>): String {
+    return speakers.joinToString(SPEAKERS_DELIMITER)
 }

app/src/main/java/nerd/tuxmobil/fahrplan/congress/dataconverters/ShiftExtensions.kt
@@ -27,7 +27,7 @@ fun Shift.toSessionAppModel(
     duration = shiftDuration // minutes
     relStartTime = minuteOfDay
     room = virtualRoomName
-    speakers = "-"
+    speakers = emptyList()
     startTime = minuteOfDay  // minutes since day start
     title = name
     subtitle = talkTitle

app/src/main/java/nerd/tuxmobil/fahrplan/congress/models/Session.java
@@ -1,5 +1,6 @@
 package nerd.tuxmobil.fahrplan.congress.models;
 
+import static java.util.Collections.emptyList;
 import static info.metadude.android.eventfahrplan.commons.temporal.Moment.MILLISECONDS_OF_ONE_MINUTE;
 
 import android.content.Context;
@@ -10,6 +11,8 @@ import androidx.annotation.Nullable;
 import androidx.core.util.ObjectsCompat;
 
 import org.threeten.bp.ZoneOffset;
+
+import java.util.List;
 
 import info.metadude.android.eventfahrplan.commons.temporal.Moment;
 import info.metadude.android.eventfahrplan.network.serialization.FahrplanParser;
@@ -44,7 +47,7 @@ public class Session {
     @Deprecated
     public int roomIndex;
 
-    public String speakers;
+    public List<String> speakers;
     public String track;
     public String sessionId;
     public String type;
@@ -87,7 +90,7 @@ public class Session {
         slug = "";
         startTime = 0;
         duration = 0;
-        speakers = "";
+        speakers = emptyList();
         track = "";
         type = "";
         lang = "";
@@ -284,8 +287,7 @@ public class Session {
 
     @NonNull
     public String getFormattedSpeakers() {
-        // language=regex
-        return speakers == null ? "" : speakers.replaceAll(";", ", ");
+        return speakers == null ? "" : TextUtils.join(", ", speakers);
     }
 
     public String getFormattedTrackLanguageText() {

app/src/test/java/nerd/tuxmobil/fahrplan/congress/calendar/CalendarDescriptionComposerTest.kt
@@ -32,7 +32,7 @@ class CalendarDescriptionComposerTest {
 
     @Test
     fun `getCalendarDescription returns speakers and session online`() {
-        val session = createSession(speakers = "Ada Lovelace;Albert Einstein")
+        val session = createSession(speakers = listOf("Ada Lovelace", "Albert Einstein"))
         assertThat(createComposer().getCalendarDescription(session)).isEqualTo("""
             Ada Lovelace, Albert Einstein
 
@@ -85,7 +85,7 @@ class CalendarDescriptionComposerTest {
     fun `getCalendarDescription returns full session description`() {
         val session = createSession(
             subtitle = "Lorem ipsum dolor",
-            speakers = "Ada Lovelace;Albert Einstein",
+            speakers = listOf("Ada Lovelace", "Albert Einstein"),
             abstract = "Lorem ipsum dolor sit amet.",
             description = "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             links = "[Engelsystem](https://engelsystem.de)"
@@ -107,7 +107,7 @@ class CalendarDescriptionComposerTest {
 
     @Test
     fun `getCalendarDescription returns session online with uninitialized session`() {
-        val session = createSession(subtitle = null, speakers = null, abstract = null, description = null, links = null)
+        val session = createSession(subtitle = null, speakers = emptyList(), abstract = null, description = null, links = null)
         assertThat(createComposer().getCalendarDescription(session)).isEqualTo("""
             Session online: https://events.ccc.de/congress/2021/Fahrplan/events/2342.html
             """.trimIndent())
@@ -127,7 +127,7 @@ class CalendarDescriptionComposerTest {
 
     private fun createSession(
         subtitle: String? = "",
-        speakers: String? = "",
+        speakers: List<String> = emptyList(),
         abstract: String? = "",
         description: String? = "",
         links: String? = ""

app/src/test/java/nerd/tuxmobil/fahrplan/congress/calendar/CalendarSharingTest.kt
@@ -62,7 +62,7 @@ class CalendarSharingTest {
     private fun createSession() = Session("2342").apply {
         title = "Title"
         subtitle = "Subtitle"
-        speakers = "Speakers"
+        speakers = listOf("Speakers")
         abstractt = "Abstract"
         description = "Description"
         links = "Links"

app/src/test/java/nerd/tuxmobil/fahrplan/congress/dataconverters/SessionExtensionsTest.kt
@@ -79,7 +79,7 @@ class SessionExtensionsTest {
                 recordingOptOut = RECORDING_OPT_OUT_ON,
                 room = "Simulacron-3",
                 roomIndex = 17,
-                speakers = "John Doe; Noah Doe",
+                speakers = "John Doe;Noah Doe",
                 startTime = 1036,
                 slug = "lorem",
                 subtitle = "My subtitle",
@@ -118,7 +118,7 @@ class SessionExtensionsTest {
             recordingOptOut = RECORDING_OPT_OUT_ON
             room = "Simulacron-3"
             roomIndex = 17
-            speakers = "John Doe; Noah Doe"
+            speakers = listOf("John Doe", "Noah Doe")
             startTime = 1036
             slug = "lorem"
             subtitle = "My subtitle"
@@ -282,11 +282,11 @@ class SessionExtensionsTest {
     @Test
     fun sanitizeWithSameSpeakersAndSubtitle() {
         val session = Session("").apply {
-            speakers = "Luke Skywalker"
+            speakers = listOf("Luke Skywalker")
             subtitle = "Luke Skywalker"
         }.sanitize()
         val expected = Session("").apply {
-            speakers = "Luke Skywalker"
+            speakers = listOf("Luke Skywalker")
             subtitle = ""
         }
         assertThat(session).isEqualTo(expected)
@@ -295,11 +295,11 @@ class SessionExtensionsTest {
     @Test
     fun sanitizeWithDifferentSpeakersAndAbstract() {
         val session = Session("").apply {
-            speakers = "Darth Vader"
+            speakers = listOf("Darth Vader")
             subtitle = "Lorem ipsum"
         }.sanitize()
         val expected = Session("").apply {
-            speakers = "Darth Vader"
+            speakers = listOf("Darth Vader")
             subtitle = "Lorem ipsum"
         }
         assertThat(session).isEqualTo(expected)

app/src/test/java/nerd/tuxmobil/fahrplan/congress/details/SessionDetailsViewModelTest.kt
@@ -85,7 +85,7 @@ class SessionDetailsViewModelTest {
             dateUTC = 100
             title = "Session title"
             subtitle = "Session subtitle"
-            speakers = "Jane Doe;John Doe"
+            speakers = listOf("Jane Doe", "John Doe")
             room = "Main hall"
             abstractt = "Session abstract"
             description = "Session description"
@@ -153,7 +153,7 @@ class SessionDetailsViewModelTest {
             dateUTC = 0
             title = ""
             subtitle = ""
-            speakers = ""
+            speakers = emptyList()
             room = ""
             abstractt = ""
             description = ""

commit 7b027edd5674a21594c214d6a704b30a902cab55

Correct the JSON output for the share action
- Shift the fields to the correct values
- Add missing field `date` as `starts_at`
- Fixes #596
- Broken since: c90b63942c0a13120a70cee8c4ef8be0fa29ea80

app/src/main/java/nerd/tuxmobil/fahrplan/congress/sharing/models/SessionExport.kt
@@ -2,6 +2,7 @@ package nerd.tuxmobil.fahrplan.congress.sharing.models
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import info.metadude.android.eventfahrplan.commons.temporal.Moment
 import nerd.tuxmobil.fahrplan.congress.models.Session
 
 @JsonClass(generateAdapter = true)
@@ -21,7 +22,8 @@ data class SessionExport(
         var abstract: String,
         var description: String = "",
         var links: String? = null,
-        var date: String? = null
+        @Json(name = "starts_at")
+        var startsAt: String? = null,
 ) {
     constructor(session: Session) : this(
             sessionId = session.sessionId,
@@ -33,10 +35,11 @@ data class SessionExport(
             url = session.url,
             speakers = session.speakers.joinToString(";"),
             track = session.track,
-            type = session.lang,
-            lang = session.abstractt,
-            abstract = session.description,
-            description = session.links,
-            links = session.date
+            type = session.type,
+            lang = session.lang,
+            abstract = session.abstractt,
+            description = session.description,
+            links = session.links,
+            startsAt = Moment.ofEpochMilli(session.dateUTC).toString()
     )
 }

app/src/test/java/nerd/tuxmobil/fahrplan/congress/sharing/JsonSessionFormatTest.kt
@@ -28,16 +28,16 @@ class JsonSessionFormatTest {
         const val EXPECTED_JSON_SINGLE = "{\"lectures\":[{\"lecture_id\":\"session1\"," +
                 "\"title\":\"\",\"subtitle\":\"\",\"day\":0,\"room\":\"\",\"slug\":\"\",\"url\":\"\"," +
                 "\"speakers\":\"\",\"track\":\"\",\"type\":\"\",\"lang\":\"\",\"abstract\":\"\"," +
-                "\"description\":\"\",\"links\":\"\"}]}"
+                "\"description\":\"\",\"links\":\"\",\"starts_at\":\"1970-01-01T00:00:00Z\"}]}"
         const val EXPECTED_JSON_LIST = "{\"lectures\":[{\"lecture_id\":\"session1\"," +
                 "\"title\":\"\",\"subtitle\":\"\",\"day\":0,\"room\":\"\",\"slug\":\"\",\"url\":\"\"," +
                 "\"speakers\":\"\",\"track\":\"\",\"type\":\"\",\"lang\":\"\",\"abstract\":\"\"," +
-                "\"description\":\"\",\"links\":\"\"},{\"lecture_id\":\"session2\",\"title\":\"\"," +
+                "\"description\":\"\",\"links\":\"\",\"starts_at\":\"1970-01-01T00:00:00Z\"},{\"lecture_id\":\"session2\",\"title\":\"\"," +
                 "\"subtitle\":\"\",\"day\":0,\"room\":\"\",\"slug\":\"\",\"url\":\"\",\"speakers\":\"\"," +
                 "\"track\":\"\",\"type\":\"\",\"lang\":\"\",\"abstract\":\"\",\"description\":\"\"," +
-                "\"links\":\"\"},{\"lecture_id\":\"session3\",\"title\":\"\",\"subtitle\":\"\"," +
+                "\"links\":\"\",\"starts_at\":\"1970-01-01T00:00:00Z\"},{\"lecture_id\":\"session3\",\"title\":\"\",\"subtitle\":\"\"," +
                 "\"day\":0,\"room\":\"\",\"slug\":\"\",\"url\":\"\",\"speakers\":\"\",\"track\":\"\"," +
-                "\"type\":\"\",\"lang\":\"\",\"abstract\":\"\",\"description\":\"\",\"links\":\"\"}]}"
+                "\"type\":\"\",\"lang\":\"\",\"abstract\":\"\",\"description\":\"\",\"links\":\"\",\"starts_at\":\"1970-01-01T00:00:00Z\"}]}"
 
     }
 