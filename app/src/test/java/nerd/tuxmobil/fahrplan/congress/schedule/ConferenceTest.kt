EventFahrplan/EventFahrplan

commit 28e555a1c306d5722cbddd36c0075c3bd37d962b

Migrate Conference class to use Moment for its fields.
+ Update implementation of ScrollAmountCalculator#calculateScrollAmount(Conference, Session, Int),
  fix wrong time zone offset behavior.

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/Conference.kt
@@ -1,6 +1,5 @@
 package nerd.tuxmobil.fahrplan.congress.schedule
 
-import androidx.annotation.VisibleForTesting
 import info.metadude.android.eventfahrplan.commons.temporal.Moment
 import info.metadude.android.eventfahrplan.commons.temporal.Moment.Companion.MINUTES_OF_ONE_DAY
 import nerd.tuxmobil.fahrplan.congress.models.Session
@@ -23,33 +22,20 @@ import org.threeten.bp.ZoneOffset
 // TODO Use Moment class, merge with ConferenceTimeFrame class?
 data class Conference(
 
-        var firstSessionStartsAt: Int = 0,
-        var lastSessionEndsAt: Int = 0,
-        var timeZoneOffset: ZoneOffset? = null
+        val firstSessionStartsAt: Moment,
+        val lastSessionEndsAt: Moment,
+        var timeZoneOffset: ZoneOffset? = null,
+        val spansMultipleDays: Boolean
 
 ) {
-
-    /**
-     * Calculates the [firstSessionStartsAt] and [lastSessionEndsAt] time stamps for the
-     * given sorted sessions. Further, the [timeZoneOffset] is derived from the first session.
-     *
-     * @param sessions     Sorted list of sessions.
-     */
-    @Deprecated("Make ofSessions public and use it and make Conference immutable as soon as Moment is used.")
-    fun calculateTimeFrame(sessions: List<Session>) {
-        val conference = ofSessions(sessions)
-        firstSessionStartsAt = conference.firstSessionStartsAt
-        lastSessionEndsAt = conference.lastSessionEndsAt
-        timeZoneOffset = conference.timeZoneOffset
-    }
 
     companion object {
 
         /**
          * Creates a [Conference] from the given chronologically sorted [sessions].
          */
-        @VisibleForTesting
-        internal fun ofSessions(sessions: List<Session>): Conference {
+        @JvmStatic
+        fun ofSessions(sessions: List<Session>): Conference {
             require(sessions.isNotEmpty()) { "Empty list of sessions." }
             val firstSession = sessions.first()
             val first = Moment.ofEpochMilli(firstSession.dateUTC)
@@ -60,10 +46,12 @@ data class Conference(
             val minutesToAdd = if (first.monthDay == last.monthDay) 0 else MINUTES_OF_ONE_DAY
             // Here we are assuming all sessions have the same time zone offset.
             val timeZoneOffset = firstSession.timeZoneOffset
+            val veryLast = last.plusMinutes(minutesToAdd.toLong())
             return Conference(
-                firstSessionStartsAt = first.minuteOfDay,
-                lastSessionEndsAt = last.minuteOfDay + minutesToAdd,
-                timeZoneOffset = timeZoneOffset
+                firstSessionStartsAt = first,
+                lastSessionEndsAt = veryLast,
+                timeZoneOffset = timeZoneOffset,
+                spansMultipleDays = minutesToAdd > 0
             )
         }
 

app/src/test/java/nerd/tuxmobil/fahrplan/congress/schedule/ConferenceTest.kt
@@ -9,14 +9,7 @@ import org.threeten.bp.ZoneOffset
 class ConferenceTest {
 
     @Test
-    fun `default values`() {
-        val conference = Conference()
-        assertThat(conference.firstSessionStartsAt).isEqualTo(0)
-        assertThat(conference.lastSessionEndsAt).isEqualTo(0)
-    }
-
-    @Test
-    fun `calculateTimeFrame throws exception if empty list is passed`() {
+    fun `ofSessions throws exception if empty list is passed`() {
         try {
             createConference(*emptyList<Session>().toTypedArray())
         } catch (e: IllegalArgumentException) {
@@ -25,39 +18,40 @@ class ConferenceTest {
     }
 
     @Test
-    fun `calculateTimeFrame with frab data spanning multiple days`() {
+    fun `ofSessions with frab data spanning multiple days`() {
         val opening = createSession("Opening", duration = 30, dateUtc = 1536332400000L, ZoneOffset.of("+02:00")) // 2018-09-07T17:00:00+02:00
         val closing = createSession("Closing", duration = 30, dateUtc = 1536504300000L, ZoneOffset.of("+02:00")) // 2018-09-09T16:45:00+02:00
-        val (firstSessionStartsAt, lastSessionEndsAt, timeZoneOffset) = createConference(opening, closing)
-        assertThat(firstSessionStartsAt).isEqualTo(17 * 60 - 2 * 60) // 17:00h -2h zone offset = 15:00h
-        assertThat(lastSessionEndsAt).isEqualTo(17 * 60 + 15 - 2 * 60 + MINUTES_OF_ONE_DAY) // -> 17:15 -2h zone offset + day switch
+        val (firstSessionStartsAt, lastSessionEndsAt, timeZoneOffset, spansMultipleDays) = createConference(opening, closing)
+        val firstSessionStartsAtMinutes = firstSessionStartsAt.minuteOfDay
+        val minutesToAdd = if (spansMultipleDays) MINUTES_OF_ONE_DAY else 0
+        val lastSessionEndsAtMinutes = lastSessionEndsAt.minuteOfDay + minutesToAdd
+        assertThat(firstSessionStartsAtMinutes).isEqualTo(17 * 60 - 2 * 60) // 17:00h -2h zone offset = 15:00h
+        assertThat(lastSessionEndsAtMinutes).isEqualTo(17 * 60 + 15 - 2 * 60 + MINUTES_OF_ONE_DAY) // -> 17:15 -2h zone offset + day switch
         assertThat(timeZoneOffset).isEqualTo(ZoneOffset.of("+02:00"))
     }
 
     @Test
-    fun `calculateTimeFrame with frab data spanning a single day`() {
+    fun `ofSessions with frab data spanning a single day`() {
         val opening = createSession("Opening", duration = 30, dateUtc = 1536332400000L, ZoneOffset.of("+02:00")) // 2018-09-07T17:00:00+02:00
         val closing = createSession("Closing", duration = 30, dateUtc = 1536336000000L, ZoneOffset.of("+02:00")) // 2018-09-07T18:00:00+02:00
-        val (firstSessionStartsAt, lastSessionEndsAt, timeZoneOffset) = createConference(opening, closing)
-        assertThat(firstSessionStartsAt).isEqualTo(17 * 60 - 2 * 60) // 17:00h -2h zone offset = 15:00h
-        assertThat(lastSessionEndsAt).isEqualTo(18 * 60 + 30 - 2 * 60) // -> 18:30 -2h zone offset
+        val (firstSessionStartsAt, lastSessionEndsAt, timeZoneOffset, _) = createConference(opening, closing)
+        assertThat(firstSessionStartsAt.minuteOfDay).isEqualTo(17 * 60 - 2 * 60) // 17:00h -2h zone offset = 15:00h
+        assertThat(lastSessionEndsAt.minuteOfDay).isEqualTo(18 * 60 + 30 - 2 * 60) // -> 18:30 -2h zone offset
         assertThat(timeZoneOffset).isEqualTo(ZoneOffset.of("+02:00"))
     }
 
     @Test
-    fun `calculateTimeFrame with frab data spanning a single day in non-chronological order`() {
+    fun `ofSessions with frab data spanning a single day in non-chronological order`() {
         val opening = createSession("Opening", duration = 30, dateUtc = 1536328800000L, ZoneOffset.of("+02:00")) // 2018-09-07T16:00:00+02:00
         val middle = createSession("Middle", duration = 20, dateUtc = 1536332400000L, ZoneOffset.of("+02:00")) // 2018-09-07T17:00:00+02:00
         val closing = createSession("Closing", duration = 30, dateUtc = 1536336000000L, ZoneOffset.of("+02:00")) // 2018-09-07T18:00:00+02:00
-        val (firstSessionStartsAt, lastSessionEndsAt, timeZoneOffset) = createConference(opening, closing, middle)
-        assertThat(firstSessionStartsAt).isEqualTo(16 * 60 - 2 * 60) // 16:00h -2h zone offset = 14:00h
-        assertThat(lastSessionEndsAt).isEqualTo(18 * 60 + 30 - 2 * 60) // -> 18:30 -2h zone offset
+        val (firstSessionStartsAt, lastSessionEndsAt, timeZoneOffset, _) = createConference(opening, closing, middle)
+        assertThat(firstSessionStartsAt.minuteOfDay).isEqualTo(16 * 60 - 2 * 60) // 16:00h -2h zone offset = 14:00h
+        assertThat(lastSessionEndsAt.minuteOfDay).isEqualTo(18 * 60 + 30 - 2 * 60) // -> 18:30 -2h zone offset
         assertThat(timeZoneOffset).isEqualTo(ZoneOffset.of("+02:00"))
     }
 
-    private fun createConference(vararg sessions: Session) = Conference().apply {
-        calculateTimeFrame(listOf(*sessions))
-    }
+    private fun createConference(vararg sessions: Session) = Conference.ofSessions(sessions.toList())
 
     private fun createSession(sessionId: String, duration: Int, dateUtc: Long, timeZoneOffset: ZoneOffset) = Session(sessionId).apply {
         this.dateUTC = dateUtc

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/FahrplanFragment.java
@@ -36,7 +36,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.ligi.tracedroid.logging.Log;
-import org.threeten.bp.Duration;
 import org.threeten.bp.ZoneId;
 
 import java.util.Arrays;
@@ -90,7 +89,6 @@ public class FahrplanFragment extends Fragment implements SessionViewEventsHandl
     private static final int CONTEXT_MENU_ITEM_ID_SHARE_TEXT = 5;
     private static final int CONTEXT_MENU_ITEM_ID_SHARE_JSON = 6;
 
-    public static final int ONE_DAY = (int) Duration.ofDays(1).toMinutes();
     public static final int FIFTEEN_MINUTES = 15;
     public static final int BOX_HEIGHT_MULTIPLIER = 3;
 
@@ -98,7 +96,7 @@ public class FahrplanFragment extends Fragment implements SessionViewEventsHandl
 
     private LayoutInflater inflater;
 
-    private final Conference conference = new Conference();
+    private Conference conference;
 
     private AppRepository appRepository;
 
@@ -261,7 +259,9 @@ public class FahrplanFragment extends Fragment implements SessionViewEventsHandl
             }
             intent.removeExtra(BundleKeys.BUNDLE_KEY_SESSION_ALARM_SESSION_ID); // jump to given sessionId only once
         }
-        fillTimes();
+        if (conference != null) {
+            fillTimes();
+        }
 
         appRepository.setOnSessionsChangeListener(onSessionsChangeListener);
     }
@@ -287,7 +287,7 @@ public class FahrplanFragment extends Fragment implements SessionViewEventsHandl
 
         if (!sessionsOfDay.isEmpty()) {
             // TODO: Move this to AppRepository and include the result in ScheduleData
-            conference.calculateTimeFrame(sessionsOfDay);
+            conference = Conference.ofSessions(sessionsOfDay);
             MyApp.LogDebug(LOG_TAG, "Conference = " + conference);
         }
 

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/LayoutCalculator.kt
@@ -28,7 +28,7 @@ data class LayoutCalculator @JvmOverloads constructor(
 
     fun calculateLayoutParams(roomData: RoomData, conference: Conference): Map<Session, LinearLayout.LayoutParams> {
         val sessions = roomData.sessions
-        var previousSessionEndsAt: Int = conference.firstSessionStartsAt
+        var previousSessionEndsAt: Int = conference.firstSessionStartsAt.minuteOfDay
         var startTime: Int
         var margin: Int
         var previousSession: Session? = null

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/ScrollAmountCalculator.kt
@@ -2,12 +2,13 @@ package nerd.tuxmobil.fahrplan.congress.schedule
 
 import info.metadude.android.eventfahrplan.commons.logging.Logging
 import info.metadude.android.eventfahrplan.commons.temporal.Moment
+import info.metadude.android.eventfahrplan.commons.temporal.Moment.Companion.MINUTES_OF_ONE_DAY
+import nerd.tuxmobil.fahrplan.congress.dataconverters.toStartsAtMoment
 import nerd.tuxmobil.fahrplan.congress.models.DateInfos
 import nerd.tuxmobil.fahrplan.congress.models.ScheduleData
 import nerd.tuxmobil.fahrplan.congress.models.Session
 import nerd.tuxmobil.fahrplan.congress.schedule.FahrplanFragment.BOX_HEIGHT_MULTIPLIER
 import nerd.tuxmobil.fahrplan.congress.schedule.FahrplanFragment.FIFTEEN_MINUTES
-import nerd.tuxmobil.fahrplan.congress.schedule.FahrplanFragment.ONE_DAY
 import nerd.tuxmobil.fahrplan.congress.schedule.TimeSegment.Companion.TIME_GRID_MINIMUM_SEGMENT_HEIGHT
 
 /**
@@ -36,27 +37,26 @@ internal class ScrollAmountCalculator(
         boxHeight: Int,
         columnIndex: Int
     ): Int {
-        var time = conference.firstSessionStartsAt
-        var printTime = time
+        var sessionStartsAt = conference.firstSessionStartsAt
+        var sessionStartsAtMinutes = sessionStartsAt.minuteOfDay
         var scrollAmount = 0
 
-        val hasStarted = nowMoment.minuteOfDay < conference.firstSessionStartsAt
+        val hasStarted = nowMoment.minuteOfDay < sessionStartsAtMinutes
         if (!(hasStarted && dateInfos.sameDay(nowMoment, currentDayIndex))) {
             var timeSegment: TimeSegment
-            while (time < conference.lastSessionEndsAt) {
-                timeSegment = TimeSegment.ofMinutesOfTheDay(printTime)
-                val isMatched = timeSegment.isMatched(nowMoment, FIFTEEN_MINUTES)
-                scrollAmount += if (isMatched) {
+            val minutesToAdd = if (conference.spansMultipleDays) MINUTES_OF_ONE_DAY else 0
+            val lastSessionEndsAtMinutes = conference.lastSessionEndsAt.minuteOfDay + minutesToAdd
+            while (sessionStartsAtMinutes < lastSessionEndsAtMinutes) {
+                timeSegment = TimeSegment.ofMoment(sessionStartsAt)
+                scrollAmount += if (timeSegment.isMatched(nowMoment, FIFTEEN_MINUTES)) {
                     break
                 } else {
                     boxHeight * BOX_HEIGHT_MULTIPLIER
                 }
-                time += FIFTEEN_MINUTES
-                printTime = time
-                if (printTime >= ONE_DAY) {
-                    printTime -= ONE_DAY
-                }
+                sessionStartsAt = sessionStartsAt.plusMinutes(FIFTEEN_MINUTES.toLong())
+                sessionStartsAtMinutes += FIFTEEN_MINUTES
             }
+            var time = sessionStartsAt.minuteOfDay
             val roomDataList = scheduleData.roomDataList
             if (columnIndex >= 0 && columnIndex < roomDataList.size) {
                 val roomData = roomDataList[columnIndex]
@@ -73,15 +73,20 @@ internal class ScrollAmountCalculator(
         return scrollAmount
     }
 
+    /**
+     * Returns the scroll amount for the given [session].
+     *
+     * First the duration between the given session and the first session of the day is calculated
+     * at minutes precision. Then based on this duration the device specific scroll amount is
+     * calculated considering the [TIME_GRID_MINIMUM_SEGMENT_HEIGHT] and the given [boxHeight].
+     *
+     * This calculation is independent of the time zone offset of the device nor conference.
+     */
     fun calculateScrollAmount(conference: Conference, session: Session, boxHeight: Int): Int {
-        // TODO Replace with proper Moment based implementation as soon as possible. See code review in https://github.com/EventFahrplan/EventFahrplan/pull/347
-        val startsAtMinuteUtc = session.relStartTime - conference.firstSessionStartsAt
-        val systemOffsetMinutes = Moment.getSystemOffsetMinutes()
-        // Translate start time minutes from UTC to system time zone rendered to the user.
-        val startsAtMinuteSystem = startsAtMinuteUtc - systemOffsetMinutes
-        val pos = startsAtMinuteSystem / TIME_GRID_MINIMUM_SEGMENT_HEIGHT * boxHeight
-        logging.e(javaClass.simpleName, "relStartTime=${session.relStartTime}, height = $boxHeight, pos = $pos")
-        return pos
+        val sessionStartsAt = session.toStartsAtMoment()
+        val firstSessionStartsAt = conference.firstSessionStartsAt
+        val minutes = firstSessionStartsAt.minutesUntil(sessionStartsAt).toInt()
+        return minutes / TIME_GRID_MINIMUM_SEGMENT_HEIGHT * boxHeight
     }
 
 }

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/TimeSegment.kt
@@ -27,12 +27,6 @@ internal class TimeSegment private constructor(
          */
         const val TIME_GRID_MINIMUM_SEGMENT_HEIGHT = 5
 
-        @Deprecated("Use TimeSegment.ofMoment", replaceWith = ReplaceWith("TimeSegment.ofMoment()"))
-        @JvmStatic
-        fun ofMinutesOfTheDay(minutesOfTheDay: Int): TimeSegment {
-            return ofMoment(Moment.now().startOfDay().plusMinutes(minutesOfTheDay.toLong()))
-        }
-
         @JvmStatic
         fun ofMoment(moment: Moment) = TimeSegment(moment)
 

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/TimeTextViewParameter.kt
@@ -2,10 +2,10 @@ package nerd.tuxmobil.fahrplan.congress.schedule
 
 import androidx.annotation.LayoutRes
 import info.metadude.android.eventfahrplan.commons.temporal.Moment
+import info.metadude.android.eventfahrplan.commons.temporal.Moment.Companion.MINUTES_OF_ONE_DAY
 import nerd.tuxmobil.fahrplan.congress.R
 import nerd.tuxmobil.fahrplan.congress.schedule.FahrplanFragment.BOX_HEIGHT_MULTIPLIER
 import nerd.tuxmobil.fahrplan.congress.schedule.FahrplanFragment.FIFTEEN_MINUTES
-import nerd.tuxmobil.fahrplan.congress.schedule.FahrplanFragment.ONE_DAY
 
 /**
  * Parameters to be used to inflate and configure a time text view.
@@ -34,15 +34,16 @@ internal data class TimeTextViewParameter private constructor(
                 normalizedBoxHeight: Int
         ): List<TimeTextViewParameter> {
             val parameters = mutableListOf<TimeTextViewParameter>()
-            var time = conference.firstSessionStartsAt
-            var printTime = time
+            var sessionStartsAt = conference.firstSessionStartsAt
+            var sessionStartsAtMinutes = sessionStartsAt.minuteOfDay
             val timeTextViewHeight = BOX_HEIGHT_MULTIPLIER * normalizedBoxHeight
             var timeSegment: TimeSegment
-            while (time < conference.lastSessionEndsAt) {
-                timeSegment = TimeSegment.ofMinutesOfTheDay(printTime)
-                var timeTextLayout: Int
+            val minutesToAdd = if (conference.spansMultipleDays) MINUTES_OF_ONE_DAY else 0
+            val lastSessionEndsAtMinutes = conference.lastSessionEndsAt.minuteOfDay + minutesToAdd
+            while (sessionStartsAtMinutes < lastSessionEndsAtMinutes) {
+                timeSegment = TimeSegment.ofMoment(sessionStartsAt)
                 val isToday = nowMoment.monthDay - firstDayStartDay == dayIndex - 1
-                timeTextLayout = if (isToday && timeSegment.isMatched(nowMoment, FIFTEEN_MINUTES)) {
+                val timeTextLayout = if (isToday && timeSegment.isMatched(nowMoment, FIFTEEN_MINUTES)) {
                     R.layout.time_layout_now
                 } else {
                     R.layout.time_layout
@@ -50,11 +51,8 @@ internal data class TimeTextViewParameter private constructor(
                 val titleText = timeSegment.getFormattedText(conference.timeZoneOffset)
                 val parameter = TimeTextViewParameter(timeTextLayout, timeTextViewHeight, titleText)
                 parameters.add(parameter)
-                time += FIFTEEN_MINUTES
-                printTime = time
-                if (printTime >= ONE_DAY) {
-                    printTime -= ONE_DAY
-                }
+                sessionStartsAt = sessionStartsAt.plusMinutes(FIFTEEN_MINUTES.toLong())
+                sessionStartsAtMinutes += FIFTEEN_MINUTES
             }
             return parameters
         }

commons/src/main/java/info/metadude/android/eventfahrplan/commons/temporal/Moment.kt
@@ -1,10 +1,10 @@
 package info.metadude.android.eventfahrplan.commons.temporal
 
+import org.threeten.bp.Duration
 import org.threeten.bp.Instant
 import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.LocalTime
-import org.threeten.bp.ZoneId
 import org.threeten.bp.ZoneOffset
 import org.threeten.bp.ZonedDateTime
 import org.threeten.bp.temporal.ChronoField
@@ -100,6 +100,13 @@ class Moment private constructor(private val time: Instant) {
      */
     fun isBefore(moment: Moment): Boolean = time.toEpochMilli() < moment.toMilliseconds()
 
+    /**
+     * Returns the duration in minutes between this and the given [moment].
+     */
+    fun minutesUntil(moment: Moment): Long {
+        return Duration.between(time, moment.time).toMinutes()
+    }
+
     override fun equals(other: Any?): Boolean {
         return time == (other as? Moment)?.time
     }
@@ -140,17 +147,6 @@ class Moment private constructor(private val time: Instant) {
          */
         @JvmStatic
         fun now() = Moment(Instant.now())
-
-        /**
-         * Returns the amount of minutes between the UTC and the system default time zone.
-         */
-        @JvmStatic
-        fun getSystemOffsetMinutes(): Int {
-            val dateTime = LocalDateTime.now()
-            val utcDateTime = ZonedDateTime.of(dateTime, ZoneId.of("UTC"))
-            val systemDateTime = utcDateTime.withZoneSameInstant(ZoneId.systemDefault())
-            return systemDateTime.offset.totalSeconds / SECONDS_OF_ONE_MINUTE
-        }
 
         /**
          * Creates a time zone neutral [Moment] instance from given [milliseconds].

commit e1efd31793d0655012b6407ed4104f7da6a3c340

Fix Conference#lastSessionEndsAt incorrectly returning of by one day.
+ Broken since: 28e555a1c306d5722cbddd36c0075c3bd37d962b WTF!
+ This error was introduced with the refactoring from minutes to Moment.
  The last session end date/time was incorrectly calculated to be one day
  later than it should have been.

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/Conference.kt
@@ -1,7 +1,6 @@
 package nerd.tuxmobil.fahrplan.congress.schedule
 
 import info.metadude.android.eventfahrplan.commons.temporal.Moment
-import info.metadude.android.eventfahrplan.commons.temporal.Moment.Companion.MINUTES_OF_ONE_DAY
 import nerd.tuxmobil.fahrplan.congress.models.Session
 import org.threeten.bp.ZoneOffset
 
@@ -43,14 +42,12 @@ data class Conference(
             // TODO Replace with firstSession.toStartsAtMoment() once Session#relStartTime is no longer used.
             val endingLatest = sessions.endingLatest()
             val last = endingLatest.endsAt
-            val minutesToAdd = if (first.monthDay == last.monthDay) 0 else MINUTES_OF_ONE_DAY
             // Here we are assuming all sessions have the same time zone offset.
             val timeZoneOffset = firstSession.timeZoneOffset
-            val veryLast = last.plusMinutes(minutesToAdd.toLong())
             return Conference(
-                timeFrame = first..veryLast,
+                timeFrame = first..last,
                 timeZoneOffset = timeZoneOffset,
-                spansMultipleDays = minutesToAdd > 0
+                spansMultipleDays = first.monthDay != last.monthDay
             )
         }
 

app/src/test/java/nerd/tuxmobil/fahrplan/congress/schedule/ConferenceTest.kt
@@ -1,6 +1,7 @@
 package nerd.tuxmobil.fahrplan.congress.schedule
 
 import com.google.common.truth.Truth.assertThat
+import info.metadude.android.eventfahrplan.commons.temporal.Moment
 import info.metadude.android.eventfahrplan.commons.temporal.Moment.Companion.MINUTES_OF_ONE_DAY
 import nerd.tuxmobil.fahrplan.congress.models.Session
 import org.junit.Assert.fail
@@ -16,6 +17,18 @@ class ConferenceTest {
             fail("Expect an IllegalArgumentException to be thrown.")
         } catch (e: IllegalArgumentException) {
             assertThat(e.message).isEqualTo("Empty list of sessions.")
+        }
+    }
+
+    @Test
+    fun `ofSession returns time range for one virtual conference day spanning two natural days`() {
+        val opening = createSession("Opening", duration = 30, dateUtc = 1703671200000, ZoneOffset.of("+00:00")) // 2023-12-27T10:00:00+00:00
+        val dancing = createSession("Dancing", duration = 60, dateUtc = 1703736000000, ZoneOffset.of("+00:00")) // 2023-12-28T04:00:00+00:00
+        with(createConference(opening, dancing)) {
+            assertThat(firstSessionStartsAt).isEqualTo(Moment.ofEpochMilli(1703671200000)) // 2023-12-27T10:00:00+00:00
+            assertThat(lastSessionEndsAt).isEqualTo(Moment.ofEpochMilli(1703739600000)) // 2023-12-28T05:00:00+00:00
+            assertThat(timeZoneOffset).isEqualTo(ZoneOffset.of("+00:00"))
+            assertThat(spansMultipleDays).isTrue()
         }
     }
 