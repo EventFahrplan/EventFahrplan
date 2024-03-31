57106c759eb7adf60977e000e8fe8ff0c06822ee
Tobias Preuss <tobias.preuss@googlemail.com>
2024-04-01 00:02:11 +0200

    Apply automatic formatting.
    + Manually indent raw string blocks.

--- app/src/test/java/nerd/tuxmobil/fahrplan/congress/sharing/SimpleSessionFormatTest.kt
+++ app/src/test/java/nerd/tuxmobil/fahrplan/congress/sharing/SimpleSessionFormatTest.kt
@@ -77,57 +77,92 @@ class SimpleSessionFormatTest {
 
     @Test
     fun `format returns formatted multiline text for a session without time zone name`() {
-        assertThat(SimpleSessionFormat().format(session1, NO_TIME_ZONE_ID, NO_SOCIAL_MEDIA_HASHTAGS_HANDLES)).isEqualTo(
-                """
-                A talk which changes your life
-                Freitag, 27. Dezember 2019, 11:00 GMT+01:00, Yellow pavilion
+        assertThat(
+            SimpleSessionFormat().format(
+                session1,
+                NO_TIME_ZONE_ID,
+                NO_SOCIAL_MEDIA_HASHTAGS_HANDLES
+            )
+        ).isEqualTo(
+            """
+            A talk which changes your life
+            Freitag, 27. Dezember 2019, 11:00 GMT+01:00, Yellow pavilion
 
-                $expectedSession1Url
-                """.trimIndent())
+            $expectedSession1Url
+            """.trimIndent()
+        )
     }
 
     @Test
     fun `format returns formatted multiline text for a session with time zone name`() {
-        assertThat(SimpleSessionFormat().format(session1, TIME_ZONE_EUROPE_BERLIN, NO_SOCIAL_MEDIA_HASHTAGS_HANDLES)).isEqualTo(
-                """
-                A talk which changes your life
-                Freitag, 27. Dezember 2019, 11:00 MEZ (Europe/Berlin), Yellow pavilion
+        assertThat(
+            SimpleSessionFormat().format(
+                session1,
+                TIME_ZONE_EUROPE_BERLIN,
+                NO_SOCIAL_MEDIA_HASHTAGS_HANDLES
+            )
+        ).isEqualTo(
+            """
+            A talk which changes your life
+            Freitag, 27. Dezember 2019, 11:00 MEZ (Europe/Berlin), Yellow pavilion
 
-                $expectedSession1Url
-                """.trimIndent())
+            $expectedSession1Url
+            """.trimIndent()
+        )
     }
 
     @Test
     fun `format returns formatted multiline text for a session without social media hashtags`() {
-        assertThat(SimpleSessionFormat().format(session1, NO_TIME_ZONE_ID, NO_SOCIAL_MEDIA_HASHTAGS_HANDLES)).isEqualTo(
-                """
-                A talk which changes your life
-                Freitag, 27. Dezember 2019, 11:00 GMT+01:00, Yellow pavilion
+        assertThat(
+            SimpleSessionFormat().format(
+                session1,
+                NO_TIME_ZONE_ID,
+                NO_SOCIAL_MEDIA_HASHTAGS_HANDLES
+            )
+        ).isEqualTo(
+            """
+            A talk which changes your life
+            Freitag, 27. Dezember 2019, 11:00 GMT+01:00, Yellow pavilion
 
-                $expectedSession1Url
-                """.trimIndent())
+            $expectedSession1Url
+            """.trimIndent()
+        )
     }
 
     @Test
     fun `format returns formatted multiline text for a session with social media hashtags`() {
-        assertThat(SimpleSessionFormat().format(session1, NO_TIME_ZONE_ID, SOCIAL_MEDIA_HASHTAGS_HANDLES)).isEqualTo(
-                """
-                A talk which changes your life
-                Freitag, 27. Dezember 2019, 11:00 GMT+01:00, Yellow pavilion
+        assertThat(
+            SimpleSessionFormat().format(
+                session1,
+                NO_TIME_ZONE_ID,
+                SOCIAL_MEDIA_HASHTAGS_HANDLES
+            )
+        ).isEqualTo(
+            """
+            A talk which changes your life
+            Freitag, 27. Dezember 2019, 11:00 GMT+01:00, Yellow pavilion
 
-                $expectedSession1Url
+            $expectedSession1Url
 
-                #fahrplan #36c3
-                """.trimIndent())
+            #fahrplan #36c3
+            """.trimIndent()
+        )
     }
 
     @Test
     fun `format returns formatted multiline text for a wiki session`() {
-        assertThat(SimpleSessionFormat().format(session3, TIME_ZONE_EUROPE_BERLIN, NO_SOCIAL_MEDIA_HASHTAGS_HANDLES)).isEqualTo(
-                """
-                Angel shifts planning
-                Sonntag, 29. Dezember 2019, 09:00 MEZ (Europe/Berlin), Main hall
-                """.trimIndent())
+        assertThat(
+            SimpleSessionFormat().format(
+                session3,
+                TIME_ZONE_EUROPE_BERLIN,
+                NO_SOCIAL_MEDIA_HASHTAGS_HANDLES
+            )
+        ).isEqualTo(
+            """
+            Angel shifts planning
+            Sonntag, 29. Dezember 2019, 09:00 MEZ (Europe/Berlin), Main hall
+            """.trimIndent()
+        )
     }
 
     @Test
@@ -137,51 +172,81 @@ class SimpleSessionFormatTest {
 
     @Test
     fun `format returns separated multiline text for a single session`() {
-        assertThat(SimpleSessionFormat().format(listOf(session1), TIME_ZONE_EUROPE_BERLIN)).isEqualTo(
-                """
-                A talk which changes your life
-                Freitag, 27. Dezember 2019, 11:00 MEZ (Europe/Berlin), Yellow pavilion
+        assertThat(
+            SimpleSessionFormat().format(
+                listOf(session1),
+                TIME_ZONE_EUROPE_BERLIN
+            )
+        ).isEqualTo(
+            """
+            A talk which changes your life
+            Freitag, 27. Dezember 2019, 11:00 MEZ (Europe/Berlin), Yellow pavilion
 
-                $expectedSession1Url
-                """.trimIndent())
+            $expectedSession1Url
+            """.trimIndent()
+        )
     }
 
     @Test
     fun `format returns separated multiline text for multiple sessions`() {
-        assertThat(SimpleSessionFormat().format(listOf(session1, session2), TIME_ZONE_EUROPE_BERLIN)).isEqualTo(
-                """
-                A talk which changes your life
-                Freitag, 27. Dezember 2019, 11:00 MEZ (Europe/Berlin), Yellow pavilion
+        assertThat(
+            SimpleSessionFormat().format(
+                listOf(session1, session2),
+                TIME_ZONE_EUROPE_BERLIN
+            )
+        ).isEqualTo(
+            """
+            A talk which changes your life
+            Freitag, 27. Dezember 2019, 11:00 MEZ (Europe/Berlin), Yellow pavilion
 
-                $expectedSession1Url
+            $expectedSession1Url
 
-                ---
+            ---
 
-                The most boring workshop ever
-                Samstag, 28. Dezember 2019, 17:00 MEZ (Europe/Berlin), Dark cellar
+            The most boring workshop ever
+            Samstag, 28. Dezember 2019, 17:00 MEZ (Europe/Berlin), Dark cellar
 
-                $expectedSession2Url
-                """.trimIndent())
+            $expectedSession2Url
+            """.trimIndent()
+        )
     }
 
     @Test
     fun `format returns formatted multiline text for a session in central european summer time`() {
-        assertThat(SimpleSessionFormat().format(session4, TIME_ZONE_EUROPE_BERLIN, NO_SOCIAL_MEDIA_HASHTAGS_HANDLES)).isEqualTo(
-                """
-                Central european summer time
-                Sonntag, 1. September 2019, 16:00 MESZ (Europe/Berlin), Sunshine tent
+        assertThat(
+            SimpleSessionFormat().format(
+                session4,
+                TIME_ZONE_EUROPE_BERLIN,
+                NO_SOCIAL_MEDIA_HASHTAGS_HANDLES
+            )
+        ).isEqualTo(
+            """
+            Central european summer time
+            Sonntag, 1. September 2019, 16:00 MESZ (Europe/Berlin), Sunshine tent
 
-                $expectedSession4Url
-                """.trimIndent())
+            $expectedSession4Url
+            """.trimIndent()
+        )
     }
 
-    private val expectedSession1Url = createSessionUrl(slug = "LD3FX9", url = "https://example.com/2019/LD3FX9.html")
-    private val expectedSession2Url = createSessionUrl(slug = "U28VSA", url = "https://example.com/2019/U28VSA.html")
-    private val expectedSession4Url = createSessionUrl(slug = "U9SD23", url = "https://example.com/2019/U9SD23.html")
+    private val expectedSession1Url = createSessionUrl(
+        slug = "LD3FX9",
+        url = "https://example.com/2019/LD3FX9.html"
+    )
+    private val expectedSession2Url = createSessionUrl(
+        slug = "U28VSA",
+        url = "https://example.com/2019/U28VSA.html"
+    )
+    private val expectedSession4Url = createSessionUrl(
+        slug = "U9SD23",
+        url = "https://example.com/2019/U9SD23.html"
+    )
 
-    private fun createSessionUrl(slug: String, url: String) = if (isPentabarfConfigured()) createPentabarfUrl(slug) else url
+    private fun createSessionUrl(slug: String, url: String) =
+        if (isPentabarfConfigured()) createPentabarfUrl(slug) else url
 
-    private fun isPentabarfConfigured() = ServerBackendType.PENTABARF.name == BuildConfig.SERVER_BACKEND_TYPE
+    private fun isPentabarfConfigured() =
+        ServerBackendType.PENTABARF.name == BuildConfig.SERVER_BACKEND_TYPE
 
     private fun createPentabarfUrl(slug: String) = String.format(BuildConfig.EVENT_URL, slug)
 
