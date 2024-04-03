416bf5620797df071a768ccb54b857c7023300ee
Tobias Preuss <tobias.preuss@googlemail.com>
2024-04-04 00:12:10 +0200

    Merge SessionFormatter into SessionPropertiesFormatter.
    + Limit visibility of SupportedIndoorNavigation and
      UnsupportedIndoorNavigation to SessionDetailsViewModelTest.

--- app/src/test/java/nerd/tuxmobil/fahrplan/congress/utils/SessionPropertiesFormatterTest.kt
+++ app/src/test/java/nerd/tuxmobil/fahrplan/congress/utils/SessionPropertiesFormatterTest.kt
@@ -30,6 +30,41 @@ class SessionPropertiesFormatterTest {
     private val formatter = SessionPropertiesFormatter()
 
     @Test
+    fun `getFormattedLinks returns an empty string`() {
+        val links = ""
+        val expected = ""
+        assertThat(formatter.getFormattedLinks(links)).isEqualTo(expected)
+    }
+
+    @Test
+    fun `getFormattedLinks returns the given unmodified string`() {
+        val links = "[VOC projects](https://www.voc.com/projects/)"
+        val expected = "[VOC projects](https://www.voc.com/projects/)"
+        assertThat(formatter.getFormattedLinks(links)).isEqualTo(expected)
+    }
+
+    @Test
+    fun `getFormattedLinks returns a br separated string`() {
+        val links = "[VOC projects](https://www.voc.com/projects/),[POC](https://poc.com/QXut1XBymAk)"
+        val expected = "[VOC projects](https://www.voc.com/projects/)<br>[POC](https://poc.com/QXut1XBymAk)"
+        assertThat(formatter.getFormattedLinks(links)).isEqualTo(expected)
+    }
+
+    @Test
+    fun `getFormattedUrl returns an empty string`() {
+        val url = ""
+        val expected = ""
+        assertThat(formatter.getFormattedUrl(url)).isEqualTo(expected)
+    }
+
+    @Test
+    fun `getFormattedUrl returns an HTML formatted weblink`() {
+        val url = "https://example.com/talk.html"
+        val expected = """<a href="https://example.com/talk.html">https://example.com/talk.html</a>"""
+        assertThat(formatter.getFormattedUrl(url)).isEqualTo(expected)
+    }
+
+    @Test
     fun `getFormattedSpeakers returns empty string if speakers is empty`() {
         val session = createSession(speakers = emptyList())
         assertThat(formatter.getFormattedSpeakers(session)).isEmpty()
