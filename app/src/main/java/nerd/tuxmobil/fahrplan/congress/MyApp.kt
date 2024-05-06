48c51285d6873ab5bfc5a717f3e3abcd55255f8c
Tobias Preuss <tobias.preuss@googlemail.com>
2024-05-06 22:25:05 +0200

    Remove now unused configuration & code to configure a schedule update alarm.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/MyApp.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/MyApp.kt
@@ -2,17 +2,9 @@ package nerd.tuxmobil.fahrplan.congress
 
 import android.app.Application
 import androidx.annotation.CallSuper
-import androidx.annotation.VisibleForTesting
 import info.metadude.android.eventfahrplan.commons.logging.Logging
-import info.metadude.android.eventfahrplan.commons.temporal.Moment
-import info.metadude.android.eventfahrplan.commons.temporal.Moment.Companion.toMoment
-import nerd.tuxmobil.fahrplan.congress.models.ConferenceTimeFrame
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 import org.ligi.tracedroid.TraceDroid
-import org.threeten.bp.LocalDate
-import org.threeten.bp.LocalTime
-import org.threeten.bp.ZoneId
-import org.threeten.bp.ZonedDateTime
 
 
 class MyApp : Application() {
@@ -22,31 +14,6 @@ class MyApp : Application() {
     }
 
     companion object {
-
-        private val FIRST_DAY_START = getMoment(
-            "Europe/Paris",
-            BuildConfig.SCHEDULE_FIRST_DAY_START_YEAR,
-            BuildConfig.SCHEDULE_FIRST_DAY_START_MONTH,
-            BuildConfig.SCHEDULE_FIRST_DAY_START_DAY
-        )
-
-        private val LAST_DAY_END = getMoment(
-            "Europe/Paris",
-            BuildConfig.SCHEDULE_LAST_DAY_END_YEAR,
-            BuildConfig.SCHEDULE_LAST_DAY_END_MONTH,
-            BuildConfig.SCHEDULE_LAST_DAY_END_DAY
-        )
-
-        @VisibleForTesting
-        fun getMoment(timeZoneId: String, year: Int, month: Int, day: Int): Moment {
-            val zoneId = ZoneId.of(timeZoneId)
-            val localDate = LocalDate.of(year, month, day)
-            val zonedDateTime = ZonedDateTime.of(localDate, LocalTime.MIDNIGHT, zoneId)
-            return zonedDateTime.toMoment()
-        }
-
-        @Deprecated("Unused. To be removed.")
-        val conferenceTimeFrame = ConferenceTimeFrame.Known(FIRST_DAY_START, LAST_DAY_END)
 
         @JvmField
         var taskRunning = TASKS.NONE
