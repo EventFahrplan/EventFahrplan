EventFahrplan/EventFahrplan

commit f27b7c43325076a41bef9afa631d67ee86c570b3

Transpile base package
+ Transpiled the classes in the 'nerd.tuxmobil.fahrplan.congress.base'
  package (AbstractListFragment, BaseActivity, SessionsAdapter) from
  Java to Kotlin.
+ Added formatting and made improvements to the code (for example
  converting 'ViewHolder' class to data class)

app/src/main/java/nerd/tuxmobil/fahrplan/congress/base/SessionsAdapter.kt
@@ -1,0 +1,198 @@
+package nerd.tuxmobil.fahrplan.congress.base
+
+import android.content.Context
+import android.view.ContextThemeWrapper
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+import androidx.annotation.LayoutRes
+import info.metadude.android.eventfahrplan.commons.temporal.DateFormatter
+
+import nerd.tuxmobil.fahrplan.congress.R
+import nerd.tuxmobil.fahrplan.congress.extensions.getLayoutInflater
+import nerd.tuxmobil.fahrplan.congress.extensions.requireViewByIdCompat
+import nerd.tuxmobil.fahrplan.congress.models.Session
+
+
+abstract class SessionsAdapter protected constructor(
+
+    context: Context,
+    @LayoutRes layout: Int,
+    list: List<Session>,
+    numDays: Int,
+    useDeviceTimeZone: Boolean
+
+) : ArrayAdapter<Session>(context, layout, list) {
+
+    companion object {
+        private const val TYPE_ITEM = 0
+        private const val TYPE_SEPARATOR = 1
+        private const val NUM_VIEW_TYPES = 2
+    }
+
+    @get:JvmName("getAdapterContext")
+    protected var context: Context
+        private set
+
+    private val list: List<Session>
+    private val numDays: Int
+    private lateinit var mapper: MutableList<Int>
+    private lateinit var separatorStrings: MutableList<String>
+    private lateinit var separatorsSet: MutableSet<Int>
+    protected val useDeviceTimeZone: Boolean
+
+    init {
+        this.context = ContextThemeWrapper(context, R.style.Theme_Congress_NoActionBar)
+        this.list = list
+        this.numDays = numDays
+        this.useDeviceTimeZone = useDeviceTimeZone
+        initMapper()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val rowView: View
+        var viewHolder: ViewHolder? = null
+        var viewHolderSeparator: ViewHolderSeparator? = null
+        val type = getItemViewType(position)
+
+        if (convertView == null) {
+            // clone the inflater using the ContextThemeWrapper
+            val inflater = context.getLayoutInflater()
+            val localInflater = inflater.cloneInContext(context)
+
+            when (type) {
+                TYPE_ITEM -> {
+                    rowView = localInflater.inflate(R.layout.session_list_item, parent, false)
+                    viewHolder = ViewHolder(
+                        rowView.requireViewByIdCompat(R.id.session_list_item_title_view),
+                        rowView.requireViewByIdCompat(R.id.session_list_item_subtitle_view),
+                        rowView.requireViewByIdCompat(R.id.session_list_item_speakers_view),
+                        rowView.requireViewByIdCompat(R.id.session_list_item_language_view),
+                        rowView.requireViewByIdCompat(R.id.session_list_item_day_view),
+                        rowView.requireViewByIdCompat(R.id.session_list_item_time_view),
+                        rowView.requireViewByIdCompat(R.id.session_list_item_room_view),
+                        rowView.requireViewByIdCompat(R.id.session_list_item_duration_view),
+                        rowView.requireViewByIdCompat(R.id.session_list_item_video_view),
+                        rowView.requireViewByIdCompat(R.id.session_list_item_no_video_view),
+                        rowView.requireViewByIdCompat(R.id.session_list_item_without_video_recording_view),
+                    )
+                    rowView.tag = viewHolder
+                }
+                TYPE_SEPARATOR -> {
+                    rowView = localInflater.inflate(R.layout.session_list_separator, parent, false)
+                    viewHolderSeparator = ViewHolderSeparator()
+                    viewHolderSeparator.text = rowView.requireViewByIdCompat(R.id.session_list_separator_title_view)
+                    rowView.tag = viewHolderSeparator
+                }
+                else -> {
+                    error("Unknown type: $type")
+                }
+            }
+        } else {
+            rowView = convertView
+            when (type) {
+                TYPE_ITEM -> viewHolder = rowView.tag as ViewHolder
+                TYPE_SEPARATOR -> viewHolderSeparator = rowView.tag as ViewHolderSeparator
+                else -> error("Unknown type: $type")
+            }
+        }
+        when (type) {
+            TYPE_ITEM -> setItemContent(position, viewHolder!!)
+            TYPE_SEPARATOR -> setSeparatorContent(position, viewHolderSeparator!!)
+            else -> error("Unknown type: $type")
+        }
+        return rowView
+    }
+
+    fun getSession(position: Int) = list[mapper[position]]
+
+    protected fun resetItemStyles(viewHolder: ViewHolder) {
+        resetTextStyle(viewHolder.title, R.style.ScheduleListPrimary)
+        resetTextStyle(viewHolder.subtitle, R.style.ScheduleListSecondary)
+        resetTextStyle(viewHolder.speakers, R.style.ScheduleListSecondary)
+        resetTextStyle(viewHolder.lang, R.style.ScheduleListSecondary)
+        resetTextStyle(viewHolder.day, R.style.ScheduleListSecondary)
+        resetTextStyle(viewHolder.time, R.style.ScheduleListSecondary)
+        resetTextStyle(viewHolder.room, R.style.ScheduleListSecondary)
+        resetTextStyle(viewHolder.duration, R.style.ScheduleListSecondary)
+        viewHolder.withoutVideoRecording.setImageResource(R.drawable.ic_without_video_recording)
+    }
+
+    protected open fun resetTextStyle(textView: TextView, style: Int) {
+        textView.setTextAppearance(context, style)
+    }
+
+    protected abstract fun setItemContent(position: Int, viewHolder: ViewHolder)
+
+    private fun setSeparatorContent(position: Int, viewHolderSeparator: ViewHolderSeparator) {
+        viewHolderSeparator.text!!.text = separatorStrings[mapper[position]]
+    }
+
+    override fun getViewTypeCount() = NUM_VIEW_TYPES
+
+    override fun areAllItemsEnabled() = false
+
+    override fun isEnabled(position: Int) = position !in separatorsSet
+
+    override fun getItemViewType(position: Int) =
+        if (position in separatorsSet) TYPE_SEPARATOR else TYPE_ITEM
+
+    override fun getCount() = list.size + separatorsSet.size
+
+    private fun initMapper() {
+        separatorsSet = mutableSetOf()
+        separatorStrings = mutableListOf()
+        mapper = mutableListOf()
+
+        var day: Int
+        var lastDay = 0
+        var sepCount = 0
+
+        val daySeparator = context.getString(R.string.day_separator)
+        for (index in list.indices) {
+            val session = list[index]
+            day = session.day
+            val formattedDate = DateFormatter.newInstance(useDeviceTimeZone)
+                .getFormattedDate(session.dateUTC, session.timeZoneOffset)
+
+            if (day != lastDay) {
+                lastDay = day
+                if (numDays > 1) {
+                    val dayDateSeparator = String.format(daySeparator, day, formattedDate)
+                    separatorStrings.add(dayDateSeparator)
+                    separatorsSet.add(index + sepCount)
+                    mapper.add(sepCount)
+                    sepCount++
+                }
+            }
+            mapper.add(index)
+        }
+    }
+
+    override fun notifyDataSetChanged() {
+        super.notifyDataSetChanged()
+        initMapper()
+    }
+
+    data class ViewHolder(
+        var title: TextView,
+        var subtitle: TextView,
+        var speakers: TextView,
+        var lang: TextView,
+        var day: TextView,
+        var time: TextView,
+        var room: TextView,
+        var duration: TextView,
+        var noVideo: ImageView,
+        var video: ImageView,
+        var withoutVideoRecording: ImageView
+    )
+
+    internal class ViewHolderSeparator {
+        var text: TextView? = null
+    }
+
+}

app/src/main/java/nerd/tuxmobil/fahrplan/congress/base/AbstractListFragment.java
@@ -1,46 +1,0 @@
-package nerd.tuxmobil.fahrplan.congress.base;
-
-import android.content.Context;
-
-import androidx.annotation.CallSuper;
-import androidx.annotation.MainThread;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.ListFragment;
-
-import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository;
-
-/**
- * A fragment representing a list of Items.
- * <p/>
- * Large screen devices (such as tablets) are supported by replacing the ListView
- * with a GridView.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnSessionListClick}
- * interface.
- */
-public abstract class AbstractListFragment extends ListFragment {
-
-    public interface OnSessionListClick {
-
-        /**
-         * This interface must be implemented by activities that contain this
-         * fragment to allow an interaction in this fragment to be communicated
-         * to the activity and potentially other fragments contained in that
-         * activity.
-         *
-         * @param sessionId The ID of the session which was clicked.
-         */
-        void onSessionListClick(@NonNull String sessionId);
-    }
-
-    protected AppRepository appRepository;
-
-    @MainThread
-    @CallSuper
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        appRepository = AppRepository.INSTANCE;
-    }
-
-}

app/src/main/java/nerd/tuxmobil/fahrplan/congress/base/AbstractListFragment.kt
@@ -1,0 +1,44 @@
+package nerd.tuxmobil.fahrplan.congress.base
+
+import android.content.Context
+import androidx.annotation.CallSuper
+import androidx.annotation.MainThread
+import androidx.fragment.app.ListFragment
+import nerd.tuxmobil.fahrplan.congress.base.AbstractListFragment.OnSessionListClick
+import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
+
+/**
+ * A fragment representing a list of Items.
+ *
+ *
+ * Large screen devices (such as tablets) are supported by replacing the ListView
+ * with a GridView.
+ *
+ *
+ * Activities containing this fragment MUST implement the [OnSessionListClick]
+ * interface.
+ */
+abstract class AbstractListFragment : ListFragment() {
+
+    interface OnSessionListClick {
+        /**
+         * This interface must be implemented by activities that contain this
+         * fragment to allow an interaction in this fragment to be communicated
+         * to the activity and potentially other fragments contained in that
+         * activity.
+         *
+         * @param sessionId The ID of the session which was clicked.
+         */
+        fun onSessionListClick(sessionId: String)
+    }
+
+    protected lateinit var appRepository: AppRepository
+
+    @MainThread
+    @CallSuper
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        appRepository = AppRepository
+    }
+
+}

app/src/main/java/nerd/tuxmobil/fahrplan/congress/base/BaseActivity.java
@@ -1,84 +1,0 @@
-package nerd.tuxmobil.fahrplan.congress.base;
-
-import android.view.MenuItem;
-import android.view.View;
-
-import androidx.annotation.ContentView;
-import androidx.annotation.IdRes;
-import androidx.annotation.LayoutRes;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
-import nerd.tuxmobil.fahrplan.congress.utils.ActivityHelper;
-
-public abstract class BaseActivity extends AppCompatActivity {
-
-    public BaseActivity() {
-        super();
-    }
-
-    @ContentView
-    public BaseActivity(@LayoutRes int contentLayoutId) {
-        super(contentLayoutId);
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        //noinspection SwitchStatementWithTooFewBranches
-        switch (item.getItemId()) {
-            // Respond to the action bar's Up/Home button
-            case android.R.id.home:
-                return ActivityHelper.navigateUp(this);
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    protected void addFragment(@IdRes int containerViewId,
-                               @NonNull Fragment fragment,
-                               @NonNull String fragmentTag) {
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(containerViewId, fragment, fragmentTag)
-                .commit();
-    }
-
-    protected void replaceFragment(@IdRes int containerViewId,
-                                   @NonNull Fragment fragment,
-                                   @NonNull String fragmentTag) {
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(containerViewId, fragment, fragmentTag)
-                .commit();
-    }
-
-    protected void removeFragment(@NonNull String fragmentTag) {
-        FragmentManager supportFragmentManager = getSupportFragmentManager();
-        Fragment fragment = supportFragmentManager.findFragmentByTag(fragmentTag);
-        if (fragment != null) {
-            supportFragmentManager
-                    .beginTransaction()
-                    .remove(fragment)
-                    .commit();
-        }
-    }
-
-    protected
-    @Nullable
-    Fragment findFragment(@NonNull String fragmentTag) {
-        return getSupportFragmentManager().findFragmentByTag(fragmentTag);
-    }
-
-    /**
-     * See {@link ActivityCompat#requireViewById}.
-     */
-    @NonNull
-    protected <T extends View> T requireViewByIdCompat(@IdRes int id) {
-        return ActivityCompat.requireViewById(this, id);
-    }
-
-}

app/src/main/java/nerd/tuxmobil/fahrplan/congress/base/BaseActivity.kt
@@ -1,0 +1,70 @@
+package nerd.tuxmobil.fahrplan.congress.base
+
+import android.view.MenuItem
+import android.view.View
+import androidx.annotation.ContentView
+import androidx.annotation.IdRes
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import nerd.tuxmobil.fahrplan.congress.utils.ActivityHelper.navigateUp
+
+abstract class BaseActivity : AppCompatActivity {
+
+    constructor() : super()
+
+    @ContentView
+    constructor(@LayoutRes contentLayoutId: Int) : super(contentLayoutId)
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> return navigateUp()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    protected fun addFragment(
+        @IdRes containerViewId: Int,
+        fragment: Fragment,
+        fragmentTag: String
+    ) {
+        supportFragmentManager
+            .beginTransaction()
+            .add(containerViewId, fragment, fragmentTag)
+            .commit()
+    }
+
+    @Suppress("SameParameterValue")
+    protected fun replaceFragment(
+        @IdRes containerViewId: Int,
+        fragment: Fragment,
+        fragmentTag: String
+    ) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(containerViewId, fragment, fragmentTag)
+            .commit()
+    }
+
+    protected fun removeFragment(fragmentTag: String) {
+        val fragment = supportFragmentManager.findFragmentByTag(fragmentTag)
+        if (fragment != null) {
+            supportFragmentManager
+                .beginTransaction()
+                .remove(fragment)
+                .commit()
+        }
+    }
+
+    protected fun findFragment(fragmentTag: String) =
+        supportFragmentManager.findFragmentByTag(fragmentTag)
+
+    /**
+     * See [ActivityCompat.requireViewById].
+     */
+    protected fun <T : View?> requireViewByIdCompat(@IdRes id: Int): T {
+        return ActivityCompat.requireViewById(this, id)
+    }
+
+}

app/src/main/java/nerd/tuxmobil/fahrplan/congress/base/SessionsAdapter.java
@@ -1,238 +1,0 @@
-package nerd.tuxmobil.fahrplan.congress.base;
-
-import static nerd.tuxmobil.fahrplan.congress.extensions.ViewExtensions.requireViewByIdCompat;
-
-import android.content.Context;
-import android.view.ContextThemeWrapper;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import androidx.annotation.LayoutRes;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeSet;
-
-import info.metadude.android.eventfahrplan.commons.temporal.DateFormatter;
-import nerd.tuxmobil.fahrplan.congress.R;
-import nerd.tuxmobil.fahrplan.congress.extensions.Contexts;
-import nerd.tuxmobil.fahrplan.congress.models.Session;
-
-public abstract class SessionsAdapter extends ArrayAdapter<Session> {
-
-    private static final int TYPE_ITEM = 0;
-    private static final int TYPE_SEPARATOR = 1;
-    private static final int NUM_VIEW_TYPES = 2;
-
-    protected final Context context;
-
-    private final List<Session> list;
-    private final int numDays;
-    protected final boolean useDeviceTimeZone;
-    private ArrayList<Integer> mMapper;
-    private ArrayList<String> mSeparatorStrings;
-    private TreeSet<Integer> mSeparatorsSet;
-
-    protected SessionsAdapter(Context context, @LayoutRes int layout, List<Session> list, int numDays, boolean useDeviceTimeZone) {
-        super(context, layout, list);
-        this.context = new ContextThemeWrapper(context, R.style.Theme_Congress_NoActionBar);
-        this.list = list;
-        this.numDays = numDays;
-        this.useDeviceTimeZone = useDeviceTimeZone;
-        initMapper();
-    }
-
-    @Override
-    @NonNull
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View rowView;
-        ViewHolder viewHolder = null;
-        ViewHolderSeparator viewHolderSeparator = null;
-
-        int type = getItemViewType(position);
-
-        if (convertView == null) {
-
-            // clone the inflater using the ContextThemeWrapper
-            LayoutInflater inflater = Contexts.getLayoutInflater(context);
-            LayoutInflater localInflater = inflater.cloneInContext(context);
-
-            switch (type) {
-                case TYPE_ITEM:
-                    rowView = localInflater.inflate(R.layout.session_list_item, parent, false);
-                    viewHolder = new ViewHolder();
-
-                    viewHolder.title = requireViewByIdCompat(rowView, R.id.session_list_item_title_view);
-                    viewHolder.subtitle = requireViewByIdCompat(rowView, R.id.session_list_item_subtitle_view);
-                    viewHolder.speakers = requireViewByIdCompat(rowView, R.id.session_list_item_speakers_view);
-                    viewHolder.lang = requireViewByIdCompat(rowView, R.id.session_list_item_language_view);
-                    viewHolder.day = requireViewByIdCompat(rowView, R.id.session_list_item_day_view);
-                    viewHolder.time = requireViewByIdCompat(rowView, R.id.session_list_item_time_view);
-                    viewHolder.room = requireViewByIdCompat(rowView, R.id.session_list_item_room_view);
-                    viewHolder.duration = requireViewByIdCompat(rowView, R.id.session_list_item_duration_view);
-                    viewHolder.video = requireViewByIdCompat(rowView, R.id.session_list_item_video_view);
-                    viewHolder.noVideo = requireViewByIdCompat(rowView, R.id.session_list_item_no_video_view);
-                    viewHolder.withoutVideoRecording = requireViewByIdCompat(rowView, R.id.session_list_item_without_video_recording_view);
-                    rowView.setTag(viewHolder);
-                    break;
-                case TYPE_SEPARATOR:
-                    rowView = localInflater.inflate(R.layout.session_list_separator, parent, false);
-                    viewHolderSeparator = new ViewHolderSeparator();
-                    viewHolderSeparator.text = requireViewByIdCompat(rowView, R.id.session_list_separator_title_view);
-                    rowView.setTag(viewHolderSeparator);
-                    break;
-                default:
-                    throw new IllegalStateException("Unknown type: " + type);
-            }
-
-        } else {
-            rowView = convertView;
-            switch (type) {
-                case TYPE_ITEM:
-                    viewHolder = (ViewHolder) rowView.getTag();
-                    break;
-                case TYPE_SEPARATOR:
-                    viewHolderSeparator = (ViewHolderSeparator) rowView.getTag();
-                    break;
-                default:
-                    throw new IllegalStateException("Unknown type: " + type);
-            }
-        }
-
-        switch (type) {
-            case TYPE_ITEM:
-                setItemContent(position, viewHolder);
-                break;
-            case TYPE_SEPARATOR:
-                setSeparatorContent(position, viewHolderSeparator);
-                break;
-            default:
-                throw new IllegalStateException("Unknown type: " + type);
-        }
-
-        return rowView;
-    }
-
-    @NonNull
-    public Session getSession(int position) {
-        return list.get(mMapper.get(position));
-    }
-
-    protected void resetItemStyles(ViewHolder viewHolder) {
-        resetTextStyle(viewHolder.title, R.style.ScheduleListPrimary);
-        resetTextStyle(viewHolder.subtitle, R.style.ScheduleListSecondary);
-        resetTextStyle(viewHolder.speakers, R.style.ScheduleListSecondary);
-        resetTextStyle(viewHolder.lang, R.style.ScheduleListSecondary);
-        resetTextStyle(viewHolder.day, R.style.ScheduleListSecondary);
-        resetTextStyle(viewHolder.time, R.style.ScheduleListSecondary);
-        resetTextStyle(viewHolder.room, R.style.ScheduleListSecondary);
-        resetTextStyle(viewHolder.duration, R.style.ScheduleListSecondary);
-        viewHolder.withoutVideoRecording.setImageResource(R.drawable.ic_without_video_recording);
-    }
-
-    protected void resetTextStyle(TextView textView, int style) {
-        textView.setTextAppearance(context, style);
-    }
-
-    protected abstract void setItemContent(int position, ViewHolder viewHolder);
-
-    private void setSeparatorContent(int position, ViewHolderSeparator viewHolderSeparator) {
-        viewHolderSeparator.text.setText(mSeparatorStrings.get(mMapper.get(position)));
-    }
-
-    @Override
-    public int getViewTypeCount() {
-        return NUM_VIEW_TYPES;
-    }
-
-    @Override
-    public boolean areAllItemsEnabled() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled(int position) {
-        return !mSeparatorsSet.contains(position);
-    }
-
-    @Override
-    public int getItemViewType(int position) {
-        return mSeparatorsSet.contains(position) ? TYPE_SEPARATOR : TYPE_ITEM;
-    }
-
-    @Override
-    public int getCount() {
-        int count = 0;
-        if (list != null) {
-            count += list.size();
-        }
-        if (mSeparatorsSet != null) {
-            count += mSeparatorsSet.size();
-        }
-        return count;
-    }
-
-    private void initMapper() {
-        mSeparatorsSet = new TreeSet<>();
-        mSeparatorStrings = new ArrayList<>();
-        mMapper = new ArrayList<>();
-        int day;
-        int lastDay = 0;
-        int sepCount = 0;
-
-        if (list == null) {
-            return;
-        }
-
-        String daySeparator = context.getString(R.string.day_separator);
-
-        for (int index = 0; index < list.size(); index++) {
-
-            Session l = list.get(index);
-            day = l.day;
-            if (day != lastDay) {
-                lastDay = day;
-                if (numDays > 1) {
-                    String formattedDate = DateFormatter.newInstance(useDeviceTimeZone).getFormattedDate(l.dateUTC, l.timeZoneOffset);
-                    String dayDateSeparator = String.format(daySeparator, day, formattedDate);
-                    mSeparatorStrings.add(dayDateSeparator);
-                    mSeparatorsSet.add(index + sepCount);
-                    mMapper.add(sepCount);
-                    sepCount++;
-                }
-            }
-
-            mMapper.add(index);
-        }
-    }
-
-    @Override
-    public void notifyDataSetChanged() {
-        super.notifyDataSetChanged();
-        initMapper();
-    }
-
-    public static class ViewHolder {
-        public TextView title;
-        public TextView subtitle;
-        public TextView speakers;
-        public TextView lang;
-        public TextView day;
-        public TextView time;
-        public TextView room;
-        public TextView duration;
-        public ImageView noVideo;
-        public ImageView video;
-        public ImageView withoutVideoRecording;
-    }
-
-    static class ViewHolderSeparator {
-        TextView text;
-    }
-}

commit c7323f09a7d9bf000edd6d9b4107077faf1d9e0b

Fix interchanged video icons in schedule changes screen.
+ Broken since: f27b7c43325076a41bef9afa631d67ee86c570b3.
+ The icons were mixed up because the named parameters were omitted and
  the order of properties in the ViewHolder class did not match the
  order of view ID references.
  See ViewHolder#video and ViewHolder#noVideo properties.

app/src/main/java/nerd/tuxmobil/fahrplan/congress/base/SessionsAdapter.kt
@@ -68,17 +68,17 @@ abstract class SessionsAdapter protected constructor(
                 TYPE_ITEM -> {
                     rowView = localInflater.inflate(R.layout.session_list_item, parent, false)
                     viewHolder = ViewHolder(
-                        rowView.requireViewByIdCompat(R.id.session_list_item_title_view),
-                        rowView.requireViewByIdCompat(R.id.session_list_item_subtitle_view),
-                        rowView.requireViewByIdCompat(R.id.session_list_item_speakers_view),
-                        rowView.requireViewByIdCompat(R.id.session_list_item_language_view),
-                        rowView.requireViewByIdCompat(R.id.session_list_item_day_view),
-                        rowView.requireViewByIdCompat(R.id.session_list_item_time_view),
-                        rowView.requireViewByIdCompat(R.id.session_list_item_room_view),
-                        rowView.requireViewByIdCompat(R.id.session_list_item_duration_view),
-                        rowView.requireViewByIdCompat(R.id.session_list_item_video_view),
-                        rowView.requireViewByIdCompat(R.id.session_list_item_no_video_view),
-                        rowView.requireViewByIdCompat(R.id.session_list_item_without_video_recording_view),
+                        title = rowView.requireViewByIdCompat(R.id.session_list_item_title_view),
+                        subtitle = rowView.requireViewByIdCompat(R.id.session_list_item_subtitle_view),
+                        speakers = rowView.requireViewByIdCompat(R.id.session_list_item_speakers_view),
+                        lang = rowView.requireViewByIdCompat(R.id.session_list_item_language_view),
+                        day = rowView.requireViewByIdCompat(R.id.session_list_item_day_view),
+                        time = rowView.requireViewByIdCompat(R.id.session_list_item_time_view),
+                        room = rowView.requireViewByIdCompat(R.id.session_list_item_room_view),
+                        duration = rowView.requireViewByIdCompat(R.id.session_list_item_duration_view),
+                        video = rowView.requireViewByIdCompat(R.id.session_list_item_video_view),
+                        noVideo = rowView.requireViewByIdCompat(R.id.session_list_item_no_video_view),
+                        withoutVideoRecording = rowView.requireViewByIdCompat(R.id.session_list_item_without_video_recording_view),
                     )
                     rowView.tag = viewHolder
                 }