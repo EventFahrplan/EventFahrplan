d9ecacaf2a3a1eb2148db275851f49f7ca221bce
Tobias Preuss <tobias.preuss@googlemail.com>
2024-09-12 23:17:30 +0200

    Use Jetpack Compose UI 2024.09.02.
    + Changelogs:
      https://developer.android.com/develop/ui/compose/bom/bom-mapping
      https://developer.android.com/jetpack/androidx/releases/compose-material3#1.3.0

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -18,7 +18,7 @@ object Android {
 object Compose {
 
     private object Versions {
-        const val bom = "2024.09.00"
+        const val bom = "2024.09.02"
     }
 
     const val bom = "androidx.compose:compose-bom:${Versions.bom}"
