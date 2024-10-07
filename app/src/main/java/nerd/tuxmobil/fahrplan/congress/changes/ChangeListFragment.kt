EventFahrplan/EventFahrplan

commit f3e530841118a64da1bc473d3a5712d811593848

Migrate "Schedule Changes" screen to Compose UI.
+ Replace XML-based screen with Compose UI setup.
+ Detach from AbstractListFragment.
+ Introduce SessionChangeParametersFactory to encapsulate screen logic. Add unit tests.
+ Let video recording icon color represent change state similar as text changes.
+ Remove now unused code from SessionsAdapter which is still used by "Favorites" screen.
+ Vertical scroll bar is missing.

app/src/main/java/nerd/tuxmobil/fahrplan/congress/changes/ChangeListFragment.kt
@@ -6,36 +6,37 @@ import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.HeaderViewListAdapter
-import android.widget.ListView
 import androidx.annotation.CallSuper
 import androidx.annotation.IdRes
 import androidx.annotation.MainThread
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
 import info.metadude.android.eventfahrplan.commons.flow.observe
 import nerd.tuxmobil.fahrplan.congress.R
-import nerd.tuxmobil.fahrplan.congress.base.AbstractListFragment
 import nerd.tuxmobil.fahrplan.congress.base.AbstractListFragment.OnSessionListClick
+import nerd.tuxmobil.fahrplan.congress.commons.ResourceResolver
+import nerd.tuxmobil.fahrplan.congress.commons.ScreenNavigation
 import nerd.tuxmobil.fahrplan.congress.contract.BundleKeys
 import nerd.tuxmobil.fahrplan.congress.extensions.replaceFragment
-import nerd.tuxmobil.fahrplan.congress.extensions.requireViewByIdCompat
 import nerd.tuxmobil.fahrplan.congress.extensions.withArguments
 import nerd.tuxmobil.fahrplan.congress.notifications.NotificationHelper
+import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 import nerd.tuxmobil.fahrplan.congress.utils.ContentDescriptionFormatter
 import nerd.tuxmobil.fahrplan.congress.utils.SessionPropertiesFormatter
 
 /**
  * A fragment representing a list of Items.
  *
- * Large screen devices (such as tablets) are supported by replacing the ListView with a GridView.
- *
  * Activities containing this fragment MUST implement the [OnSessionListClick] interface.
  *
  * Mandatory empty constructor for the fragment manager to instantiate the fragment (e.g. upon
  * screen orientation changes).
  */
-class ChangeListFragment : AbstractListFragment() {
+class ChangeListFragment : Fragment() {
 
     companion object {
 
@@ -55,8 +56,16 @@ class ChangeListFragment : AbstractListFragment() {
     }
 
     private var onSessionListClickListener: OnSessionListClick? = null
-    private lateinit var currentListView: ListView
-    private val viewModelFactory by lazy { ChangeListViewModelFactory(appRepository) }
+    private lateinit var screenNavigation: ScreenNavigation
+    private val viewModelFactory by lazy {
+        ChangeListViewModelFactory(
+            AppRepository,
+            ResourceResolver(requireContext()),
+            screenNavigation,
+            SessionPropertiesFormatter(),
+            ContentDescriptionFormatter(requireContext()),
+        )
+    }
     private val viewModel: ChangeListViewModel by viewModels { viewModelFactory }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -65,16 +74,18 @@ class ChangeListFragment : AbstractListFragment() {
 
         val contextThemeWrapper = ContextThemeWrapper(requireContext(), R.style.Theme_Congress_NoActionBar)
         val localInflater = inflater.cloneInContext(contextThemeWrapper)
-        val (fragmentLayout, headerLayout) = when {
-            sidePane -> R.layout.fragment_session_list_narrow to R.layout.changes_header
-            else -> R.layout.fragment_session_list to R.layout.header_empty
+        val fragmentView = localInflater.inflate(R.layout.fragment_session_list, container, false)
+        fragmentView.findViewById<ComposeView>(R.id.session_changes_view).apply {
+            setViewCompositionStrategy(DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                SessionChangesScreen(
+                    state = viewModel.sessionChangesState.collectAsState().value,
+                    showInSidePane = sidePane,
+                    onViewEvent = viewModel::onViewEvent,
+                )
+            }
+            isClickable = true
         }
-
-        val fragmentView = localInflater.inflate(fragmentLayout, container, false)
-        val headerView = localInflater.inflate(headerLayout, null, false)
-        currentListView = fragmentView.requireViewByIdCompat(android.R.id.list)
-        currentListView.addHeaderView(headerView, null, false)
-        currentListView.setHeaderDividersEnabled(false)
         return fragmentView
     }
 
@@ -84,17 +95,6 @@ class ChangeListFragment : AbstractListFragment() {
     }
 
     private fun observeViewModel() {
-        viewModel.changeListParameter.observe(this) { (sessions, numDays, useDeviceTimeZone) ->
-            val adapter = ChangeListAdapter(
-                context = requireContext(),
-                list = sessions,
-                numDays = numDays,
-                useDeviceTimeZone = useDeviceTimeZone,
-                sessionPropertiesFormatter = SessionPropertiesFormatter(),
-                contentDescriptionFormatter = ContentDescriptionFormatter(requireContext())
-            )
-            currentListView.adapter = adapter
-        }
         viewModel.scheduleChangesSeen.observe(viewLifecycleOwner) {
             NotificationHelper(requireContext()).cancelScheduleUpdateNotification()
         }
@@ -111,6 +111,9 @@ class ChangeListFragment : AbstractListFragment() {
         super.onAttach(context)
         if (context is OnSessionListClick) {
             onSessionListClickListener = context
+            screenNavigation = ScreenNavigation { sessionId ->
+                onSessionListClickListener?.onSessionListClick(sessionId)
+            }
         } else {
             error("$context must implement OnSessionListClick")
         }
@@ -121,19 +124,6 @@ class ChangeListFragment : AbstractListFragment() {
     override fun onDetach() {
         super.onDetach()
         onSessionListClickListener = null
-    }
-
-    override fun onListItemClick(listView: ListView, view: View, listPosition: Int, rowId: Long) {
-        val headerViewListAdapter = currentListView.adapter as HeaderViewListAdapter
-        val adapter = headerViewListAdapter.wrappedAdapter as ChangeListAdapter
-        onSessionListClickListener?.let { listener ->
-            // Notify the active callbacks interface (the activity, if the
-            // fragment is attached to one) that an item has been selected.
-            val clicked = adapter.getSession(listPosition - 1)
-            if (!clicked.changedIsCanceled) {
-                listener.onSessionListClick(clicked.sessionId)
-            }
-        }
     }
 
 }

app/src/main/java/nerd/tuxmobil/fahrplan/congress/changes/ChangeListViewModel.kt
@@ -2,50 +2,59 @@ package nerd.tuxmobil.fahrplan.congress.changes
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import info.metadude.android.eventfahrplan.commons.logging.Logging
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.SendChannel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
-import nerd.tuxmobil.fahrplan.congress.models.Session
+import nerd.tuxmobil.fahrplan.congress.changes.SessionChangeState.Loading
+import nerd.tuxmobil.fahrplan.congress.changes.SessionChangeState.Success
+import nerd.tuxmobil.fahrplan.congress.changes.SessionChangeViewEvent.OnSessionChangeItemClick
+import nerd.tuxmobil.fahrplan.congress.commons.ScreenNavigation
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 import nerd.tuxmobil.fahrplan.congress.repositories.ExecutionContext
 
 class ChangeListViewModel(
-
     private val repository: AppRepository,
     private val executionContext: ExecutionContext,
-    private val logging: Logging
-
+    private val screenNavigation: ScreenNavigation,
+    private val sessionChangeParametersFactory: SessionChangeParametersFactory,
 ) : ViewModel() {
 
-    private companion object {
-        const val LOG_TAG = "ChangesListViewModel"
-    }
+    private val mutableSessionChangeState = MutableStateFlow<SessionChangeState>(Loading)
+    val sessionChangesState = mutableSessionChangeState.asStateFlow()
 
-    val changeListParameter: Flow<ChangeListParameter> = repository.changedSessions
-        .map { sessions -> sessions.toChangeListParameter() }
-        .flowOn(executionContext.database)
+    init {
+        repository.changedSessions
+            .onEach { sessions ->
+                mutableSessionChangeState.value = Success(
+                    sessionChangeParametersFactory.createSessionChangeParameters(
+                        sessions = sessions,
+                        numDays = if (sessions.isEmpty()) 0 else repository.readMeta().numDays,
+                        useDeviceTimeZone = repository.readUseDeviceTimeZoneEnabled(),
+                    )
+                )
+            }
+            .launchIn(viewModelScope)
+    }
 
     private val mutableScheduleChangesSeen = Channel<Unit>()
     val scheduleChangesSeen = mutableScheduleChangesSeen.receiveAsFlow()
+
+    fun onViewEvent(viewEvent: SessionChangeViewEvent) {
+        when (viewEvent) {
+            is OnSessionChangeItemClick -> screenNavigation.navigateToSessionDetails(viewEvent.sessionId)
+        }
+    }
 
     fun updateScheduleChangesSeen(changesSeen: Boolean) {
         launch {
             mutableScheduleChangesSeen.sendOneTimeEvent(Unit)
             repository.updateScheduleChangesSeen(changesSeen)
-        }
-    }
-
-    private fun List<Session>.toChangeListParameter(): ChangeListParameter {
-        val numDays = if (isEmpty()) 0 else repository.readMeta().numDays
-        val useDeviceTimeZone = isNotEmpty() && repository.readUseDeviceTimeZoneEnabled()
-        return ChangeListParameter(this, numDays, useDeviceTimeZone).also {
-            logging.d(LOG_TAG, "Loaded $size changed sessions.")
         }
     }
 

app/src/main/java/nerd/tuxmobil/fahrplan/congress/changes/ChangeListViewModelFactory.kt
@@ -2,23 +2,34 @@ package nerd.tuxmobil.fahrplan.congress.changes
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import info.metadude.android.eventfahrplan.commons.logging.Logging
+import info.metadude.android.eventfahrplan.commons.temporal.DateFormatter
+import nerd.tuxmobil.fahrplan.congress.commons.ResourceResolving
+import nerd.tuxmobil.fahrplan.congress.commons.ScreenNavigation
 import nerd.tuxmobil.fahrplan.congress.repositories.AppExecutionContext
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
+import nerd.tuxmobil.fahrplan.congress.utils.ContentDescriptionFormatter
+import nerd.tuxmobil.fahrplan.congress.utils.SessionPropertiesFormatter
 
 class ChangeListViewModelFactory(
-
-    private val appRepository: AppRepository
-
+    private val appRepository: AppRepository,
+    private val resourceResolving: ResourceResolving,
+    private val screenNavigation: ScreenNavigation,
+    private val sessionPropertiesFormatter: SessionPropertiesFormatter,
+    private val contentDescriptionFormatter: ContentDescriptionFormatter,
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val logging = Logging.get()
         @Suppress("UNCHECKED_CAST")
         return ChangeListViewModel(
             repository = appRepository,
             executionContext = AppExecutionContext,
-            logging = logging
+            screenNavigation = screenNavigation,
+            sessionChangeParametersFactory = SessionChangeParametersFactory(
+                resourceResolving = resourceResolving,
+                sessionPropertiesFormatter = sessionPropertiesFormatter,
+                contentDescriptionFormatter = contentDescriptionFormatter,
+                onDateFormatter = { useDeviceTimeZone -> DateFormatter.newInstance(useDeviceTimeZone) }
+            )
         ) as T
     }
 }

app/src/test/java/nerd/tuxmobil/fahrplan/congress/changes/ChangeListViewModelTest.kt
@@ -9,13 +9,19 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
-import nerd.tuxmobil.fahrplan.congress.NoLogging
 import nerd.tuxmobil.fahrplan.congress.TestExecutionContext
+import nerd.tuxmobil.fahrplan.congress.changes.SessionChangeProperty.ChangeState.UNCHANGED
+import nerd.tuxmobil.fahrplan.congress.changes.SessionChangeState.Loading
+import nerd.tuxmobil.fahrplan.congress.changes.SessionChangeState.Success
+import nerd.tuxmobil.fahrplan.congress.changes.SessionChangeViewEvent.OnSessionChangeItemClick
+import nerd.tuxmobil.fahrplan.congress.commons.ScreenNavigation
+import nerd.tuxmobil.fahrplan.congress.commons.VideoRecordingState.None
 import nerd.tuxmobil.fahrplan.congress.models.Meta
 import nerd.tuxmobil.fahrplan.congress.models.Session
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 
@@ -23,45 +29,114 @@ import org.mockito.kotlin.mock
 class ChangeListViewModelTest {
 
     @Test
-    fun `changeListParameter emits null`() = runTest {
-        val repository = createRepository(sessionsFlow = emptyFlow())
+    fun `sessionChangesState initially emits Loading state`() = runTest {
+        val repository = createRepository(changedSessions = emptyFlow())
         val viewModel = createViewModel(repository)
-        viewModel.changeListParameter.test {
-            awaitComplete()
+        viewModel.sessionChangesState.test {
+            assertThat(awaitItem()).isEqualTo(Loading)
+            expectNoEvents()
         }
         verifyInvokedNever(repository).readMeta()
         verifyInvokedNever(repository).readUseDeviceTimeZoneEnabled()
     }
 
     @Test
-    fun `changeListParameter emits zero sessions`() = runTest {
-        val repository = createRepository()
-        val viewModel = createViewModel(repository)
-        val expected = ChangeListParameter(emptyList(), 0, false)
-        viewModel.changeListParameter.test {
+    fun `sessionChangesState emits Success state with empty list`() = runTest {
+        val repository = createRepository(changedSessions = flowOf(emptyList()))
+        val viewModel = createViewModel(repository, factory = createFactory(emptyList()))
+        val expected = Success(emptyList())
+        viewModel.sessionChangesState.test {
             assertThat(awaitItem()).isEqualTo(expected)
-            awaitComplete()
+            expectNoEvents()
         }
-        verifyInvokedNever(repository).readMeta()
-        verifyInvokedNever(repository).readUseDeviceTimeZoneEnabled()
     }
 
     @Test
-    fun `changeListParameter emits a single session`() = runTest {
+    fun `sessionChangesState emits Success state with converted sessions`() = runTest {
+        val session = Session(
+            sessionId = "18",
+            title = "Title",
+            subtitle = "Subtitle",
+            recordingOptOut = false,
+            speakers = listOf("Jane Doe", "John Doe"),
+            dateUTC = 1439478900000L,
+            duration = 30,
+            roomName = "Main room",
+            language = "de, en",
+            dayIndex = 0,
+        )
+        val sessions = listOf(session)
         val repository = createRepository(
-            sessionsFlow = flowOf(listOf(Session("18"))),
-            meta = Meta(numDays = 3),
+            changedSessions = flowOf(sessions),
+            meta = Meta(numDays = 1),
             useDeviceTimeZoneEnabled = true
         )
-        val viewModel = createViewModel(repository)
-        val expectedSessions = listOf(Session("18"))
-        val expected = ChangeListParameter(expectedSessions, 3, true)
-        viewModel.changeListParameter.test {
-            assertThat(awaitItem()).isEqualTo(expected)
-            awaitComplete()
+        val viewModel = createViewModel(repository, factory = createFactory(sessions))
+        val expectedSessionChangeParameter = SessionChangeParameter.SessionChange(
+            id = "18",
+            title = SessionChangeProperty(
+                value = "Title",
+                contentDescription = "",
+                changeState = UNCHANGED
+            ),
+            subtitle = SessionChangeProperty(
+                value = "Subtitle",
+                contentDescription = "",
+                changeState = UNCHANGED
+            ),
+            speakerNames = SessionChangeProperty(
+                value = "Jane Doe, John Doe",
+                contentDescription = "",
+                changeState = UNCHANGED,
+            ),
+            roomName = SessionChangeProperty(
+                value = "Main room",
+                contentDescription = "",
+                changeState = UNCHANGED,
+            ),
+            dayText = SessionChangeProperty(
+                value = "",
+                contentDescription = "",
+                changeState = UNCHANGED,
+            ),
+            startsAt = SessionChangeProperty(
+                value = "",
+                contentDescription = "",
+                changeState = UNCHANGED,
+            ),
+            duration = SessionChangeProperty(
+                value = "30",
+                contentDescription = "",
+                changeState = UNCHANGED,
+            ),
+            languages = SessionChangeProperty(
+                value = "de, en",
+                contentDescription = "",
+                changeState = UNCHANGED,
+            ),
+            videoRecordingState = SessionChangeProperty(
+                value = None,
+                contentDescription = "",
+                changeState = UNCHANGED,
+            ),
+            isCanceled = false,
+        )
+
+        val expectedState = Success(listOf(expectedSessionChangeParameter))
+        viewModel.sessionChangesState.test {
+            assertThat(awaitItem()).isEqualTo(expectedState)
+            expectNoEvents()
         }
         verifyInvokedOnce(repository).readMeta()
         verifyInvokedOnce(repository).readUseDeviceTimeZoneEnabled()
+    }
+
+    @Test
+    fun `onViewEvent(OnSessionChangeItemClick) invokes navigateToSessionDetails`() = runTest {
+        val screenNavigation = mock<ScreenNavigation>()
+        val viewModel = createViewModel(createRepository(), screenNavigation)
+        viewModel.onViewEvent(OnSessionChangeItemClick(sessionId = "42"))
+        verifyInvokedOnce(screenNavigation).navigateToSessionDetails(sessionId = "42")
     }
 
     @Test
@@ -75,22 +150,82 @@ class ChangeListViewModelTest {
         verifyInvokedOnce(repository).updateScheduleChangesSeen(changesSeen = true)
     }
 
+    private fun createFactory(sessions: List<Session>): SessionChangeParametersFactory {
+        val parameters = sessions.map {
+            SessionChangeParameter.SessionChange(
+                id = it.sessionId,
+                title = SessionChangeProperty(
+                    value = it.title,
+                    contentDescription = "",
+                    changeState = UNCHANGED
+                ),
+                subtitle = SessionChangeProperty(
+                    value = it.subtitle,
+                    contentDescription = "",
+                    changeState = UNCHANGED
+                ),
+                speakerNames = SessionChangeProperty(
+                    value = it.speakers.joinToString(),
+                    contentDescription = "",
+                    changeState = UNCHANGED,
+                ),
+                roomName = SessionChangeProperty(
+                    value = it.roomName,
+                    contentDescription = "",
+                    changeState = UNCHANGED,
+                ),
+                dayText = SessionChangeProperty(
+                    value = "",
+                    contentDescription = "",
+                    changeState = UNCHANGED,
+                ),
+                startsAt = SessionChangeProperty(
+                    value = "",
+                    contentDescription = "",
+                    changeState = UNCHANGED,
+                ),
+                duration = SessionChangeProperty(
+                    value = "${it.duration}",
+                    contentDescription = "",
+                    changeState = UNCHANGED,
+                ),
+                languages = SessionChangeProperty(
+                    value = it.language,
+                    contentDescription = "",
+                    changeState = UNCHANGED,
+                ),
+                videoRecordingState = SessionChangeProperty(
+                    value = None,
+                    contentDescription = "",
+                    changeState = UNCHANGED,
+                ),
+                isCanceled = false,
+            )
+        }
+        return mock<SessionChangeParametersFactory> {
+            on { createSessionChangeParameters(anyOrNull(), anyOrNull(), anyOrNull()) } doReturn parameters
+        }
+    }
+
     private fun createRepository(
-        sessionsFlow: Flow<List<Session>> = flowOf(emptyList()),
+        changedSessions: Flow<List<Session>> = flowOf(emptyList()),
         meta: Meta = Meta(numDays = 0),
         useDeviceTimeZoneEnabled: Boolean = false
     ) = mock<AppRepository> {
-        on { changedSessions } doReturn sessionsFlow
-        on { readMeta() } doReturn meta
-        on { readUseDeviceTimeZoneEnabled() } doReturn useDeviceTimeZoneEnabled
+        on { this.changedSessions } doReturn changedSessions
+        on { this.readMeta() } doReturn meta
+        on { this.readUseDeviceTimeZoneEnabled() } doReturn useDeviceTimeZoneEnabled
     }
 
     private fun createViewModel(
         repository: AppRepository,
+        screenNavigation: ScreenNavigation = mock(),
+        factory: SessionChangeParametersFactory = mock(),
     ) = ChangeListViewModel(
-        repository,
-        TestExecutionContext,
-        NoLogging
+        repository = repository,
+        executionContext = TestExecutionContext,
+        screenNavigation = screenNavigation,
+        sessionChangeParametersFactory = factory,
     )
 
 }

app/src/main/java/nerd/tuxmobil/fahrplan/congress/base/SessionsAdapter.kt
@@ -76,8 +76,6 @@ abstract class SessionsAdapter protected constructor(
                         time = rowView.requireViewByIdCompat(R.id.session_list_item_time_view),
                         room = rowView.requireViewByIdCompat(R.id.session_list_item_room_view),
                         duration = rowView.requireViewByIdCompat(R.id.session_list_item_duration_view),
-                        video = rowView.requireViewByIdCompat(R.id.session_list_item_video_view),
-                        noVideo = rowView.requireViewByIdCompat(R.id.session_list_item_no_video_view),
                         withoutVideoRecording = rowView.requireViewByIdCompat(R.id.session_list_item_without_video_recording_view),
                     )
                     rowView.tag = viewHolder
@@ -187,8 +185,6 @@ abstract class SessionsAdapter protected constructor(
         var time: TextView,
         var room: TextView,
         var duration: TextView,
-        var noVideo: ImageView,
-        var video: ImageView,
         var withoutVideoRecording: ImageView
     )
 

commit 5708263f458aa03b9297b7b038a9bc097864950b

Fix broken screen navigation when tapping session change item.
+ When the device was rotated while the alarms screen was visible then
  tapping the session change resulted in no action.
+ The onSessionListClickListener referenced in the ScreenNavigation lambda
  was null once the fragment was recreated.
+ Broken since: f3e530841118a64da1bc473d3a5712d811593848

app/src/main/java/nerd/tuxmobil/fahrplan/congress/changes/ChangeListFragment.kt
@@ -56,12 +56,10 @@ class ChangeListFragment : Fragment() {
     }
 
     private var onSessionListClickListener: OnSessionListClick? = null
-    private lateinit var screenNavigation: ScreenNavigation
     private val viewModelFactory by lazy {
         ChangeListViewModelFactory(
             AppRepository,
             ResourceResolver(requireContext()),
-            screenNavigation,
             SessionPropertiesFormatter(),
             ContentDescriptionFormatter(requireContext()),
         )
@@ -109,11 +107,11 @@ class ChangeListFragment : Fragment() {
     @CallSuper
     override fun onAttach(context: Context) {
         super.onAttach(context)
+        viewModel.screenNavigation = ScreenNavigation { sessionId ->
+            onSessionListClickListener?.onSessionListClick(sessionId)
+        }
         if (context is OnSessionListClick) {
             onSessionListClickListener = context
-            screenNavigation = ScreenNavigation { sessionId ->
-                onSessionListClickListener?.onSessionListClick(sessionId)
-            }
         } else {
             error("$context must implement OnSessionListClick")
         }
@@ -124,6 +122,7 @@ class ChangeListFragment : Fragment() {
     override fun onDetach() {
         super.onDetach()
         onSessionListClickListener = null
+        viewModel.screenNavigation = null
     }
 
 }

app/src/main/java/nerd/tuxmobil/fahrplan/congress/changes/ChangeListViewModel.kt
@@ -21,9 +21,10 @@ import nerd.tuxmobil.fahrplan.congress.repositories.ExecutionContext
 class ChangeListViewModel(
     private val repository: AppRepository,
     private val executionContext: ExecutionContext,
-    private val screenNavigation: ScreenNavigation,
     private val sessionChangeParametersFactory: SessionChangeParametersFactory,
 ) : ViewModel() {
+
+    var screenNavigation: ScreenNavigation? = null
 
     private val mutableSessionChangeState = MutableStateFlow<SessionChangeState>(Loading)
     val sessionChangesState = mutableSessionChangeState.asStateFlow()
@@ -47,7 +48,7 @@ class ChangeListViewModel(
 
     fun onViewEvent(viewEvent: SessionChangeViewEvent) {
         when (viewEvent) {
-            is OnSessionChangeItemClick -> screenNavigation.navigateToSessionDetails(viewEvent.sessionId)
+            is OnSessionChangeItemClick -> screenNavigation?.navigateToSessionDetails(viewEvent.sessionId)
         }
     }
 

app/src/main/java/nerd/tuxmobil/fahrplan/congress/changes/ChangeListViewModelFactory.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider.Factory
 import info.metadude.android.eventfahrplan.commons.temporal.DateFormatter
 import nerd.tuxmobil.fahrplan.congress.commons.ResourceResolving
-import nerd.tuxmobil.fahrplan.congress.commons.ScreenNavigation
 import nerd.tuxmobil.fahrplan.congress.repositories.AppExecutionContext
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 import nerd.tuxmobil.fahrplan.congress.utils.ContentDescriptionFormatter
@@ -13,7 +12,6 @@ import nerd.tuxmobil.fahrplan.congress.utils.SessionPropertiesFormatter
 class ChangeListViewModelFactory(
     private val appRepository: AppRepository,
     private val resourceResolving: ResourceResolving,
-    private val screenNavigation: ScreenNavigation,
     private val sessionPropertiesFormatter: SessionPropertiesFormatter,
     private val contentDescriptionFormatter: ContentDescriptionFormatter,
 ) : Factory {
@@ -23,7 +21,6 @@ class ChangeListViewModelFactory(
         return ChangeListViewModel(
             repository = appRepository,
             executionContext = AppExecutionContext,
-            screenNavigation = screenNavigation,
             sessionChangeParametersFactory = SessionChangeParametersFactory(
                 resourceResolving = resourceResolving,
                 sessionPropertiesFormatter = sessionPropertiesFormatter,

app/src/test/java/nerd/tuxmobil/fahrplan/congress/changes/ChangeListViewModelTest.kt
@@ -134,7 +134,8 @@ class ChangeListViewModelTest {
     @Test
     fun `onViewEvent(OnSessionChangeItemClick) invokes navigateToSessionDetails`() = runTest {
         val screenNavigation = mock<ScreenNavigation>()
-        val viewModel = createViewModel(createRepository(), screenNavigation)
+        val viewModel = createViewModel(createRepository())
+        viewModel.screenNavigation = screenNavigation
         viewModel.onViewEvent(OnSessionChangeItemClick(sessionId = "42"))
         verifyInvokedOnce(screenNavigation).navigateToSessionDetails(sessionId = "42")
     }
@@ -219,12 +220,10 @@ class ChangeListViewModelTest {
 
     private fun createViewModel(
         repository: AppRepository,
-        screenNavigation: ScreenNavigation = mock(),
         factory: SessionChangeParametersFactory = mock(),
     ) = ChangeListViewModel(
         repository = repository,
         executionContext = TestExecutionContext,
-        screenNavigation = screenNavigation,
         sessionChangeParametersFactory = factory,
     )
 