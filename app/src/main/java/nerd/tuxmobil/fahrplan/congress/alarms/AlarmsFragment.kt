EventFahrplan/EventFahrplan

commit 0d3b128a665906df6d34d59b91f3ef6846e34f03

Reimplement session alarms screen.
+ Use Jetpack Compose UI to implement the user interface.
+ Use default Material 3 theming for now. Custom theme has to be
  configured later.
+ Light and dark theme support is not desired at this point.
  Defaults apply and might or not provide good defaults.
+ Bell icon fill and text colors are exposed to be customizable.
+ A close icon in the menu bar is not implemented here, maybe later.
+ The long tap action to delete an alarm has been replaced by a
  trash icon actionable by a single tap.
+ New: Tapping an alarm entry takes you to the session details
  screen.
+ Unused properties of the Alarm data class need to be cleaned up.

Resolves #512

app/src/main/java/nerd/tuxmobil/fahrplan/congress/alarms/AlarmsFragment.kt
@@ -1,0 +1,114 @@
+package nerd.tuxmobil.fahrplan.congress.alarms
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.IdRes
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.viewModels
+import nerd.tuxmobil.fahrplan.congress.R
+import nerd.tuxmobil.fahrplan.congress.base.OnSessionItemClickListener
+import nerd.tuxmobil.fahrplan.congress.commons.ResourceResolver
+import nerd.tuxmobil.fahrplan.congress.commons.ResourceResolving
+import nerd.tuxmobil.fahrplan.congress.commons.ScreenNavigation
+import nerd.tuxmobil.fahrplan.congress.contract.BundleKeys
+import nerd.tuxmobil.fahrplan.congress.extensions.replaceFragment
+import nerd.tuxmobil.fahrplan.congress.extensions.withArguments
+import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
+
+class AlarmsFragment : Fragment() {
+
+    companion object {
+        const val FRAGMENT_TAG = "ALARMS_FRAGMENT_TAG"
+
+        fun replace(
+            fragmentManager: FragmentManager,
+            @IdRes containerViewId: Int,
+            sidePane: Boolean
+        ) {
+            val fragment = AlarmsFragment().withArguments(
+                BundleKeys.SIDEPANE to sidePane
+            )
+            fragmentManager.replaceFragment(containerViewId, fragment, FRAGMENT_TAG, FRAGMENT_TAG)
+        }
+    }
+
+    private lateinit var appRepository: AppRepository
+    private lateinit var resourceResolving: ResourceResolving
+    private lateinit var alarmServices: AlarmServices
+    private lateinit var screenNavigation: ScreenNavigation
+    private val viewModel: AlarmsViewModel by viewModels {
+        AlarmsViewModelFactory(appRepository, resourceResolving, alarmServices, screenNavigation)
+    }
+    private var sidePane = false
+    private var onSessionItemClickListener: OnSessionItemClickListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        appRepository = AppRepository
+        resourceResolving = ResourceResolver(context)
+        alarmServices = AlarmServices.newInstance(context, appRepository)
+        screenNavigation = ScreenNavigation { sessionId ->
+            onSessionItemClickListener?.onSessionItemClick(sessionId)
+        }
+        onSessionItemClickListener = try {
+            context as OnSessionItemClickListener
+        } catch (e: ClassCastException) {
+            throw ClassCastException("$context must implement ClassCastException")
+        }
+    }
+
+    override fun onDetach() {
+        onSessionItemClickListener = null
+        super.onDetach()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            sidePane = it.getBoolean(BundleKeys.SIDEPANE)
+        }
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_alarms, container, false).apply {
+        findViewById<ComposeView>(R.id.alarms_view).apply {
+            setViewCompositionStrategy(DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                AlarmsScreen(
+                    state = viewModel.alarmsState.collectAsState().value,
+                )
+            }
+            isClickable = true
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.alarms_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.menu_item_delete_all_alarms -> {
+            viewModel.onDeleteAllClick()
+            true
+        }
+
+        else -> super.onOptionsItemSelected(item)
+    }
+
+}
+

app/src/main/java/nerd/tuxmobil/fahrplan/congress/alarms/AlarmsViewModel.kt
@@ -1,0 +1,156 @@
+package nerd.tuxmobil.fahrplan.congress.alarms
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import info.metadude.android.eventfahrplan.commons.temporal.DateFormatter
+import info.metadude.android.eventfahrplan.commons.temporal.Moment.Companion.MILLISECONDS_OF_ONE_MINUTE
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.launch
+import nerd.tuxmobil.fahrplan.congress.R
+import nerd.tuxmobil.fahrplan.congress.alarms.AlarmsState.Loading
+import nerd.tuxmobil.fahrplan.congress.alarms.AlarmsState.Success
+import nerd.tuxmobil.fahrplan.congress.commons.ResourceResolving
+import nerd.tuxmobil.fahrplan.congress.commons.ScreenNavigation
+import nerd.tuxmobil.fahrplan.congress.dataconverters.toSchedulableAlarm
+import nerd.tuxmobil.fahrplan.congress.models.SchedulableAlarm
+import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
+import nerd.tuxmobil.fahrplan.congress.repositories.ExecutionContext
+import org.threeten.bp.ZoneOffset
+
+internal class AlarmsViewModel(
+
+    private val repository: AppRepository = AppRepository,
+    private val executionContext: ExecutionContext,
+    private val resourceResolving: ResourceResolving,
+    private val alarmServices: AlarmServices,
+    private val screenNavigation: ScreenNavigation,
+    private val formattingDelegate: FormattingDelegate = DateFormatterDelegate
+
+) : ViewModel() {
+
+    /**
+     * Delegate to get a formatted date/time.
+     */
+    fun interface FormattingDelegate {
+        fun getFormattedDateTimeShort(
+            useDeviceTimeZone: Boolean,
+            alarmTime: Long,
+            timeZoneOffset: ZoneOffset?
+        ): String
+    }
+
+    /**
+     * [DateFormatter] delegate to handle calls to get a formatted date/time.
+     * Do not introduce any business logic here because this class is not unit tested.
+     */
+    @Suppress("kotlin:S6516")
+    private object DateFormatterDelegate : FormattingDelegate {
+        override fun getFormattedDateTimeShort(
+            useDeviceTimeZone: Boolean,
+            alarmTime: Long,
+            timeZoneOffset: ZoneOffset?
+        ) = DateFormatter.newInstance(useDeviceTimeZone)
+            .getFormattedDateTimeLong(alarmTime, timeZoneOffset)
+    }
+
+    private val useDeviceTimeZone: Boolean
+        get() = repository.readUseDeviceTimeZoneEnabled()
+
+    private val mutableAlarmsState = MutableStateFlow<AlarmsState>(Loading)
+    val alarmsState = mutableAlarmsState.asStateFlow()
+
+    init {
+        combine(repository.alarms, repository.sessions) { alarms, sessions ->
+            mutableAlarmsState.value = Success(
+                sessionAlarmParameters = alarms.mapNotNull { alarm ->
+                    sessions
+                        .find { session -> session.sessionId == alarm.sessionId }
+                        ?.let { found ->
+                            val titleContentDescription = resourceResolving.getString(
+                                R.string.session_list_item_title_content_description,
+                                found.title
+                            )
+                            val subtitleContentDescription = resourceResolving.getString(
+                                R.string.session_list_item_subtitle_content_description,
+                                found.subtitle
+                            )
+                            val alarmOffset =
+                                (found.startTimeMilliseconds - alarm.startTime).toMinutes()
+                            val alarmOffsetContentDescription = when (alarmOffset == 0) {
+                                true -> resourceResolving.getString(R.string.alarms_item_alarm_time_zero_minutes_content_description)
+                                false -> resourceResolving.getString(
+                                    R.string.alarms_item_alarm_time_minutes_content_description,
+                                    alarmOffset
+                                )
+                            }
+                            val firesAtText = formattingDelegate.getFormattedDateTimeShort(
+                                useDeviceTimeZone,
+                                alarm.startTime,
+                                found.timeZoneOffset
+                            )
+                            val firesAtContentDescription = resourceResolving.getString(
+                                R.string.alarms_item_fires_at_content_description,
+                                firesAtText
+                            )
+
+                            SessionAlarmParameter(
+                                sessionId = found.sessionId,
+                                title = found.title,
+                                titleContentDescription = titleContentDescription,
+                                subtitle = found.subtitle,
+                                subtitleContentDescription = subtitleContentDescription,
+                                alarmOffsetInMin = alarmOffset,
+                                alarmOffsetContentDescription = alarmOffsetContentDescription,
+                                firesAt = alarm.startTime,
+                                firesAtText = firesAtText,
+                                firesAtContentDescription = firesAtContentDescription,
+                                dayIndex = found.day
+                            )
+                        }
+                },
+                onItemClick = ::navigateToSessionDetails,
+                onDeleteItemClick = ::deleteSessionAlarm
+            )
+        }.launchIn(viewModelScope)
+    }
+
+    private fun navigateToSessionDetails(value: SessionAlarmParameter) {
+        screenNavigation.navigateToSessionDetails(value.sessionId)
+    }
+
+    private fun deleteSessionAlarm(value: SessionAlarmParameter) {
+        launch {
+            repository.deleteAlarmForSessionId(value.sessionId)
+            val alarm = SchedulableAlarm(
+                day = value.dayIndex,
+                sessionId = value.sessionId,
+                sessionTitle = value.title,
+                startTime = value.firesAt
+            )
+            alarmServices.discardSessionAlarm(alarm)
+        }
+    }
+
+    fun onDeleteAllClick() {
+        launch {
+            val alarms = repository.readAlarms()
+            alarms
+                .map { it.toSchedulableAlarm() }
+                .forEach { alarmServices.discardSessionAlarm(it) }
+            if (alarms.isNotEmpty()) {
+                repository.deleteAllAlarms()
+            }
+        }
+    }
+
+    private fun launch(block: suspend CoroutineScope.() -> Unit) {
+        viewModelScope.launch(executionContext.database, block = block)
+    }
+
+    private fun Long.toMinutes() = (this / MILLISECONDS_OF_ONE_MINUTE).toInt()
+
+}

app/src/main/java/nerd/tuxmobil/fahrplan/congress/alarms/AlarmsViewModelFactory.kt
@@ -1,0 +1,30 @@
+package nerd.tuxmobil.fahrplan.congress.alarms
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider.Factory
+import nerd.tuxmobil.fahrplan.congress.commons.ResourceResolving
+import nerd.tuxmobil.fahrplan.congress.commons.ScreenNavigation
+import nerd.tuxmobil.fahrplan.congress.repositories.AppExecutionContext
+import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
+
+internal class AlarmsViewModelFactory(
+
+    private val appRepository: AppRepository,
+    private val resourceResolving: ResourceResolving,
+    private val alarmServices: AlarmServices,
+    private val screenNavigation: ScreenNavigation,
+
+    ) : Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        @Suppress("UNCHECKED_CAST")
+        return AlarmsViewModel(
+            repository = appRepository,
+            executionContext = AppExecutionContext,
+            resourceResolving = resourceResolving,
+            alarmServices = alarmServices,
+            screenNavigation = screenNavigation,
+        ) as T
+    }
+
+}

app/src/test/java/nerd/tuxmobil/fahrplan/congress/alarms/AlarmsViewModelTest.kt
@@ -1,0 +1,261 @@
+package nerd.tuxmobil.fahrplan.congress.alarms
+
+import app.cash.turbine.ReceiveTurbine
+import app.cash.turbine.test
+import com.google.common.truth.Truth.assertThat
+import info.metadude.android.eventfahrplan.commons.temporal.Moment
+import info.metadude.android.eventfahrplan.commons.testing.MainDispatcherTestRule
+import info.metadude.android.eventfahrplan.commons.testing.verifyInvokedNever
+import info.metadude.android.eventfahrplan.commons.testing.verifyInvokedOnce
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.test.runTest
+import nerd.tuxmobil.fahrplan.congress.TestExecutionContext
+import nerd.tuxmobil.fahrplan.congress.alarms.AlarmsState.Loading
+import nerd.tuxmobil.fahrplan.congress.alarms.AlarmsState.Success
+import nerd.tuxmobil.fahrplan.congress.commons.ResourceResolving
+import nerd.tuxmobil.fahrplan.congress.commons.ScreenNavigation
+import nerd.tuxmobil.fahrplan.congress.models.Alarm
+import nerd.tuxmobil.fahrplan.congress.models.Session
+import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doNothing
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+
+class AlarmsViewModelTest {
+
+    private companion object {
+        const val CONTENT_DESCRIPTION = "Some content description"
+        const val FIRES_AT_TEXT = "***"
+        val SESSION_STARTS_AT = Moment.ofEpochMilli(1683981000000) // 2023-05-13T12:30:00+00:00
+        const val ALARM_TIME_IN_MIN = 10
+        val ALARM_STARTS_AT = SESSION_STARTS_AT.minusMinutes(ALARM_TIME_IN_MIN.toLong())
+
+    }
+
+    @get:Rule
+    val mainDispatcherTestRule = MainDispatcherTestRule()
+
+    @Test
+    fun `alarmsState emits Loading`() = runTest {
+        val repository = createRepository(
+            alarmsList = emptyList(),
+            sessionsFlow = emptyFlow()
+        )
+        val viewModel = createViewModel(repository)
+        viewModel.alarmsState.test {
+            assertThat(awaitItem()).isEqualTo(Loading)
+        }
+        verifyInvokedNever(repository).readUseDeviceTimeZoneEnabled()
+    }
+
+    @Test
+    fun `alarmsState emits empty values Success when alarms emits empty list`() = runTest {
+        val repository = createRepository(
+            alarmsList = emptyList(),
+            sessionsFlow = flowOf(listOf(Session("s0")))
+        )
+        val viewModel = createViewModel(repository)
+        viewModel.alarmsState.test {
+            assertThat(awaitSuccess().sessionAlarmParameters).isEqualTo(emptyList<SessionAlarmParameter>())
+        }
+    }
+
+    @Test
+    fun `alarmsState emits empty values Success when sessions emits empty list`() = runTest {
+        val repository = createRepository(
+            alarmsList = listOf(createAlarm("s0")),
+            sessionsFlow = flowOf(emptyList())
+        )
+        val viewModel = createViewModel(repository)
+        viewModel.alarmsState.test {
+            assertThat(awaitSuccess().sessionAlarmParameters).isEqualTo(emptyList<SessionAlarmParameter>())
+        }
+    }
+
+    @Test
+    fun `alarmsState emits empty values Success when alarm and session are not associated`() =
+        runTest {
+            val repository = createRepository(
+                alarmsList = listOf(createAlarm("s1")),
+                sessionsFlow = flowOf(listOf(Session("s0")))
+            )
+            val viewModel = createViewModel(repository)
+            viewModel.alarmsState.test {
+                assertThat(awaitSuccess().sessionAlarmParameters).isEqualTo(emptyList<SessionAlarmParameter>())
+            }
+        }
+
+    @Test
+    fun `alarmsState emits values list Success when alarm and session are associated`() = runTest {
+        val repository = createRepository(
+            alarmsList = listOf(
+                createAlarm(
+                    sessionId = "s0",
+                    alarmTimeInMin = ALARM_TIME_IN_MIN,
+                    alarmStartsAt = ALARM_STARTS_AT.toMilliseconds()
+                )
+            ),
+            sessionsFlow = flowOf(listOf(Session("s0").apply {
+                title = "Title"
+                subtitle = "Subtitle"
+                dateUTC = SESSION_STARTS_AT.toMilliseconds()
+            }))
+        )
+        val viewModel = createViewModel(repository)
+        viewModel.alarmsState.test {
+            val parameter = SessionAlarmParameter(
+                sessionId = "s0",
+                title = "Title",
+                titleContentDescription = CONTENT_DESCRIPTION,
+                subtitle = "Subtitle",
+                subtitleContentDescription = CONTENT_DESCRIPTION,
+                alarmOffsetContentDescription = CONTENT_DESCRIPTION,
+                alarmOffsetInMin = ALARM_TIME_IN_MIN,
+                firesAt = ALARM_STARTS_AT.toMilliseconds(),
+                firesAtText = FIRES_AT_TEXT,
+                firesAtContentDescription = CONTENT_DESCRIPTION,
+                dayIndex = 0,
+                selected = false
+            )
+            assertThat(awaitSuccess().sessionAlarmParameters).isEqualTo(listOf(parameter))
+        }
+        verifyInvokedOnce(repository).readUseDeviceTimeZoneEnabled()
+    }
+
+    @Test
+    fun `onItemClick triggers navigation to session details`() = runTest {
+        val repository = createRepository(
+            alarmsList = listOf(
+                createAlarm(
+                    sessionId = "s0",
+                    alarmTimeInMin = ALARM_TIME_IN_MIN,
+                    alarmStartsAt = ALARM_STARTS_AT.toMilliseconds()
+                )
+            ),
+            sessionsFlow = flowOf(listOf(Session("s0").apply {
+                title = "Title"
+                subtitle = "Subtitle"
+                dateUTC = SESSION_STARTS_AT.toMilliseconds()
+            }))
+        )
+        val screenNavigation = mock<ScreenNavigation>()
+        doNothing().`when`(screenNavigation).navigateToSessionDetails(any())
+        val viewModel = createViewModel(repository, screenNavigation = screenNavigation)
+        viewModel.alarmsState.take(1).collect { state ->
+            assertThat(state).isInstanceOf(Success::class.java)
+            val success = state as Success
+            val sessionAlarmParameter = state.sessionAlarmParameters.first()
+            success.onItemClick(sessionAlarmParameter)
+            verifyInvokedOnce(screenNavigation).navigateToSessionDetails(any())
+        }
+    }
+
+    @Test
+    fun `onDeleteItemClick invokes corresponding repository and alarm services functions`() =
+        runTest {
+            val repository = createRepository(
+                alarmsList = listOf(
+                    createAlarm(
+                        sessionId = "s0",
+                        alarmTimeInMin = ALARM_TIME_IN_MIN,
+                        alarmStartsAt = ALARM_STARTS_AT.toMilliseconds()
+                    )
+                ),
+                sessionsFlow = flowOf(listOf(Session("s0").apply {
+                    title = "Title"
+                    subtitle = "Subtitle"
+                    dateUTC = SESSION_STARTS_AT.toMilliseconds()
+                }))
+            )
+            val alarmServices = mock<AlarmServices>()
+            val viewModel = createViewModel(repository, alarmServices)
+            viewModel.alarmsState.take(1).collect { state ->
+                assertThat(state).isInstanceOf(Success::class.java)
+                val success = state as Success
+                val sessionAlarmParameter = state.sessionAlarmParameters.first()
+                success.onDeleteItemClick(sessionAlarmParameter)
+                verifyInvokedOnce(repository).deleteAlarmForSessionId(any())
+                verifyInvokedOnce(alarmServices).discardSessionAlarm(any())
+            }
+        }
+
+    @Test
+    fun `onDeleteAllClick never invokes repository nor alarm services functions when no alarm is present`() =
+        runTest {
+            val alarmServices = mock<AlarmServices>()
+            val repository = createRepository(
+                alarmsList = emptyList()
+            )
+            val viewModel = createViewModel(repository, alarmServices)
+            viewModel.onDeleteAllClick()
+            verifyInvokedOnce(repository).readAlarms()
+            verifyInvokedNever(alarmServices).discardSessionAlarm(any())
+            verifyInvokedNever(repository).deleteAllAlarms()
+        }
+
+    @Test
+    fun `onDeleteAllClick invokes corresponding repository and alarm services functions when any alarm is present`() =
+        runTest {
+            val alarmServices = mock<AlarmServices>()
+            val repository = createRepository(
+                alarmsList = listOf(createAlarm("s0"))
+            )
+            val viewModel = createViewModel(repository, alarmServices)
+            viewModel.onDeleteAllClick()
+            verifyInvokedOnce(repository).readAlarms()
+            verifyInvokedOnce(alarmServices).discardSessionAlarm(any())
+            verifyInvokedOnce(repository).deleteAllAlarms()
+        }
+
+    private fun createRepository(
+        sessionsFlow: Flow<List<Session>> = flowOf(emptyList()),
+        alarmsList: List<Alarm> = emptyList()
+    ) = mock<AppRepository> {
+        on { alarms } doReturn flowOf(alarmsList)
+        on { sessions } doReturn sessionsFlow
+        on { readAlarms(any()) } doReturn alarmsList
+        on { readUseDeviceTimeZoneEnabled() } doReturn true
+        on { deleteAlarmForSessionId(any()) } doReturn 0
+        on { deleteAllAlarms() } doReturn 0
+    }
+
+    private fun createViewModel(
+        repository: AppRepository,
+        alarmServices: AlarmServices = mock(),
+        screenNavigation: ScreenNavigation = mock(),
+    ) = AlarmsViewModel(
+        repository = repository,
+        executionContext = TestExecutionContext,
+        alarmServices = alarmServices,
+        resourceResolving = TestResourceResolver,
+        screenNavigation = screenNavigation,
+        formattingDelegate = { _, _, _ -> FIRES_AT_TEXT }
+    )
+
+    private object TestResourceResolver : ResourceResolving {
+        override fun getString(id: Int, vararg formatArgs: Any) = CONTENT_DESCRIPTION
+    }
+
+    private fun createAlarm(
+        sessionId: String,
+        alarmTimeInMin: Int = 10,
+        alarmStartsAt: Long = 1620909000000
+    ) = Alarm(
+        alarmTimeInMin = alarmTimeInMin,
+        day = 2,
+        displayTime = -1,
+        sessionId = sessionId,
+        sessionTitle = "Unused",
+        startTime = alarmStartsAt,
+        timeText = "Unused"
+    )
+
+    private suspend fun <T> ReceiveTurbine<T>.awaitSuccess() = awaitItem() as Success
+
+}

commit e346bbd5dd695369ba64bfdd47281d09ff277931

Fix broken screen navigation when tapping session alarm item.
+ When the device was rotated while the alarms screen was visible then
  tapping the session alarm resulted in no action.
+ The onSessionItemClickListener referenced in the ScreenNavigation lambda
  was null once the fragment was recreated.
+ Broken since: 0d3b128a665906df6d34d59b91f3ef6846e34f03

app/src/main/java/nerd/tuxmobil/fahrplan/congress/alarms/AlarmsFragment.kt
@@ -45,9 +45,8 @@ class AlarmsFragment : Fragment() {
     private lateinit var appRepository: AppRepository
     private lateinit var resourceResolving: ResourceResolving
     private lateinit var alarmServices: AlarmServices
-    private lateinit var screenNavigation: ScreenNavigation
     private val viewModel: AlarmsViewModel by viewModels {
-        AlarmsViewModelFactory(appRepository, resourceResolving, alarmServices, screenNavigation)
+        AlarmsViewModelFactory(appRepository, resourceResolving, alarmServices)
     }
     private var sidePane = false
     private var onSessionItemClickListener: OnSessionItemClickListener? = null
@@ -57,7 +56,7 @@ class AlarmsFragment : Fragment() {
         appRepository = AppRepository
         resourceResolving = ResourceResolver(context)
         alarmServices = AlarmServices.newInstance(context, appRepository)
-        screenNavigation = ScreenNavigation { sessionId ->
+        viewModel.screenNavigation = ScreenNavigation { sessionId ->
             onSessionItemClickListener?.onSessionItemClick(sessionId)
         }
         onSessionItemClickListener = try {
@@ -69,6 +68,7 @@ class AlarmsFragment : Fragment() {
 
     override fun onDetach() {
         onSessionItemClickListener = null
+        viewModel.screenNavigation = null
         super.onDetach()
     }
 

app/src/main/java/nerd/tuxmobil/fahrplan/congress/alarms/AlarmsViewModel.kt
@@ -21,10 +21,11 @@ internal class AlarmsViewModel(
     private val repository: AppRepository = AppRepository,
     private val executionContext: ExecutionContext,
     private val alarmServices: AlarmServices,
-    private val screenNavigation: ScreenNavigation,
     private val alarmsStateFactory: AlarmsStateFactory,
 
 ) : ViewModel() {
+
+    var screenNavigation: ScreenNavigation? = null
 
     private val useDeviceTimeZone: Boolean
         get() = repository.readUseDeviceTimeZoneEnabled()
@@ -44,7 +45,7 @@ internal class AlarmsViewModel(
     }
 
     private fun navigateToSessionDetails(value: SessionAlarmParameter) {
-        screenNavigation.navigateToSessionDetails(value.sessionId)
+        screenNavigation?.navigateToSessionDetails(value.sessionId)
     }
 
     private fun deleteSessionAlarm(value: SessionAlarmParameter) {

app/src/main/java/nerd/tuxmobil/fahrplan/congress/alarms/AlarmsViewModelFactory.kt
@@ -3,7 +3,6 @@ package nerd.tuxmobil.fahrplan.congress.alarms
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider.Factory
 import nerd.tuxmobil.fahrplan.congress.commons.ResourceResolving
-import nerd.tuxmobil.fahrplan.congress.commons.ScreenNavigation
 import nerd.tuxmobil.fahrplan.congress.repositories.AppExecutionContext
 import nerd.tuxmobil.fahrplan.congress.repositories.AppRepository
 
@@ -11,7 +10,6 @@ internal class AlarmsViewModelFactory(
     private val appRepository: AppRepository,
     private val resourceResolving: ResourceResolving,
     private val alarmServices: AlarmServices,
-    private val screenNavigation: ScreenNavigation,
 ) : Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
@@ -20,7 +18,6 @@ internal class AlarmsViewModelFactory(
             repository = appRepository,
             executionContext = AppExecutionContext,
             alarmServices = alarmServices,
-            screenNavigation = screenNavigation,
             alarmsStateFactory = AlarmsStateFactory(resourceResolving),
         ) as T
     }

app/src/test/java/nerd/tuxmobil/fahrplan/congress/alarms/AlarmsViewModelTest.kt
@@ -99,7 +99,8 @@ class AlarmsViewModelTest {
         )
         val screenNavigation = mock<ScreenNavigation>()
         doNothing().`when`(screenNavigation).navigateToSessionDetails(any())
-        val viewModel = createViewModel(repository, screenNavigation = screenNavigation)
+        val viewModel = createViewModel(repository)
+        viewModel.screenNavigation = screenNavigation
         viewModel.alarmsState.take(1).collect { state ->
             assertThat(state).isInstanceOf(Success::class.java)
             val success = state as Success
@@ -187,12 +188,10 @@ class AlarmsViewModelTest {
     private fun createViewModel(
         repository: AppRepository,
         alarmServices: AlarmServices = mock(),
-        screenNavigation: ScreenNavigation = mock(),
     ) = AlarmsViewModel(
         repository = repository,
         executionContext = TestExecutionContext,
         alarmServices = alarmServices,
-        screenNavigation = screenNavigation,
         alarmsStateFactory = createAlarmsStateFactory(),
     )
 