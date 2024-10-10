0d0103eb32e8b47d600910e13fe11e0ecdfff5f2
Tobias Preuss <tobias.preuss@googlemail.com>
2024-10-10 22:16:40 +0200

    Use mockito v.5.14.2.
    + Changelog: https://github.com/mockito/mockito/compare/v5.13.0...v5.14.2

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -67,7 +67,7 @@ object Libs {
         const val lifecycle = "2.8.5"
         const val markwon = "4.6.2"
         const val material = "1.12.0"
-        const val mockito = "5.13.0"
+        const val mockito = "5.14.2"
         const val mockitoKotlin = "5.4.0"
         const val moshi = "1.15.1"
         const val okhttp = "4.12.0"
