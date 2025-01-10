c853f298d621e56335e0163aec21066c890201a8
Tobias Preuss <tobias.preuss@googlemail.com>
2025-01-10 12:32:07 +0100

    Use moshi v.1.15.2.
    + Changelog: https://github.com/square/moshi/blob/master/CHANGELOG.md#version-1152

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -71,7 +71,7 @@ object Libs {
         const val material = "1.12.0"
         const val mockito = "5.15.2"
         const val mockitoKotlin = "5.4.0"
-        const val moshi = "1.15.1"
+        const val moshi = "1.15.2"
         const val okhttp = "4.12.0"
         const val preference = "1.2.1"
         const val retrofit = "2.11.0"
