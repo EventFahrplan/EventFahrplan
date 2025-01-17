8a2af9601a041f93b5ab4c45235cd063af38a08b
Tobias Preuss <tobias.preuss@googlemail.com>
2025-01-17 16:51:32 +0100

    Extract design system composables from "schedule changes" screen.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/changes/SessionChangeComposables.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/changes/SessionChangeComposables.kt
@@ -12,9 +12,6 @@ import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
@@ -25,7 +22,6 @@ import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextDecoration.Companion.LineThrough
-import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,6 +45,10 @@ import nerd.tuxmobil.fahrplan.congress.commons.SessionListHeader
 import nerd.tuxmobil.fahrplan.congress.commons.VideoRecordingIcon
 import nerd.tuxmobil.fahrplan.congress.commons.VideoRecordingState.Drawable.Available
 import nerd.tuxmobil.fahrplan.congress.commons.VideoRecordingState.Drawable.Unavailable
+import nerd.tuxmobil.fahrplan.congress.designsystem.dividers.DividerHorizontal
+import nerd.tuxmobil.fahrplan.congress.designsystem.templates.Scaffold
+import nerd.tuxmobil.fahrplan.congress.designsystem.texts.TextHeadlineContent
+import nerd.tuxmobil.fahrplan.congress.designsystem.texts.TextSupportingContent
 
 @Composable
 internal fun SessionChangesScreen(
@@ -114,7 +114,7 @@ private fun SessionChangesList(
                     })
                     val next = parameters.getOrNull(index + 1)
                     if (index < parameters.size - 1 && (next != null && next !is Separator)) {
-                        HorizontalDivider(Modifier.padding(horizontal = 12.dp))
+                        DividerHorizontal(Modifier.padding(horizontal = 12.dp))
                     }
                 }
             }
@@ -139,7 +139,7 @@ fun SessionChangeItem(
         ) {
             val titleColor = session.title.changeState.color()
             val textDecoration = textDecorationOf(session.title)
-            Text(
+            TextHeadlineContent(
                 modifier = Modifier
                     .weight(1f)
                     .semantics {
@@ -150,8 +150,6 @@ fun SessionChangeItem(
                 fontWeight = Bold,
                 color = colorResource(titleColor),
                 textDecoration = textDecoration,
-                maxLines = 1,
-                overflow = Ellipsis,
             )
             val iconColor = session.videoRecordingState.changeState.color()
             VideoRecordingIcon(
@@ -201,7 +199,7 @@ private fun SecondaryText(
     if (property.value.isNotEmpty()) {
         val color = property.changeState.color()
         val textDecoration = textDecorationOf(property)
-        Text(
+        TextSupportingContent(
             modifier = modifier.semantics {
                 contentDescription = property.contentDescription
             },
@@ -209,8 +207,6 @@ private fun SecondaryText(
             fontSize = 13.sp,
             color = colorResource(color),
             textDecoration = textDecoration,
-            maxLines = 1,
-            overflow = Ellipsis,
         )
     }
 }
--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/texts/TextHeadlineContent.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/texts/TextHeadlineContent.kt
@@ -2,17 +2,39 @@ package nerd.tuxmobil.fahrplan.congress.designsystem.texts
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.material3.Text as Material3Text
 
+@Suppress("kotlin:S107")
 @Composable
 fun TextHeadlineContent(
     text: String,
     modifier: Modifier = Modifier,
+    textAlign: TextAlign? = null,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    fontFamily: FontFamily? = null,
+    textDecoration: TextDecoration? = null,
+    color: Color = Color.Unspecified,
 ) {
     Material3Text(
         text = text,
         modifier = modifier,
+        color = color,
+        fontSize = fontSize,
+        fontStyle = fontStyle,
+        fontWeight = fontWeight,
+        fontFamily = fontFamily,
+        textAlign = textAlign,
+        textDecoration = textDecoration,
         overflow = TextOverflow.Ellipsis,
         maxLines = 1,
     )
