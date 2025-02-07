0d7986e692c32ce32a221a7260da7988f446d0b5
Tobias Preuss <tobias.preuss@googlemail.com>
2025-02-07 23:02:27 +0100

    Use kotlin v.2.1.10 & ksp v.2.1.10-1.0.29.
    + Changelogs:
      https://github.com/JetBrains/kotlin/releases/tag/v2.1.10
      https://github.com/google/ksp/releases/tag/2.1.10-1.0.29

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -33,8 +33,8 @@ object Plugins {
     private object Versions {
         const val android = "8.8.0"
         const val dexcount = "4.0.0"
-        const val kotlin = "2.1.0"
-        const val ksp = "2.1.0-1.0.29"
+        const val kotlin = "2.1.10"
+        const val ksp = "2.1.10-1.0.29"
         const val sonarQube = "5.1.0.4882" // Breaks CI build as of 6.x. See https://community.sonarsource.com/t/sonarqube-gradle-plugin-6-0-breaks-android-tasks/130863
         const val unMock = "0.9.0"
         const val versions = "0.52.0"
