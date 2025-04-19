EventFahrplan/EventFahrplan

commit 0d5affe6e4ac3750447525d9b257466a5748ca4c

Increase space for session card content.
+ This makes the text of short sessions with of 5 minutes actually
  readable and their cards high enough to touch comfortably.
+ The new default height is 56dp per 15 minutes.

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/FahrplanFragment.kt
@@ -527,7 +527,7 @@ class FahrplanFragment : Fragment(), MenuProvider, SessionViewEventsHandler {
         }
 
     private fun getNormalizedBoxHeight(): Int {
-        return (resources.getInteger(R.integer.box_height) * displayDensityScale).toInt()
+        return resources.getDimension(R.dimen.schedule_time_column_box_height).toInt()
     }
 
     override fun onClick(view: View) {

app/src/main/res/values-land/dimens.xml
@@ -17,7 +17,6 @@
     <dimen name="actionbar_height">35dip</dimen>
     <dimen name="actionbar_item_height">35dip</dimen>
     <dimen name="actionbar_item_width">35dip</dimen>
-    <item type="integer" name="box_height">10</item>
     <item type="integer" name="max_cols">4</item>
     <item type="integer" name="min_width_dip">140</item>
     <item type="integer" name="room_title_size">12</item>

app/src/main/res/values-sw600dp-port/dimens.xml
@@ -14,7 +14,6 @@
      limitations under the License.
 -->
 <resources>
-    <item type="integer" name="box_height">18</item>
     <item type="integer" name="max_cols">4</item>
     <dimen name="list_pane_leftright_padding">64dp</dimen>
     <dimen name="header_height">16dp</dimen>

app/src/main/res/values-sw600dp/dimens.xml
@@ -14,7 +14,6 @@
      limitations under the License.
 -->
 <resources>
-    <item type="integer" name="box_height">13</item>
     <item type="integer" name="max_cols">4</item>
     <item type="integer" name="min_width_dip">160</item>
     <item type="integer" name="room_title_size">16</item>

app/src/main/res/values-sw720dp/dimens.xml
@@ -14,7 +14,6 @@
      limitations under the License.
 -->
 <resources>
-    <item type="integer" name="box_height">13</item>
     <item type="integer" name="max_cols">6</item>
     <dimen name="list_pane_leftright_padding">32dp</dimen>
     <dimen name="header_height">32dp</dimen>

app/src/main/res/values-xlarge-port/dimens.xml
@@ -1,18 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!-- Copyright (C) 2010 Johan Nilsson <http://markupartist.com>
-
-     Licensed under the Apache License, Version 2.0 (the "License");
-     you may not use this file except in compliance with the License.
-     You may obtain a copy of the License at
-  
-          http://www.apache.org/licenses/LICENSE-2.0
-  
-     Unless required by applicable law or agreed to in writing, software
-     distributed under the License is distributed on an "AS IS" BASIS,
-     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     See the License for the specific language governing permissions and
-     limitations under the License.
--->
-<resources>
-    <item type="integer" name="box_height">18</item>
-</resources>

app/src/main/res/values-xlarge/dimens.xml
@@ -14,7 +14,6 @@
      limitations under the License.
 -->
 <resources>
-    <item type="integer" name="box_height">13</item>
 
     <!-- Session details text sizes -->
     <dimen name="session_detailbar_text">14sp</dimen>

app/src/main/res/values/dimens.xml
@@ -13,7 +13,6 @@
      limitations under the License.
 -->
 <resources>
-    <item name="box_height" type="integer">13</item>
     <item name="max_cols" type="integer">1</item>
     <item name="min_width_dip" type="integer">160</item>
     <item name="room_title_size" type="integer">14</item>
@@ -100,5 +99,6 @@
     <dimen name="schedule_time_column_layout_width">38dp</dimen>
     <dimen name="schedule_time_column_padding">3dp</dimen>
     <dimen name="schedule_time_column_text_size">11sp</dimen>
+    <dimen name="schedule_time_column_box_height">19dp</dimen> <!-- 56/3 -->
 
 </resources>

commit 5c0dbfd8355cc0c7cf2c5fc3126c801312cc0847

Fix multiple room columns not rendering when device is rotated.
+ Visible at 10" tablets in portrait mode (expect 4 columns) and smartphones
  in landscape mode (expect 4 columns).
+ Broken since: 0d5affe6e4ac3750447525d9b257466a5748ca4c resp.
  87fb2746f252fbffefcf5ae0bfb41dc119377747
+ Decouple creating/updating days menu from creating/updating session cards.
+ Update day menu selection after it has been built.
+ Collect uncanceledSessionsForDayIndex once for schedule updates.
+ Do render setting/removing alarms only because Session#hasAlarm and
  highlight are part of equals/hashCode. See 9c5b09d14a6736c34790fcdf754c68be73113241
  and this commit which replaces Flow property with MutableStateFlow property.

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/FahrplanFragment.kt
@@ -230,13 +230,16 @@ class FahrplanFragment : Fragment(), MenuProvider {
     @SuppressLint("InlinedApi")
     private fun observeViewModel() {
         viewModel.fahrplanParameter
-            .observe(this) { (scheduleData, useDeviceTimeZone, numDays, dayIndex, menuEntries) ->
+            .observe(this) { (scheduleData, useDeviceTimeZone) ->
                 hideNoScheduleView()
-                buildNavigationMenu(menuEntries, numDays)
                 viewModel.fillTimes(Moment.now(), getNormalizedBoxHeight())
-                viewDay(scheduleData, useDeviceTimeZone, numDays, dayIndex)
+                viewDay(scheduleData, useDeviceTimeZone)
                 updateHorizontalScrollingProgressLine(0)
             }
+        viewModel.dayMenuParameter.observe(this) { parameter ->
+            buildNavigationMenu(parameter.dayMenuEntries)
+            updateNavigationMenuSelection(numDays = parameter.dayMenuEntries.size, dayIndex = parameter.displayDayIndex)
+        }
         viewModel.showHorizontalScrollingProgressLine.observe(this) { shouldShow ->
             updateHorizontalScrollingProgressLine(shouldShow)
         }
@@ -339,7 +342,7 @@ class FahrplanFragment : Fragment(), MenuProvider {
     /**
      * Updates the session data in the schedule view.
      */
-    private fun viewDay(scheduleData: ScheduleData, useDeviceTimeZone: Boolean, numDays: Int, dayIndex: Int) {
+    private fun viewDay(scheduleData: ScheduleData, useDeviceTimeZone: Boolean) {
         val layoutRoot = requireView()
         val horizontalScroller = layoutRoot.requireViewByIdCompat<HorizontalSnapScrollView>(R.id.horizScroller)
         horizontalScroller.scrollTo(0, 0)
@@ -359,7 +362,6 @@ class FahrplanFragment : Fragment(), MenuProvider {
                 viewModel.preserveVerticalScrollPosition = false
             }
         }
-        updateNavigationMenuSelection(numDays, dayIndex)
     }
 
     private fun updateNavigationMenuSelection(numDays: Int, dayIndex: Int) {
@@ -598,7 +600,7 @@ class FahrplanFragment : Fragment(), MenuProvider {
      * The [dayMenuEntries] can be passed both as an empty list or a list with entries.
      * The empty list is important for [updateNavigationMenuSelection] to work correctly.
      */
-    private fun buildNavigationMenu(dayMenuEntries: List<String>, numDays: Int) {
+    private fun buildNavigationMenu(dayMenuEntries: List<String>) {
         val actionBar = (requireActivity() as AppCompatActivity).supportActionBar
         actionBar!!.navigationMode = NAVIGATION_MODE_LIST
         val arrayAdapter = ArrayAdapter(
@@ -607,7 +609,7 @@ class FahrplanFragment : Fragment(), MenuProvider {
             dayMenuEntries
         )
         arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_list_item)
-        actionBar.setListNavigationCallbacks(arrayAdapter, OnDaySelectedListener(numDays))
+        actionBar.setListNavigationCallbacks(arrayAdapter, OnDaySelectedListener(dayMenuEntries.size))
     }
 
     private fun showAlarmTimePicker() {

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/FahrplanViewModel.kt
@@ -11,7 +11,8 @@ import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.filterNotNull
@@ -33,6 +34,7 @@ import nerd.tuxmobil.fahrplan.congress.repositories.LoadScheduleState.Fetching
 import nerd.tuxmobil.fahrplan.congress.repositories.LoadScheduleState.InitialFetching
 import nerd.tuxmobil.fahrplan.congress.repositories.LoadScheduleState.InitialParsing
 import nerd.tuxmobil.fahrplan.congress.repositories.LoadScheduleState.Parsing
+import nerd.tuxmobil.fahrplan.congress.schedule.observables.DayMenuParameter
 import nerd.tuxmobil.fahrplan.congress.schedule.observables.FahrplanEmptyParameter
 import nerd.tuxmobil.fahrplan.congress.schedule.observables.FahrplanParameter
 import nerd.tuxmobil.fahrplan.congress.schedule.observables.ScrollToCurrentSessionParameter
@@ -72,15 +74,11 @@ internal class FahrplanViewModel(
             runsAtLeastOnAndroidTiramisu,
         )
 
-    val fahrplanParameter = combine(
-        repository.uncanceledSessionsForDayIndex.filter { it.allSessions.isNotEmpty() },
-        repository.sessionsWithoutShifts.filterNotNull(),
-    ) { scheduleDataForDayIndex, allSessionsForAllDaysWithoutShifts ->
-        createFahrplanParameter(
-            scheduleData = scheduleDataForDayIndex.customizeEngelsystemRoomName(),
-            allSessionsForAllDaysWithoutShifts = allSessionsForAllDaysWithoutShifts,
-        )
-    }
+    private val mutableDayMenuParameter = MutableStateFlow(DayMenuParameter())
+    val dayMenuParameter = mutableDayMenuParameter.asStateFlow().filter { it.isValid }
+
+    private val mutableFahrplanParameter = MutableStateFlow<FahrplanParameter?>(null)
+    val fahrplanParameter = mutableFahrplanParameter.asStateFlow().filterNotNull()
 
     private val mutableFahrplanEmptyParameter = Channel<FahrplanEmptyParameter>()
     val fahrplanEmptyParameter = mutableFahrplanEmptyParameter.receiveAsFlow()
@@ -121,20 +119,41 @@ internal class FahrplanViewModel(
     var preserveVerticalScrollPosition: Boolean = false
 
     init {
-        updateUncanceledSessions()
+        updateDayMenu()
+        updateSchedule()
         requestScheduleUpdateAlarm()
         updateHorizontalScrollingProgressLineVisibility()
     }
 
-    private fun updateUncanceledSessions() {
+    private fun updateDayMenu() {
+        launch {
+            repository.sessionsWithoutShifts.filterNotNull().collectLatest { sessions ->
+                val displayDayIndex = repository.readDisplayDayIndex()
+                val numDays = repository.readMeta().numDays
+                val dayMenuEntries = if (numDays > 1) {
+                    navigationMenuEntriesGenerator.getDayMenuEntries(numDays, sessions)
+                } else {
+                    emptyList()
+                }
+                mutableDayMenuParameter.value = DayMenuParameter(dayMenuEntries, displayDayIndex)
+            }
+        }
+    }
+
+    private fun updateSchedule() {
         launch {
             repository.uncanceledSessionsForDayIndex.collect { scheduleData ->
-                val sessions = scheduleData.allSessions
-                if (sessions.isEmpty()) {
+                if (scheduleData.allSessions.isEmpty()) {
                     val scheduleVersion = repository.readMeta().version
                     if (scheduleVersion.isNotEmpty()) {
                         mutableFahrplanEmptyParameter.sendOneTimeEvent(FahrplanEmptyParameter(scheduleVersion))
                     } // else: Nothing to do because schedule has not been loaded yet
+                } else {
+                    val useDeviceTimeZone = repository.readUseDeviceTimeZoneEnabled()
+                    val customizedScheduleData = scheduleData.customizeEngelsystemRoomName()
+                    val parameter = FahrplanParameter(customizedScheduleData, useDeviceTimeZone)
+                    logging.d(LOG_TAG, "Loaded ${parameter.scheduleData.allSessions.size} uncanceled sessions.")
+                    mutableFahrplanParameter.value = parameter
                 }
             }
         }
@@ -200,33 +219,6 @@ internal class FahrplanViewModel(
             roomData.copy(roomName = customRoomName, sessions = customSessions)
         }
     )
-
-    private fun createFahrplanParameter(
-        scheduleData: ScheduleData,
-        allSessionsForAllDaysWithoutShifts: List<Session>,
-    ): FahrplanParameter {
-        val dayIndex = repository.readDisplayDayIndex()
-        val numDays = repository.readMeta().numDays
-        val dayMenuEntries = if (numDays > 1) {
-            navigationMenuEntriesGenerator.getDayMenuEntries(
-                numDays,
-                allSessionsForAllDaysWithoutShifts
-            )
-        } else {
-            emptyList()
-        }
-        val useDeviceTimeZone = repository.readUseDeviceTimeZoneEnabled()
-
-        return FahrplanParameter(
-            scheduleData = scheduleData,
-            useDeviceTimeZone = useDeviceTimeZone,
-            numDays = numDays,
-            dayIndex = dayIndex,
-            dayMenuEntries = dayMenuEntries
-        ).also {
-            logging.d(LOG_TAG, "Loaded ${it.scheduleData.allSessions.size} uncanceled sessions.")
-        }
-    }
 
     /**
      * Requests loading the schedule from the [AppRepository] to update the UI. UI components must

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/observables/DayMenuParameter.kt
@@ -1,0 +1,8 @@
+package nerd.tuxmobil.fahrplan.congress.schedule.observables
+
+data class DayMenuParameter(
+    val dayMenuEntries: List<String> = emptyList(),
+    val displayDayIndex: Int = 0,
+) {
+    val isValid = displayDayIndex > 0
+}

app/src/main/java/nerd/tuxmobil/fahrplan/congress/schedule/observables/FahrplanParameter.kt
@@ -9,11 +9,6 @@ import nerd.tuxmobil.fahrplan.congress.schedule.FahrplanViewModel
  * in the [FahrplanViewModel] which is observed by the [FahrplanFragment].
  */
 data class FahrplanParameter(
-
     val scheduleData: ScheduleData,
     val useDeviceTimeZone: Boolean,
-    val numDays: Int,
-    val dayIndex: Int,
-    val dayMenuEntries: List<String>
-
 )

app/src/test/java/nerd/tuxmobil/fahrplan/congress/schedule/FahrplanViewModelTest.kt
@@ -35,6 +35,7 @@ import nerd.tuxmobil.fahrplan.congress.repositories.LoadScheduleState.InitialPar
 import nerd.tuxmobil.fahrplan.congress.repositories.LoadScheduleState.ParseFailure
 import nerd.tuxmobil.fahrplan.congress.repositories.LoadScheduleState.ParseSuccess
 import nerd.tuxmobil.fahrplan.congress.repositories.LoadScheduleState.Parsing
+import nerd.tuxmobil.fahrplan.congress.schedule.observables.DayMenuParameter
 import nerd.tuxmobil.fahrplan.congress.schedule.observables.FahrplanEmptyParameter
 import nerd.tuxmobil.fahrplan.congress.schedule.observables.FahrplanParameter
 import nerd.tuxmobil.fahrplan.congress.schedule.observables.ScrollToCurrentSessionParameter
@@ -52,6 +53,8 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 import org.threeten.bp.ZoneOffset
 
 @ExtendWith(MainDispatcherTestExtension::class)
@@ -175,7 +178,7 @@ class FahrplanViewModelTest {
                 val repository = createRepository(uncanceledSessionsForDayIndexFlow = emptyFlow())
                 val viewModel = createViewModel(repository)
                 viewModel.fahrplanParameter.test {
-                    awaitComplete()
+                    expectNoEvents()
                 }
                 viewModel.fahrplanEmptyParameter.test {
                     expectNoEvents()
@@ -197,9 +200,10 @@ class FahrplanViewModelTest {
                 val expected = FahrplanEmptyParameter("test-version")
                 viewModel.fahrplanEmptyParameter.test {
                     assertThat(awaitItem()).isEqualTo(expected)
+                    expectNoEvents()
                 }
                 viewModel.fahrplanParameter.test {
-                    awaitComplete()
+                    expectNoEvents()
                 }
                 verifyInvokedOnce(repository).readMeta()
                 verifyInvokedNever(repository).readDisplayDayIndex()
@@ -218,7 +222,7 @@ class FahrplanViewModelTest {
                     expectNoEvents()
                 }
                 viewModel.fahrplanParameter.test {
-                    awaitComplete()
+                    expectNoEvents()
                 }
                 verifyInvokedOnce(repository).readMeta()
                 verifyInvokedNever(repository).readDisplayDayIndex()
@@ -237,13 +241,10 @@ class FahrplanViewModelTest {
             val expected = FahrplanParameter(
                 scheduleData = createScheduleData("session-01"),
                 useDeviceTimeZone = false,
-                numDays = 1,
-                dayIndex = 2,
-                dayMenuEntries = emptyList()
             )
             viewModel.fahrplanParameter.test {
                 assertThat(awaitItem()).isEqualTo(expected)
-                awaitComplete()
+                expectNoEvents()
             }
             viewModel.fahrplanEmptyParameter.test {
                 expectNoEvents()
@@ -267,14 +268,11 @@ class FahrplanViewModelTest {
                 val expected = FahrplanParameter(
                     scheduleData = createScheduleData("session-01", hasAlarm = true, isHighlight = true),
                     useDeviceTimeZone = false,
-                    numDays = 1,
-                    dayIndex = 2,
-                    dayMenuEntries = emptyList()
                 )
                 viewModel.fahrplanParameter.test {
                     val actual = awaitItem()
                     assertThat(actual).isEqualTo(expected)
-                    awaitComplete()
+                    expectNoEvents()
                 }
                 viewModel.fahrplanEmptyParameter.test {
                     expectNoEvents()
@@ -290,33 +288,22 @@ class FahrplanViewModelTest {
     inner class NavigationMenu {
 
         @Test
-        fun `fahrplanParameter property emits and generates navigation menu entries`() = runTest {
+        fun `dayMenuEntries property emits and generates navigation menu entries`() = runTest {
             val repository = createRepository(
                 uncanceledSessionsForDayIndexFlow = flowOf(createScheduleData("session-01")),
-                sessionsWithoutShiftsFlow = flowOf(listOf(Session("not relevant"))),
+                sessionsWithoutShiftsFlow = flowOf(listOf(Session("session-01"))),
                 meta = Meta(numDays = 2),
-                displayDayIndex = 0
+                displayDayIndex = 1,
             )
             val menuEntriesGenerator = mock<NavigationMenuEntriesGenerator> {
                 on { getDayMenuEntries(any(), anyOrNull(), any()) } doReturn listOf("Day 1", "Day 2")
             }
             val viewModel = createViewModel(repository, navigationMenuEntriesGenerator = menuEntriesGenerator)
-            val expected = FahrplanParameter(
-                scheduleData = createScheduleData("session-01"),
-                useDeviceTimeZone = false,
-                numDays = 2,
-                dayIndex = 0,
-                dayMenuEntries = listOf("Day 1", "Day 2")
-            )
-            viewModel.fahrplanParameter.test {
-                assertThat(awaitItem()).isEqualTo(expected)
-                awaitComplete()
-            }
-            viewModel.fahrplanEmptyParameter.test {
+            viewModel.dayMenuParameter.test {
+                assertThat(awaitItem()).isEqualTo(DayMenuParameter(listOf("Day 1", "Day 2"), 1))
                 expectNoEvents()
             }
             verifyInvokedOnce(menuEntriesGenerator).getDayMenuEntries(any(), anyOrNull(), any())
-            verifyInvokedOnce(repository).readDisplayDayIndex()
             verifyInvokedOnce(repository).readMeta()
         }
 
@@ -375,7 +362,7 @@ class FahrplanViewModelTest {
             viewModel.timeTextViewParameters.test {
                 assertThat(awaitItem()).isEqualTo(expected)
             }
-            verifyInvokedOnce(repository).readUseDeviceTimeZoneEnabled()
+            verify(repository, times(2)).readUseDeviceTimeZoneEnabled()
         }
 
     }