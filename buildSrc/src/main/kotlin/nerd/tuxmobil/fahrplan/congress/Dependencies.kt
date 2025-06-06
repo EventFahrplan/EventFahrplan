1940b83cd868675de28052daa0a13ebcfcf4a3b1
Tobias Preuss <tobias.preuss@googlemail.com>
2025-06-06 15:33:03 +0200

    Use Android Gradle Plugin v.8.10.1.
    + Changelogs:
      https://developer.android.com/build/releases/past-releases/agp-8-10-0-release-notes
      https://developer.android.com/build/releases/gradle-plugin

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -31,7 +31,7 @@ object Compose {
 object Plugins {
 
     private object Versions {
-        const val android = "8.9.1"
+        const val android = "8.10.1"
         const val dexcount = "4.0.0"
         const val kotlin = "2.1.21"
         const val ksp = "2.1.21-2.0.1"
