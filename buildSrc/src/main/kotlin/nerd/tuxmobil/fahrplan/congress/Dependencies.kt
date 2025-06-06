656b21882e0aed4ea7b5b937e9857eb4df6bc637
Tobias Preuss <tobias.preuss@googlemail.com>
2025-06-06 15:24:05 +0200

    Use junit-jupiter v.5.13.0 & junit-platform-launcher v.1.13.0.
    + Changelogs:
      https://junit.org/junit5/docs/5.13.0/release-notes/

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -65,8 +65,8 @@ object Libs {
         const val engelsystem = "10.0.0"
         const val fragmentCompose = "1.8.6"
         const val htmlConverter = "1.0.3"
-        const val junitJupiter = "5.12.2"
-        const val junitPlatform = "1.12.2"
+        const val junitJupiter = "5.13.0"
+        const val junitPlatform = "1.13.0"
         const val kotlinCoroutines = "1.10.2"
         const val lifecycle = "2.8.7" // compileSdk 35 is required as of 2.9.0
         const val markdownRenderer = "0.32.0"
