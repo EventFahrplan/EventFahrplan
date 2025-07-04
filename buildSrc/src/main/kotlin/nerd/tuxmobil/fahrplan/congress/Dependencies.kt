d7932b0fe8086f7899228415ac57fa5a72677258
Tobias Preuss <tobias.preuss@googlemail.com>
2025-07-04 17:22:51 +0200

    Document compileSdk requirement for htmlconverter.
    + Changelog: https://github.com/cbeyls/HtmlConverterCompose/releases/tag/1.1.0

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -64,7 +64,7 @@ object Libs {
         const val emailIntentBuilder = "2.0.0"
         const val engelsystem = "10.0.0"
         const val fragmentCompose = "1.8.8"
-        const val htmlConverter = "1.0.4"
+        const val htmlConverter = "1.0.4" // compose 2025.04.01 is required as of 1.1.0
         const val junitJupiter = "5.13.0"
         const val junitPlatform = "1.13.0"
         const val kotlinCoroutines = "1.10.2"
