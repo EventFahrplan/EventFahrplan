f2b8352def1c81b26e45e26b63c9b48d05e281f4
Tobias Preuss <tobias.preuss@googlemail.com>
2025-07-18 13:53:40 +0200

    Show day separators in search result list.
    + This visual grouping makes it easier to understand when sessions of
      the search result list take place without reading the detail
      information of each item.
    + The implementation matches the one of the schedule changes screen.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/search/SearchResultParameterFactory.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/search/SearchResultParameterFactory.kt
@@ -1,10 +1,14 @@
 package nerd.tuxmobil.fahrplan.congress.search
 
 import nerd.tuxmobil.fahrplan.congress.R
+import nerd.tuxmobil.fahrplan.congress.commons.DaySeparatorFactory
+import nerd.tuxmobil.fahrplan.congress.commons.DaySeparatorProperty
 import nerd.tuxmobil.fahrplan.congress.commons.FormattingDelegate
 import nerd.tuxmobil.fahrplan.congress.commons.ResourceResolving
+import nerd.tuxmobil.fahrplan.congress.dataconverters.toVirtualDays
 import nerd.tuxmobil.fahrplan.congress.models.Session
 import nerd.tuxmobil.fahrplan.congress.search.SearchResultParameter.SearchResult
+import nerd.tuxmobil.fahrplan.congress.search.SearchResultParameter.Separator
 import nerd.tuxmobil.fahrplan.congress.utils.ContentDescriptionFormatting
 import nerd.tuxmobil.fahrplan.congress.utils.SessionPropertiesFormatting
 
@@ -12,11 +16,37 @@ class SearchResultParameterFactory(
     private val resourceResolving: ResourceResolving,
     private val sessionPropertiesFormatting: SessionPropertiesFormatting,
     private val contentDescriptionFormatting: ContentDescriptionFormatting,
+    private val daySeparatorFactory: DaySeparatorFactory,
     private val formattingDelegate: FormattingDelegate,
 ) : FormattingDelegate by formattingDelegate {
 
-    fun createSearchResults(sessions: List<Session>, useDeviceTimeZone: Boolean): List<SearchResult> {
-        return sessions.map { createSearchResult(it, useDeviceTimeZone) }
+    fun createSearchResults(sessions: List<Session>, useDeviceTimeZone: Boolean): List<SearchResultParameter> {
+        val list = mutableListOf<SearchResultParameter>()
+        sessions
+            .toVirtualDays()
+            .filter { it.sessions.isNotEmpty() }
+            .forEach { virtualDay ->
+                val dayIndex = virtualDay.index
+                val session = virtualDay.sessions.first()
+                list += Separator(
+                    DaySeparatorProperty(
+                        value = daySeparatorFactory.createDaySeparatorText(
+                            dayIndex = dayIndex,
+                            session = session,
+                            useDeviceTimeZone = useDeviceTimeZone,
+                        ),
+                        contentDescription = daySeparatorFactory.createDaySeparatorContentDescription(
+                            dayIndex = dayIndex,
+                            session = session,
+                            useDeviceTimeZone = useDeviceTimeZone,
+                        )
+                    )
+                )
+                virtualDay.sessions.forEach {
+                    list += createSearchResult(it, useDeviceTimeZone)
+                }
+            }
+        return list
     }
 
     private fun createSearchResult(session: Session, useDeviceTimeZone: Boolean): SearchResult {
