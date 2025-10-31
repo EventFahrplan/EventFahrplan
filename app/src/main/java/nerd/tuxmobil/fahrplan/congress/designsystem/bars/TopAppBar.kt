e18bce71a7c84cc9cab62d3dcd556ed2246524f7
cketti <ck@cketti.de>
2025-10-31 18:46:56 +0100

    Change `TopAppBar` default insets to "safe drawing"

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/bars/TopAppBar.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/bars/TopAppBar.kt
@@ -1,12 +1,16 @@
 package nerd.tuxmobil.fahrplan.congress.designsystem.bars
 
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import nerd.tuxmobil.fahrplan.congress.designsystem.themes.EventFahrplanTheme
 import androidx.compose.material3.TopAppBar as Material3TopAppBar
+import androidx.compose.material3.TopAppBarDefaults as Material3TopAppBarDefaults
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -15,14 +19,22 @@ fun TopAppBar(
     modifier: Modifier = Modifier,
     navigationIcon: @Composable () -> Unit,
     actions: @Composable RowScope.() -> Unit,
+    windowInsets: WindowInsets = TopAppBarDefaults.windowInsets,
 ) {
     Material3TopAppBar(
         modifier = modifier,
-        colors = TopAppBarDefaults.topAppBarColors(
+        colors = Material3TopAppBarDefaults.topAppBarColors(
             containerColor = EventFahrplanTheme.colorScheme.topAppBarContainer,
         ),
         title = title,
         navigationIcon = navigationIcon,
+        windowInsets = windowInsets,
         actions = actions,
     )
+}
+
+object TopAppBarDefaults {
+    val windowInsets: WindowInsets
+        @Composable
+        get() = WindowInsets.safeDrawing.only(WindowInsetsSides.Horizontal + WindowInsetsSides.Top)
 }
