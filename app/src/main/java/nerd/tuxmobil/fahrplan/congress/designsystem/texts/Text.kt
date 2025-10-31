8bfa2b84879cfae6153cfb0cb567b0060107acec
cketti <ck@cketti.de>
2025-10-31 18:26:44 +0100

    Add `SwitchPreference` widget

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/texts/Text.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/texts/Text.kt
@@ -1,8 +1,10 @@
 package nerd.tuxmobil.fahrplan.congress.designsystem.texts
 
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -25,6 +27,7 @@ fun Text(
     lineHeight: TextUnit = TextUnit.Unspecified,
     overflow: TextOverflow = TextOverflow.Clip,
     maxLines: Int = Int.MAX_VALUE,
+    style: TextStyle = LocalTextStyle.current,
 ) {
     Material3Text(
         modifier = modifier,
@@ -38,5 +41,6 @@ fun Text(
         lineHeight = lineHeight,
         overflow = overflow,
         maxLines = maxLines,
+        style = style,
     )
 }
