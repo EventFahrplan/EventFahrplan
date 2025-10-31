f59518ff14ffed24632aef5e737536cfe5424c75
cketti <ck@cketti.de>
2025-10-31 18:19:39 +0100

    Don't override `outline` and `surfaceContainerHigh` in theme

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/search/SearchBar.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/search/SearchBar.kt
@@ -2,8 +2,11 @@ package nerd.tuxmobil.fahrplan.congress.designsystem.search
 
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SearchBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import nerd.tuxmobil.fahrplan.congress.designsystem.themes.EventFahrplanTheme
 import androidx.compose.material3.SearchBar as Material3SearchBar
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -13,6 +16,8 @@ fun SearchBar(
     expanded: Boolean,
     onExpandedChange: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
+    containerColor: Color = Color.Transparent,
+    dividerColor: Color = EventFahrplanTheme.colorScheme.searchBarDivider,
     content: @Composable ColumnScope.() -> Unit,
 ) {
     Material3SearchBar(
@@ -20,6 +25,10 @@ fun SearchBar(
         expanded = expanded,
         onExpandedChange = onExpandedChange,
         modifier = modifier,
+        colors = SearchBarColors(
+            containerColor = containerColor,
+            dividerColor = dividerColor,
+        ),
         content = content,
     )
 }
