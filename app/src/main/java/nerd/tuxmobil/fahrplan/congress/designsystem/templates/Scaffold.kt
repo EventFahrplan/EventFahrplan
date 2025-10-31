657dde09ecbd7572439aea7b21b454f8ec8c0459
cketti <ck@cketti.de>
2025-10-31 18:31:18 +0100

    Add support for custom content insets to `Scaffold`

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/templates/Scaffold.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/templates/Scaffold.kt
@@ -1,6 +1,8 @@
 package nerd.tuxmobil.fahrplan.congress.designsystem.templates
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Scaffold as Material3Scaffold
@@ -9,11 +11,13 @@ import androidx.compose.material3.Scaffold as Material3Scaffold
 fun Scaffold(
     modifier: Modifier = Modifier,
     topBar: @Composable () -> Unit = {},
+    contentWindowInsets: WindowInsets = ScaffoldDefaults.contentWindowInsets,
     content: @Composable (PaddingValues) -> Unit,
 ) {
     Material3Scaffold(
         content = content,
         modifier = modifier,
+        contentWindowInsets = contentWindowInsets,
         topBar = topBar,
     )
 }
