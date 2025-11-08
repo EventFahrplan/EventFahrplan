9bd9976ad99c2d8b62e57953d7f7d02144de585d
Tobias Preuss <tobias.preuss@googlemail.com>
2025-11-08 14:49:26 +0100

    Fix border color of "Clear" button at search history screen.
    + Broken since: 48a0d2f132d49ec1d11baa8f935d507851b33ea9

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/buttons/ButtonOutlined.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/buttons/ButtonOutlined.kt
@@ -1,6 +1,8 @@
 package nerd.tuxmobil.fahrplan.congress.designsystem.buttons
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.OutlinedButton as Material3OutlinedButton
@@ -8,12 +10,15 @@ import androidx.compose.material3.OutlinedButton as Material3OutlinedButton
 @Composable
 fun ButtonOutlined(
     modifier: Modifier = Modifier,
+    enabled: Boolean = true,
     onClick: () -> Unit,
+    border: BorderStroke? = ButtonDefaults.outlinedButtonBorder(enabled),
     content: @Composable RowScope.() -> Unit,
 ) {
     Material3OutlinedButton(
         onClick = { onClick() },
         modifier = modifier,
+        border = border,
         content = content,
     )
 }
