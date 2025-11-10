d9d82e2bc7c1941f42182ff18c5eb139ff4100bc
Tobias Preuss <tobias.preuss@googlemail.com>
2025-11-10 23:16:17 +0100

    Clear database from 38C3 on upgrade.
    + Version 1.72.0 (110) was the last 38C3 release on
      Google Play and F-Droid.
      Related: 137f5e14f1864b369e6a108b3fc42a85e5f8ed7f

--- database/src/main/java/info/metadude/android/eventfahrplan/database/sqliteopenhelper/HighlightDBOpenHelper.kt
+++ database/src/main/java/info/metadude/android/eventfahrplan/database/sqliteopenhelper/HighlightDBOpenHelper.kt
@@ -17,7 +17,7 @@ internal class HighlightDBOpenHelper(context: Context) : SQLiteOpenHelper(
 ) {
 
     private companion object {
-        const val DATABASE_VERSION = 6
+        const val DATABASE_VERSION = 7
         const val DATABASE_NAME = "highlight"
 
         // language=sql
@@ -33,7 +33,7 @@ internal class HighlightDBOpenHelper(context: Context) : SQLiteOpenHelper(
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) = with(db) {
-        // Clear database from Camp 2023 & 37C3 2023.
+        // Clear database from Camp 2023 & 38C3 2024.
         dropTableIfExist(NAME)
         onCreate(this)
     }
