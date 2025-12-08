9e10d1e7f7d159deaf29f868ef08be64ee9c0767
Tobias Preuss <tobias.preuss@googlemail.com>
2025-12-08 18:32:38 +0100

    Avoid recreating Regex instance.
    Co-authored-by: cketti <ck@cketti.de>

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/utils/MarkdownConverter.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/utils/MarkdownConverter.kt
@@ -6,14 +6,14 @@ package nerd.tuxmobil.fahrplan.congress.utils
 object MarkdownConverter : MarkdownConversion {
 
     // language=regex
-    private const val MARKDOWN_LINK_REGEX = """\[(.*?)\]\(([^ \)]+)[^\\)]*+\)"""
+    private val MARKDOWN_LINK_REGEX = """\[(.*?)\]\(([^ \)]+)[^\\)]*+\)""".toRegex()
 
     /**
      * Converts Markdown formatted links in the given [markdown] text
      * into HTML formatted links and returns the text as a string.
      */
     override fun markdownLinksToHtmlLinks(markdown: String): String {
-        return MARKDOWN_LINK_REGEX.toRegex().replace(markdown) { matchResult ->
+        return MARKDOWN_LINK_REGEX.replace(markdown) { matchResult ->
             val title = matchResult.groupValues[1]
             val url = matchResult.groupValues[2]
             val displayText = title.ifEmpty { url }
@@ -26,7 +26,7 @@ object MarkdownConverter : MarkdownConversion {
      * into plain text links and return the text as a string.
      */
     override fun markdownLinksToPlainTextLinks(markdown: String): String {
-        return MARKDOWN_LINK_REGEX.toRegex().replace(markdown) { matchResult ->
+        return MARKDOWN_LINK_REGEX.replace(markdown) { matchResult ->
             val title = matchResult.groupValues[1]
             val url = matchResult.groupValues[2]
             if (title.isEmpty()) url else "$title ($url)"
