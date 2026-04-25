9b11ce1cd4159a0747d22b22febaa7fdddc62eef
Tobias Preuss <tobias.preuss@googlemail.com>
2026-04-25 22:43:38 +0200

    Add previews to composables.

--- app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/buttons/ButtonIcon.kt
+++ app/src/main/java/nerd/tuxmobil/fahrplan/congress/designsystem/buttons/ButtonIcon.kt
@@ -2,6 +2,10 @@ package nerd.tuxmobil.fahrplan.congress.designsystem.buttons
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import nerd.tuxmobil.fahrplan.congress.R
+import nerd.tuxmobil.fahrplan.congress.designsystem.icons.IconDecorative
+import nerd.tuxmobil.fahrplan.congress.designsystem.themes.EventFahrplanTheme
 import androidx.compose.material3.IconButton as Material3IconButton
 
 @Composable
@@ -11,8 +15,19 @@ fun ButtonIcon(
     content: @Composable () -> Unit,
 ) {
     Material3IconButton(
-        onClick = { onClick() },
+        onClick = onClick,
         modifier = modifier,
         content = content,
     )
+}
+
+@PreviewLightDark
+@Composable
+private fun ButtonIconPreview() {
+    EventFahrplanTheme {
+        ButtonIcon(
+            content = { IconDecorative(R.drawable.ic_action_navigate) },
+            onClick = { },
+        )
+    }
 }
