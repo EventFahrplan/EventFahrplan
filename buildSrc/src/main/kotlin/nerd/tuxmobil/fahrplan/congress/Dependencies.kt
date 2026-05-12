ea06567030462ea6aa902979df6cc361fbcf1034
Tobias Preuss <tobias.preuss@googlemail.com>
2026-05-12 16:27:41 +0200

    Use kotlin v.2.3.21 & ksp v.2.3.7.
    + Changelogs:
      https://github.com/JetBrains/kotlin/releases/tag/v2.3.21
      https://github.com/google/ksp/releases/tag/2.3.7

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -34,8 +34,8 @@ object Plugins {
     private object Versions {
         const val android = "8.13.2"
         const val dexcount = "4.0.0"
-        const val kotlin = "2.3.20"
-        const val ksp = "2.3.6"
+        const val kotlin = "2.3.21"
+        const val ksp = "2.3.7"
         const val sonarQube = "7.3.0.8198"
         const val unMock = "0.9.0"
         const val versions = "0.54.0"
